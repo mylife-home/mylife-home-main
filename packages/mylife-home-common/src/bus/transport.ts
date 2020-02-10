4772689c5d07d6b671fceeb2d9ebebd281b59f78
vincent-tr <vincent@stoic.com>
2020-02-10 17:55:42 +0100

    wip transport

--- packages/mylife-home-common/src/bus/transport.ts
+++ packages/mylife-home-common/src/bus/transport.ts
@@ -1,6 +1,26 @@
 import mqtt from 'mqtt';
 import * as encoding from './encoding';
 
+export class Transport {
+  client: mqtt.Client;
+  instanceName: string;
+
+  constructor(instanceName: string, serverUrl: string) {
+    this.instanceName = instanceName;
+    this.client = mqtt.connect(serverUrl, {
+      // will: { topic: `${instanceName}/online`, payload: encoding.writeBool(false), retain: false } TODO: bad payload type
+    });
+
+    this.client.on('connect', () => {
+      this.client.publish(`${this.instanceName}/online`, encoding.writeBool(true), { retain: true });
+    });
+  }
+
+  terminate(): void {
+    this.client.publish(`${this.instanceName}/online`, encoding.writeBool(false));
+  }
+}
+/*
 const client = mqtt.connect('mqtt://localhost');
 
 client.on('connect', () => {
@@ -19,3 +39,4 @@ client.on('connect', () => {
     console.log(topic, encoding.readBool(message));
   });
 });
+*/
