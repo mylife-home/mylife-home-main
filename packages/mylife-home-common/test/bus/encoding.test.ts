94d787fb9d51f9316eb98f4473f171abd2bfecc4
vincent-tr <vincent@stoic.com>
2020-03-02 15:34:04 +0100

    tests

---
+++ packages/mylife-home-common/test/bus/encoding.test.ts
@@ -0,0 +1,33 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as encoding from '../../src/bus/encoding';
+
+describe('encoding', () => {
+  it('should encode bool (true)', () => {
+    const buffer = encoding.writeBool(true);
+    expect(buffer.toString('hex')).to.equal('01');
+  });
+
+  it('should encode bool (false)', () => {
+    const buffer = encoding.writeBool(false);
+    expect(buffer.toString('hex')).to.equal('00');
+  });
+
+  it('should decode bool (01)', () => {
+    const buffer = Buffer.from('01', 'hex');
+    const value = encoding.readBool(buffer);
+    expect(value).to.equal(true);
+  });
+
+  it('should decode bool (02)', () => {
+    const buffer = Buffer.from('02', 'hex');
+    const value = encoding.readBool(buffer);
+    expect(value).to.equal(true);
+  });
+
+  it('should decode bool (00)', () => {
+    const buffer = Buffer.from('00', 'hex');
+    const value = encoding.readBool(buffer);
+    expect(value).to.equal(false);
+  });
+});
