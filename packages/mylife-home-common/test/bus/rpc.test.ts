187d4fe0d6d3b6d54c2ed5fe70d2324ccc19d334
vincent-tr <vincent@stoic.com>
2020-03-03 17:34:28 +0100

    tests refactoring

--- packages/mylife-home-common/test/bus/rpc.test.ts
+++ packages/mylife-home-common/test/bus/rpc.test.ts
@@ -1,23 +1,18 @@
-import net from 'net';
 import 'mocha';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import aedes from 'aedes';
-import { Transport } from '../../src/bus/transport';
+import { MqttTestSession } from './tools';
 
-const SERVER_PORT = 11883;
-const SERVER_URL = `tcp://localhost:${SERVER_PORT}`;
 const RPC_ADDRESS = 'testRpc';
 
 describe('bus/rpc', () => {
-  setupMqttServer();
-
   it('should call rpc server', async () => {
-    const client = new Transport('client', SERVER_URL);
-    const server = new Transport('server', SERVER_URL);
+    const session = new MqttTestSession();
+    await session.init();
     try {
-      await waitForConnected(client);
-      await waitForConnected(server);
+
+      const client = await session.createTransport('client');
+      const server = await session.createTransport('server');
 
       const REQUEST = { foo: 'bar' };
       const RESPONSE = { foo: 'baz' };
@@ -33,56 +28,7 @@ describe('bus/rpc', () => {
       expect(serverImpl.firstCall.args[0]).to.deep.equal(REQUEST);
 
     } finally {
-      await client.terminate();
-      await server.terminate();
+      await session.terminate();
     }
   });
 });
-
-function setupMqttServer() {
-
-  let server: net.Server;
-  let aedesServer: aedes.Aedes;
-
-  beforeEach('setup mqtt server', async () => {
-    aedesServer = aedes.Server();
-    server = net.createServer(aedesServer.handle);
-    await new Promise(resolve => server.listen(SERVER_PORT, resolve));
-  });
-
-  afterEach('setup mqtt server', async () => {
-    await new Promise(resolve => aedesServer.close(resolve));
-    await new Promise(resolve => server.close(resolve));
-  });
-}
-
-async function waitForConnected(transport: Transport) {
-  return new Promise((resolve, reject) => {
-    if (transport.online) {
-      resolve();
-      return;
-    }
-
-    const onEnd = () => {
-      transport.off('onlineChange', onOnlineChange);
-      transport.off('error', onError);
-    };
-
-    const onError = (err: Error) => {
-      onEnd();
-      reject(err);
-    }
-
-    const onOnlineChange = (value: boolean) => {
-      if (!value) {
-        return;
-      }
-
-      onEnd();
-      resolve();
-    };
-
-    transport.on('onlineChange', onOnlineChange);
-    transport.on('error', onError);
-  });
-}
