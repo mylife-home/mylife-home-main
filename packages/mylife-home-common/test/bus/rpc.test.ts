71064365023b6ed95009a25a75cf44920abdae2c
vincent-tr <vincent@stoic.com>
2020-03-03 17:04:25 +0100

    testing

--- packages/mylife-home-common/test/bus/rpc.test.ts
+++ packages/mylife-home-common/test/bus/rpc.test.ts
@@ -3,7 +3,7 @@ import util from 'util';
 import 'mocha';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import aedes from 'aedes';
+import aedes, { Server } from 'aedes';
 import { Transport } from '../../src/bus/transport';
 
 const SERVER_PORT = 11883;
@@ -16,48 +16,40 @@ describe('bus/rpc', () => {
   it('should call rpc server', async () => {
     const client = new Transport('client', SERVER_URL);
     const server = new Transport('server', SERVER_URL);
+    try {
+      const REQUEST = { foo: 'bar' };
+      const RESPONSE = { foo: 'baz' };
 
-    const REQUEST = { foo: 'bar' };
-    const RESPONSE = { foo: 'baz' };
+      const serverImpl = sinon.fake.returns(RESPONSE);
 
-    const serverImpl = sinon.fake.returns(RESPONSE);
+      server.rpc.serve(RPC_ADDRESS, serverImpl);
 
-    server.rpc.serve(RPC_ADDRESS, serverImpl);
+      const result = await client.rpc.call('server', RPC_ADDRESS, REQUEST);
 
-    const result = await client.rpc.call('server', RPC_ADDRESS, REQUEST);
+      expect(result).to.deep.equal(RESPONSE);
+      expect(serverImpl.calledOnce).to.be.true;
+      expect(serverImpl.firstCall.args[0]).to.deep.equal(REQUEST);
 
-    expect(result).to.deep.equal(RESPONSE);
-    expect(serverImpl.calledOnce).to.be.true;
-    expect(serverImpl.firstCall.args[0]).to.deep.equal(REQUEST);
+    } finally {
+      await client.terminate();
+      await server.terminate();
+    }
   });
 });
 
 function setupMqttServer() {
 
-  let destroy: () => Promise<void>;
+  let server: net.Server;
+  let aedesServer: aedes.Aedes;
 
   beforeEach('setup mqtt server', async () => {
-    const aedesServer = aedes.Server();
-    const server = net.createServer(aedesServer.handle);
-    destroy = createDestroy(server);
+    aedesServer = aedes.Server();
+    server = net.createServer(aedesServer.handle);
     await new Promise(resolve => server.listen(SERVER_PORT, resolve));
   });
 
-  afterEach('setup mqtt server', destroy);
-}
-
-function createDestroy(server: net.Server): () => Promise<void> {
-  const connections = new Set<net.Socket>();
-
-  server.on('connection', conn => {
-    connections.add(conn);
-    conn.on('close', () => connections.delete(conn));
+  afterEach('setup mqtt server', async () => {
+    await new Promise(resolve => aedesServer.close(resolve));
+    await new Promise(resolve => server.close(resolve));
   });
-
-  return async () => {
-    for (const conn of connections) {
-      conn.destroy();
-    }
-    await new Promise((resolve, reject) => server.close((err) => err ? reject() : resolve()));
-  }
 }
