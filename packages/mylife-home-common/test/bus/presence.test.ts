1b8bd18be31f06d1787d1895ff46cf5b00211461
vincent-tr <vincent@stoic.com>
2020-03-04 17:57:00 +0100

    add tests

--- packages/mylife-home-common/test/bus/presence.test.ts
+++ packages/mylife-home-common/test/bus/presence.test.ts
@@ -33,7 +33,7 @@ describe('bus/presence', () => {
     }
   });
 
-  it('should handle server disconnection', async () => {
+  it('should handle other disconnection', async () => {
     const session = new MqttTestSession();
     await session.init();
     try {
@@ -59,6 +59,72 @@ describe('bus/presence', () => {
 
       spy.resetHistory();
       await session.reconnectTransport('other');
+      await sleep(20);
+
+      expect(tester.presence.getOnlines()).to.deep.equal(['other']);
+      expect(spy.calledOnce);
+      expect(spy.lastCall.args).to.deep.equal(['other', true]);
+
+    } finally {
+      await session.terminate();
+    }
+  });
+
+  it('should handle other disconnection gracefully', async () => {
+    const session = new MqttTestSession();
+    await session.init();
+    try {
+
+      const spy = sinon.fake();
+      const tester = await session.createTransport('tester');
+      tester.presence.on('instanceChange', spy);
+
+      await session.createTransport('other');
+      await sleep(20);
+
+      expect(tester.presence.getOnlines()).to.deep.equal(['other']);
+      expect(spy.calledOnce);
+      expect(spy.lastCall.args).to.deep.equal(['other', true]);
+
+      spy.resetHistory();
+      await session.closeTransport('other');
+      await sleep(20);
+
+      expect(tester.presence.getOnlines()).to.deep.equal([]);
+      expect(spy.calledOnce);
+      expect(spy.lastCall.args).to.deep.equal(['other', false]);
+
+    } finally {
+      await session.terminate();
+    }
+  });
+
+  it('should handle self disconnection', async () => {
+    const session = new MqttTestSession();
+    await session.init();
+    try {
+
+      const spy = sinon.fake();
+      const tester = await session.createTransport('tester');
+      tester.presence.on('instanceChange', spy);
+
+      await session.createTransport('other');
+      await sleep(20);
+
+      expect(tester.presence.getOnlines()).to.deep.equal(['other']);
+      expect(spy.calledOnce);
+      expect(spy.lastCall.args).to.deep.equal(['other', true]);
+
+      spy.resetHistory();
+      await session.disconnectTransport('tester');
+      await sleep(20);
+
+      expect(tester.presence.getOnlines()).to.deep.equal([]);
+      expect(spy.calledOnce);
+      expect(spy.lastCall.args).to.deep.equal(['other', false]);
+
+      spy.resetHistory();
+      await session.reconnectTransport('tester');
       await sleep(20);
 
       expect(tester.presence.getOnlines()).to.deep.equal(['other']);
