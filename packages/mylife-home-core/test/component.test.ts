b8421c6a2846d289b411f8480b051f1a230fcaec
vincent-tr <vincent.tr@gmail.com>
2020-04-19 21:34:22 +0200

    fix overriden name

--- packages/mylife-home-core/test/component.test.ts
+++ packages/mylife-home-core/test/component.test.ts
@@ -1,21 +1,48 @@
 import 'mocha';
 import 'reflect-metadata';
 import { expect } from 'chai';
-import { component, state, action, getDescriptor, ComponentDescriptor } from '../src/metadata';
-
-@component({ name: 'overridden-name'})
-class TestComponent {
-  @state
-  value: number;
-
-  @action
-  setValue(newValue: number) {
-    this.value = newValue;
-  }
-}
+import { component, state, action, getDescriptor, ComponentDescriptor, Type } from '../src/metadata';
 
 describe('components', () => {
-  it('should ...', () => {
-    console.log(getDescriptor(TestComponent).toMetadata());
+  it('should produce right medata using basic decorators', () => {
+    @component
+    class TestComponent1 {
+      @state
+      value: number;
+
+      @action
+      setValue(newValue: number) {
+        this.value = newValue;
+      }
+    }
+
+    expect(getDescriptor(TestComponent1).toMetadata()).to.deep.equal({
+      name: 'test-component1',
+      members: {
+        value: { member: 'state', type: Type.FLOAT },
+        setValue: { member: 'action', type: Type.FLOAT },
+      },
+    });
+  });
+
+  it('should produce right medata using advanced decorators', () => {
+    @component({ name: 'overridden-name' })
+    class TestComponent2 {
+      @state({ type: Type.INT32 })
+      value: number;
+
+      @action({ type: Type.UINT8 })
+      setValue(newValue: number) {
+        this.value = newValue;
+      }
+    }
+
+    expect(getDescriptor(TestComponent2).toMetadata()).to.deep.equal({
+      name: 'overridden-name',
+      members: {
+        value: { member: 'state', type: Type.INT32 },
+        setValue: { member: 'action', type: Type.UINT8 },
+      },
+    });
   });
 });
