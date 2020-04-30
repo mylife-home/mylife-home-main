d703f2be210d613bc19fa38c5104c49bed230306
vincent-tr <vincent.tr@gmail.com>
2020-04-30 17:38:24 +0200

    wip registry

--- packages/mylife-home-common/src/components/registry.ts
+++ packages/mylife-home-common/src/components/registry.ts
@@ -11,6 +11,7 @@ export interface Component extends EventEmitter {
   off(event: 'state', listener: (name: string, value: any) => void): this;
   once(event: 'state', listener: (name: string, value: any) => void): this;
 
+  readonly instanceName: string; // or null for local
   readonly id: string;
   readonly descriptor: ComponentDescriptor;
 
@@ -19,13 +20,33 @@ export interface Component extends EventEmitter {
   getStates(): { [name: string]: any; };
 }
 
-export class Registry {
-  constructor() {
+export interface Registry extends EventEmitter {
+  on(event: 'add', listener: (component: Component) => void): this;
+  off(event: 'add', listener: (component: Component) => void): this;
+  once(event: 'add', listener: (component: Component) => void): this;
 
-  }
+  on(event: 'remove', listener: (component: Component) => void): this;
+  off(event: 'remove', listener: (component: Component) => void): this;
+  once(event: 'remove', listener: (component: Component) => void): this;
+}
+
+export class Registry extends EventEmitter implements Registry {
+  private readonly store = new Set<Component>();
 
   addComponent(component: Component) {
+    if (this.store.has(component)) {
+      throw new Error(`Component ${component.instanceName}:${component.id} does already exist in the store`);
+    }
+    this.store.add(component);
+    this.emit('add', component);
+  }
 
+  removeComponent(component: Component) {
+    if (!this.store.has(component)) {
+      throw new Error(`Component ${component.instanceName}:${component.id} does not exist in the store`);
+    }
+    this.store.delete(component);
+    this.emit('remove', component);
   }
 }
 
