39cb5af183a0d05d09c41534319821b544ebb3de
vincent-tr <vincent.tr@gmail.com>
2020-05-04 17:54:58 +0200

    wip tests

--- packages/mylife-home-common/test/components/metadata.test.ts
+++ packages/mylife-home-common/test/components/metadata.test.ts
@@ -3,11 +3,18 @@ import { expect } from 'chai';
 import * as types from '../../src/components/metadata/types';
 
 describe('components/metadata', () => {
-  it('should parse type', async () => {
-    const type = types.parseType('range[1;2]');
-    expect(type).to.be.an.instanceof(types.Range);
-    expect(type.primitive).to.equal(types.Primitive.UINT8);
-    expect(type.toString()).to.equals('range[1;2]');
-  });
+  it('should parse range type', () => parserTest('range[1;2]'));
+  it('should parse range type with negative bound', () => parserTest('range[-2;-1]'));
+  it('should parse text type', () => parserTest('text'));
+  it('should parse float type', () => parserTest('float'));
+  it('should parse bool type', () => parserTest('bool'));
+  it('should parse enum type', () => parserTest('enum{a,b,c}'));
+
+  // TODO: test wrong type, wrong range/enum values
+
+  function parserTest(value: string) {
+    const type = types.parseType(value);
+    expect(type.toString()).to.equals(value);
+  }
 
 });
