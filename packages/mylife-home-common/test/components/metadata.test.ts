0045623d4c89d4112bd851feebbc435eb3f86f31
vincent-tr <vincent.tr@gmail.com>
2020-05-05 17:49:49 +0200

    add index + adapt tests

--- packages/mylife-home-common/test/components/metadata.test.ts
+++ packages/mylife-home-common/test/components/metadata.test.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
-import * as types from '../../src/components/metadata/types';
+import * as metadata from '../../src/components/metadata';
 
 describe('components/metadata', () => {
 
@@ -25,42 +25,42 @@ describe('components/metadata', () => {
     it('should fail to parse type with unrequired args', () => parserTestFail('bool-abc', `Type 'bool' requires no argument but got '-abc'`));
 
     function parserTest(value: string) {
-      const type = types.parseType(value);
+      const type = metadata.parseType(value);
       expect(type.toString()).to.equals(value);
     }
 
     function parserTestFail(value: string, expectedError: string | RegExp) {
-      expect(() => types.parseType(value)).to.throw(expectedError);
+      expect(() => metadata.parseType(value)).to.throw(expectedError);
     }
 
-    it('should build type text', () => testTrivialType(new types.Text(), 'string', 'text'));
-    it('should build type float', () => testTrivialType(new types.Float(), 'float', 'float'));
-    it('should build type bool', () => testTrivialType(new types.Bool(), 'bool', 'bool'));
-    it('should build type complex', () => testTrivialType(new types.Complex(), 'json', 'complex'));
+    it('should build type text', () => testTrivialType(new metadata.Text(), 'string', 'text'));
+    it('should build type float', () => testTrivialType(new metadata.Float(), 'float', 'float'));
+    it('should build type bool', () => testTrivialType(new metadata.Bool(), 'bool', 'bool'));
+    it('should build type complex', () => testTrivialType(new metadata.Complex(), 'json', 'complex'));
 
     it('should build type range[1;2]', () => testRangeType(1, 2, 'uint8'));
     it('should build type range[-1;0]', () => testRangeType(-1, 0, 'int8'));
     it('should build type range[0;5000]', () => testRangeType(0, 5000, 'uint32'));
     it('should build type range[-1;5000]', () => testRangeType(-1, 5000, 'int32'));
-    it('should fail to build type range[2;1]', () => expect(() => new types.Range(2, 1)).to.throw('Min >= max for range: min=2, max=1'));
-    it('should fail to build type range[0;1.1]', () => expect(() => new types.Range(0, 1.1)).to.throw('Bad max value for range: 1.1'));
-    it('should fail to build type range[0;5000000000]', () => expect(() => new types.Range(0, 5000000000)).to.throw('Cannot represent range type with min=0 and max=5000000000 because bounds are too big'));
-    it('should fail to build type range[-5000000000;0]', () => expect(() => new types.Range(-5000000000, 0)).to.throw('Cannot represent range type with min=-5000000000 and max=0 because bounds are too big'));
+    it('should fail to build type range[2;1]', () => expect(() => new metadata.Range(2, 1)).to.throw('Min >= max for range: min=2, max=1'));
+    it('should fail to build type range[0;1.1]', () => expect(() => new metadata.Range(0, 1.1)).to.throw('Bad max value for range: 1.1'));
+    it('should fail to build type range[0;5000000000]', () => expect(() => new metadata.Range(0, 5000000000)).to.throw('Cannot represent range type with min=0 and max=5000000000 because bounds are too big'));
+    it('should fail to build type range[-5000000000;0]', () => expect(() => new metadata.Range(-5000000000, 0)).to.throw('Cannot represent range type with min=-5000000000 and max=0 because bounds are too big'));
 
     it('should build type enum{a,b}', () => testEnumType('a', 'b'));
     it('should build type enum{a-b-c,b}', () => testEnumType('a-b-c', 'b'));
     it('should build type enum{a_b_c,b}', () => testEnumType('a_b_c', 'b'));
     it('should build type enum{ABC,DEF}', () => testEnumType('ABC', 'DEF'));
-    it('should fail to build type enum{a}', () => expect(() => new types.Enum('a')).to.throw('Cannot build an enum without at least 2 values'));
-    it('should fail to build type enum{}', () => expect(() => new types.Enum()).to.throw('Cannot build an enum without at least 2 values'));
+    it('should fail to build type enum{a}', () => expect(() => new metadata.Enum('a')).to.throw('Cannot build an enum without at least 2 values'));
+    it('should fail to build type enum{}', () => expect(() => new metadata.Enum()).to.throw('Cannot build an enum without at least 2 values'));
 
-    function testTrivialType(type: types.Type, expectedPrimitive: string, expectedToString: string) {
+    function testTrivialType(type: metadata.Type, expectedPrimitive: string, expectedToString: string) {
       expect(type.primitive.id).to.equal(expectedPrimitive);
       expect(type.toString()).to.equal(expectedToString);
     }
 
     function testRangeType(min: number, max: number, expectedPrimitive: string) {
-      const type = new types.Range(min, max);
+      const type = new metadata.Range(min, max);
       expect(type.min).to.equal(min);
       expect(type.max).to.equal(max);
       expect(type.primitive.id).to.equal(expectedPrimitive);
@@ -68,7 +68,7 @@ describe('components/metadata', () => {
     }
 
     function testEnumType(...values: string[]) {
-      const type = new types.Enum(...values);
+      const type = new metadata.Enum(...values);
       expect(type.values).to.deep.equal(values);
       expect(type.primitive.id).to.equal('string');
       expect(type.toString()).to.equal(`enum{${values.join(',')}}`);
