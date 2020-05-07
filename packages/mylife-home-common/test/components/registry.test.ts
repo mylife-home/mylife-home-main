3954f7a53f182c7839f75def5e5d70334b305bbc
vincent-tr <vincent.tr@gmail.com>
2020-05-07 13:32:01 +0200

    wip tests

--- packages/mylife-home-common/test/components/registry.test.ts
+++ packages/mylife-home-common/test/components/registry.test.ts
@@ -18,7 +18,7 @@ const TEST_PLUGIN: metadata.Plugin = Object.freeze({
 });
 
 describe('components/registry', () => {
-  it('should register plugin', () => {
+  it('should add plugin', () => {
     const registry = new Registry();
 
     const onPluginAdd = sinon.fake();
@@ -33,5 +33,28 @@ describe('components/registry', () => {
     expect(onPluginAdd.calledOnceWithExactly('my-instance', TEST_PLUGIN)).to.be.true;
     expect(onOther.notCalled).to.be.true;
     expect(registry.getPlugin('my-instance', 'module.name')).to.equal(TEST_PLUGIN);
+    expect(Array.from(registry.getPlugins('my-instance'))).to.deep.equal([TEST_PLUGIN]);
   });
+
+  it('should remove plugin', () => {
+    const registry = new Registry();
+    registry.addPlugin('my-instance', TEST_PLUGIN);
+
+    const onPluginRemove = sinon.fake();
+    const onOther = sinon.fake();
+    registry.on('plugin.add', onOther);
+    registry.on('plugin.remove', onPluginRemove);
+    registry.on('component.add', onOther);
+    registry.on('component.remove', onOther);
+
+    registry.removePlugin('my-instance', TEST_PLUGIN);
+
+    expect(onPluginRemove.calledOnceWithExactly('my-instance', TEST_PLUGIN)).to.be.true;
+    expect(onOther.notCalled).to.be.true;
+    expect(() => registry.getPlugin('my-instance', 'module.name')).to.throw('Plugin my-instance:module.name does not exist in the registry');
+    expect(Array.from(registry.getPlugins('my-instance'))).to.deep.equal([]);
+  });
+
+  // TODO: same with components
+  // TODO: test bus-publisher
 });
