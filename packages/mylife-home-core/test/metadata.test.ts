bc17372f2352156f0aa5be47833862888b5a3398
vincent-tr <vincent.tr@gmail.com>
2020-05-08 15:11:16 +0200

    adapt to new model

--- packages/mylife-home-core/test/metadata.test.ts
+++ packages/mylife-home-core/test/metadata.test.ts
@@ -1,19 +1,24 @@
 import 'mocha';
 import 'reflect-metadata';
 import { expect } from 'chai';
-import { component, config, state, action, getDescriptor, Float, Range, Enum, PluginUsage, ConfigType, build } from '../src/metadata';
+import { component, config, state, action, getDescriptor, Text, Float, Range, Enum, PluginUsage, ConfigType, build } from '../src/metadata';
 
 describe('metadata', () => {
   it('should produce right medata using basic decorators', () => {
     const descriptor = basic();
 
     expect(descriptor.getMetadata()).to.deep.equal({
+      id: 'module-TODO.test-component',
       name: 'test-component',
+      module: 'module-TODO',
+      version: '1.0.0-TODO',
+      usage: 'logic',
+      description: undefined,
       members: {
-        value: { member: 'state', type: new Float() },
-        setValue: { member: 'action', type: new Float() },
+        value: { memberType: 'state', valueType: new Float(), description: undefined },
+        setValue: { memberType: 'action', valueType: new Float(), description: undefined },
       },
-      config: {}
+      config: {},
     });
   });
 
@@ -21,16 +26,20 @@ describe('metadata', () => {
     const descriptor = advanced();
 
     expect(descriptor.getMetadata()).to.deep.equal({
+      id: 'module-TODO.overridden-name',
       name: 'overridden-name',
+      module: 'module-TODO',
+      version: '1.0.0-TODO',
+      usage: 'logic',
       description: 'component description',
       members: {
-        value: { member: 'state', type: new Range(-10, 10), description: 'state description' },
-        setValue: { member: 'action', type: new Enum('a', 'b', 'c'), description: 'action description' },
+        value: { memberType: 'state', valueType: new Range(-10, 10), description: 'state description' },
+        setValue: { memberType: 'action', valueType: new Range(-10, 10), description: 'action description' },
       },
       config: {
-        config1: { type: 'string', description: 'config description' },
-        config2: { type: 'integer' },
-      }
+        config1: { valueType: 'string', description: 'config description' },
+        config2: { valueType: 'integer', description: undefined },
+      },
     });
   });
 
@@ -40,6 +49,20 @@ describe('metadata', () => {
       value: number;
 
       @action
+      setValue(newValue: number) {
+        this.value = newValue;
+      }
+    }
+
+    expect(() => build()).to.throw(`Class 'TestComponent' looks like component but @component decorator is missing`);
+  });
+
+  it('should fail if wrong action type', () => {
+    class TestComponent {
+      @state
+      value: number;
+
+      @action({ type: new Text() })
       setValue(newValue: number) {
         this.value = newValue;
       }
@@ -74,7 +97,7 @@ function advanced() {
     @state({ description: 'state description', type: new Range(-10, 10) })
     value: number;
 
-    @action({ description: 'action description', type: new Enum('a', 'b', 'c') })
+    @action({ description: 'action description', type: new Range(-10, 10) })
     setValue(newValue: number) {
       this.value = newValue;
     }
