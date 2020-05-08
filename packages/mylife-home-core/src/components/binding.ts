c97834672417b4a436a8d4849ce21a647cbc7105
vincent-tr <vincent.tr@gmail.com>
2020-05-08 21:49:44 +0200

    wip binding skeleton

--- packages/mylife-home-core/src/components/binding.ts
+++ packages/mylife-home-core/src/components/binding.ts
@@ -1,5 +1,12 @@
 import { components } from 'mylife-home-common';
 
+export interface BindingConfiguration {
+  readonly sourceId: string;
+  readonly sourceState: string;
+  readonly targetId: string;
+  readonly targetAction: string;
+}
+
 export class Binding {
-  constructor(registry: components.Registry, sourceId: string, sourceState: string, targetId: string, targetAction: string) {}
+  constructor(registry: components.Registry, config: BindingConfiguration) {}
 }
