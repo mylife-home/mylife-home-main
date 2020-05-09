fdf89738345941ef67e8216490992ddc71c9076a
vincent-tr <vincent.tr@gmail.com>
2020-05-09 19:46:23 +0200

    add resisdent state cleanup tests

---
+++ packages/mylife-home-common/test/bus/client.test.ts
@@ -0,0 +1,32 @@
+import 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import * as bus from '../../src/bus';
+import { MqttTestSession, sleep } from './tools';
+
+describe('bus/client', () => {
+  it('should clean resident state on connection', async () => {
+    const session = new MqttTestSession();
+    await session.init();
+    try {
+      const observer = await session.createTransport('observer');
+
+      const tester1 = await session.createTransport('tester');
+      await tester1.metadata.set('garbage', true);
+      await expectMetaPaths(['garbage']);
+      await session.closeTransport('tester');
+
+      await session.createTransport('tester');
+      await expectMetaPaths([]);
+
+      async function expectMetaPaths(expectedPaths: string[]) {
+        const meta = await observer.metadata.createView('tester');
+        await sleep(20);
+        expect(Array.from(meta.paths)).to.deep.equal(expectedPaths);
+        await observer.metadata.closeView(meta);
+      }
+    } finally {
+      await session.terminate();
+    }
+  });
+});
