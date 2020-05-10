1c16f4ac71e0f6ac57ea53612ab32ac33be56ebc
vincent-tr <vincent.tr@gmail.com>
2020-05-10 17:31:59 +0200

    wip tests

--- packages/mylife-home-core/test/components/bus-publisher.test.ts
+++ packages/mylife-home-core/test/components/bus-publisher.test.ts
@@ -2,7 +2,7 @@ import 'mocha';
 import 'reflect-metadata';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { components, bus } from 'mylife-home-common';
+import { components, bus, tools } from 'mylife-home-common';
 import { metadata, ComponentHost, BusPublisher } from '../../src/components';
 import { MqttTestSession } from 'mylife-home-common/test/bus/tools';
 
@@ -11,19 +11,18 @@ describe('components/host', () => {
     const session = new MqttTestSession();
     await session.init();
     try {
-
       interface Instance {
         transport?: bus.Transport;
         registry?: components.Registry;
       }
 
       const tester: Instance = {};
-      tester.transport = await session.createTransport('tester'),
+      tester.transport = await session.createTransport('tester');
       tester.registry = new components.Registry({ transport: tester.transport });
       const busPublisher = new BusPublisher(tester.registry, tester.transport);
 
       const observer: Instance = {};
-      observer.transport = await session.createTransport('observer', { presenceTracking: true }),
+      observer.transport = await session.createTransport('observer', { presenceTracking: true });
       observer.registry = new components.Registry({ transport: observer.transport, publishRemoteComponents: true });
 
       const actionHandler = sinon.fake();
@@ -31,12 +30,11 @@ describe('components/host', () => {
       const plugin = build(tester.registry, () => {
         @metadata.plugin({ usage: metadata.PluginUsage.LOGIC })
         class TestPlugin {
-          constructor() {
-          }
-    
+          constructor() {}
+
           @metadata.state
           value: number = 42;
-    
+
           @metadata.action
           setValue(newValue: number) {
             actionHandler(newValue);
@@ -48,18 +46,28 @@ describe('components/host', () => {
       const component = new ComponentHost('my-component', plugin, {});
       tester.registry.addComponent(null, component);
 
+      await tools.sleep(20);
+
+      const remotePlugin = observer.registry.getPlugin('tester', 'test-module.test-plugin');
+      expect(remotePlugin).to.deep.equal(plugin);
+
       const remoteComponent = observer.registry.getComponent('tester', 'my-component');
       expect(remoteComponent).to.exist;
-      expect(remote)
-      expect()
 
+      expect(remoteComponent.getStates()).to.deep.equal({ value: 42 });
+      expect(actionHandler.notCalled).to.be.true;
+
+      remoteComponent.executeAction('setValue', 43);
+
+      expect(remoteComponent.getStates()).to.deep.equal({ value: 43 });
+      expect(actionHandler.calledOnceWithExactly(43)).to.be.true;
     } finally {
       await session.terminate();
     }
   });
 });
 
-function build(registry, callback: () => void) {
+function build(registry: components.Registry, callback: () => void) {
   metadata.builder.init('test-module', 'test-version', registry);
   try {
     callback();
