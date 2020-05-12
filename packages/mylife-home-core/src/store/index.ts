7d5ffaeaf1f95bc7eeababc73cd2e8a0ea1183c7
vincent-tr <vincent.tr@gmail.com>
2020-05-12 18:56:11 +0200

    store skeleton

---
+++ packages/mylife-home-core/src/store/index.ts
@@ -0,0 +1,72 @@
+export interface ComponentConfig {
+  readonly id: string;
+  readonly plugin: string;
+  readonly config: { [name: string]: any; };
+}
+
+export interface BindingConfig {
+  readonly sourceId: string;
+  readonly sourceState: string;
+  readonly targetId: string;
+  readonly targetAction: string;
+}
+
+export class Store {
+  private readonly components = new Map<string, ComponentConfig>();
+  private readonly bindings = new Map<string, BindingConfig>();
+  private dirty = false;
+
+  constructor() {
+
+  }
+
+  async close() {
+    if(this.dirty) {
+      await this.sync();
+    }
+  }
+
+  async sync() {
+    // TODO
+    this.dirty = false;
+  }
+
+  private change() {
+    this.dirty = true;
+    // TODO: schedule sync
+  }
+
+  setComponent(config: ComponentConfig) {
+    this.components.set(config.id, config);
+    this.change();
+  }
+
+  removeComponent(id: string) {
+    this.components.delete(id);
+    this.change();
+  }
+
+  addBinding(config: BindingConfig) {
+    const key = this.buildBindingKey(config);
+    this.bindings.set(key, config);
+    this.change();
+  }
+
+  removeBinding(config: BindingConfig) {
+    const key = this.buildBindingKey(config);
+    this.bindings.delete(key);
+    this.change();
+  }
+
+  private buildBindingKey(config: BindingConfig) {
+    return [config.sourceId, config.sourceState, config.targetId, config.targetAction].join('|');
+  }
+
+  getComponents() {
+    return new Set(this.components.values());
+  }
+
+  getBindings() {
+    return new Set(this.bindings.values());
+  }
+}
