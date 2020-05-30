05b521b44cdf58cd1e46a5337f2b9e94dc1ad502
vincent-tr <vincent.tr@gmail.com>
2020-05-30 16:15:31 +0200

    plugin loader

--- packages/mylife-home-core/src/manager/plugin-loader.ts
+++ packages/mylife-home-core/src/manager/plugin-loader.ts
@@ -1,20 +1,32 @@
 import path from 'path';
-import { components, buildInfo } from 'mylife-home-common';
+import fs from 'fs';
+import { components, buildInfo, logger } from 'mylife-home-common';
 import { metadata } from '../components';
 
+const log = logger.createLogger('mylife:home:core:manager:plugin-loader');
+
+declare function __non_webpack_require__(path: string): any;
+
 export function loadPlugins(registry: components.Registry) {
-  // TODO: list files
+  const pluginDirectory = path.join(__dirname, 'plugins');
+  for(const fileName of fs.readdirSync(pluginDirectory)) {
+    if(fileName.endsWith('.js') && !fileName.endsWith('.map.js')) {
+      const filePath = path.join(pluginDirectory, fileName);
+      loadPlugin(registry, filePath);
+    }
+  }
 }
 
 function loadPlugin(registry: components.Registry, filePath: string) {
-  // TODO: logs
   const bi = buildInfo.getInfo();
-  const moduleName = path.parse(filePath).name;
-  const moduleBuildInfo = bi.modules[`mylife-home-core-plugins-${moduleName}`];
+  const pluginName = path.parse(filePath).name;
+  const pluginVersion = bi.modules[`mylife-home-core-plugins-${pluginName}`].version;
 
-  metadata.builder.init(moduleName, moduleBuildInfo.version, registry);
+  log.info(`loading plugin ${pluginName} v${pluginVersion}`);
+
+  metadata.builder.init(pluginName, pluginVersion, registry);
   try {
-    require(filePath);
+    __non_webpack_require__(filePath);
     metadata.builder.build();
   } finally {
     metadata.builder.terminate();
