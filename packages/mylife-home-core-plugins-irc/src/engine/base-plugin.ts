3be5ef9b35e26a53a63d5c74bf989897f1159480
vincent-tr <vincent.tr@gmail.com>
2020-06-02 17:18:19 +0200

    wip irc plugin

--- packages/mylife-home-core-plugins-irc/src/engine/base-plugin.ts
+++ packages/mylife-home-core-plugins-irc/src/engine/base-plugin.ts
@@ -1,20 +1,49 @@
+import { registry, State } from './registry';
+
 export interface PluginConfiguration {
-  readonly configKey: string;
+  readonly networkKey: string;
   readonly ircComponent: string;
 }
 
 export abstract class BasePlugin {
-  constructor(config: PluginConfiguration) {
+  private readonly networkKey: string;
+  private readonly componentId: string;
+  private state: State = {};
 
+  constructor(config: PluginConfiguration) {
+    this.networkKey = config.networkKey;
+    this.componentId = config.ircComponent;
+
+    registry.on('change', this.onChange);
   }
 
   destroy() {
-
+    registry.off('change', this.onChange);
   }
+
+  private readonly onChange = () => {
+    const oldState = this.state;
+    const newState = registry.findState(this.networkKey, this.componentId) || {};
+    this.state = newState;
+
+    const keys = new Set([...Object.keys(oldState), ...Object.keys(newState)]);
+    for (const key of keys) {
+      const oldValue = undefinedToNull(oldState[key]);
+      const newValue = undefinedToNull(newState[key]);
+
+      if (!Object.is(oldValue, newValue)) {
+        this.onStateChanged(key, newValue);
+      }
+    }
+  };
 
   protected abstract onStateChanged(name: string, value: string): void;
 
-  protected executeAction(arg: string) {
-
+  protected executeAction(actionName: string, argument: string) {
+    registry.executeAction(this.networkKey, this.componentId, actionName, argument);
   }
+}
+
+function undefinedToNull(value: string) {
+  return value === undefined ? null : value;
 }
--- packages/mylife-home-core-plugins-irc/src/engine/registry.ts
+++ packages/mylife-home-core-plugins-irc/src/engine/registry.ts
@@ -1,23 +1,61 @@
-export interface Client {
-  readonly key: string;
+import { EventEmitter } from 'events';
+
+export type State = { readonly [name: string]: string; };
+export type Components = { readonly [id: string]: State; };
+export type Networks = { readonly [key: string]: Components; };
+
+export interface IrcComponent {
+  readonly networkKey: string;
+  readonly id: string;
+  readonly state: { [name: string]: string; };
 }
 
-export class Registry {
-  constructor() {
+export class Registry extends EventEmitter {
+  private _networks: Networks;
+
+  executeAction(networkKey: string, componentId: string, actionName: string, argument: string) {
+    this.emit('execute-action', networkKey, componentId, actionName, argument);
   }
 
-  publish(client: Client) {
-
+  createNetwork(networkKey: string) {
+    this.change({ ...this.networks, [networkKey]: {} });
   }
 
-  unpublish(client: Client) {
-
+  deleteNetwork(networkKey: string) {
+    const { [networkKey]: removed, ...newNetworks } = this.networks;
+    this.change(newNetworks);
   }
 
-  attach(key: string, binder: (client: Client) => void, unbinder: () => void): () => void {
-
+  setComponent(networkKey: string, componentId: string, state: { [name: string]: string; }) {
+    const newNetwork = { ... this.networks[networkKey], [componentId]: state };
+    this.change({ ...this.networks, [networkKey]: newNetwork });
   }
 
+  deleteComponent(networkKey: string, componentId: string) {
+    const { [componentId]: removed, ...newNetwork } = this.networks[networkKey];
+    this.change({ ...this.networks, [networkKey]: newNetwork });
+  }
+
+  private change(networks: Networks) {
+    this._networks = networks;
+    this.emit('change');
+  }
+
+  get networks(): Networks {
+    return this._networks;
+  }
+
+  hasComponent(networkKey: string, componentId: string) {
+    return !!this.findState(networkKey, componentId);
+  }
+
+  findState(networkKey: string, componentId: string) {
+    return this.networks[networkKey][componentId];
+  }
+
+  findComponents(networkKey: string) {
+    return this.networks[networkKey];
+  }
 }
 
 export const registry = new Registry();
