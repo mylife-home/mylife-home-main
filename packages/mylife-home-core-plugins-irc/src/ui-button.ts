a46342bc5b77f8c887a6ed4d2cb37c24aecd5b93
vincent-tr <vincent.tr@gmail.com>
2020-06-02 11:16:04 +0200

    wip irc plugin

--- packages/mylife-home-core-plugins-irc/src/ui-button.ts
+++ packages/mylife-home-core-plugins-irc/src/ui-button.ts
@@ -1,22 +1,31 @@
 import { components } from 'mylife-home-core';
+import { BasePlugin } from './engine/base-plugin';
+import * as encoding from './engine/encoding';
 
-import plugin = components.metadata.plugin;
-import config = components.metadata.config;
-import state = components.metadata.state;
-import action = components.metadata.action;
-import ConfigType = components.metadata.ConfigType;
-import PluginUsage = components.metadata.PluginUsage;
+import m = components.metadata;
 
-@plugin({ usage: PluginUsage.UI })
-@config({ name: 'ircConfigKey', type: ConfigType.STRING })
-@config({ name: 'ircComponent', type: ConfigType.STRING })
-export class UiButton {
+@m.plugin({ usage: m.PluginUsage.UI })
+@m.config({ name: 'configKey', type: m.ConfigType.STRING })
+@m.config({ name: 'ircComponent', type: m.ConfigType.STRING })
+export class UiButton extends BasePlugin {
+  constructor({ configKey, ircComponent }: { configKey: string; ircComponent: string; }) {
+    super(configKey);
+  }
 
-  @state
-  value: boolean;
+  @m.state
+  value: boolean = false;
 
-  @action
+  @m.action
   action(value: boolean) {
+    this.executeAction(encoding.writeBool(value));
+  }
 
+  protected onStateChanged(name: string, value: string) {
+    switch (name) {
+      case 'value':
+        this.value = encoding.readBool(value);
+        break;
+    }
   }
 }
+
