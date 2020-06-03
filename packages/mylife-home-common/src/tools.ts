0cde4132afad1e5abba95f27e271afd2448b67a5
vincent-tr <vincent.tr@gmail.com>
2020-06-03 09:15:59 +0200

    add config stuff

--- packages/mylife-home-common/src/tools.ts
+++ packages/mylife-home-common/src/tools.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import readConfig from 'read-config';
 import * as logger from './logger';
 
 const log = logger.createLogger('mylife:home:common:tools');
@@ -8,4 +10,25 @@ export function fireAsync(target: () => Promise<void>): void {
 
 export async function sleep(delay: number) {
   await new Promise(resolve => setTimeout(resolve, delay));
+}
+
+let cachedConfig: any;
+
+export function getConfig() {
+  if (!cachedConfig) {
+    cachedConfig = loadConfig();
+  }
+
+  return cachedConfig;
+}
+
+export function injectConfig(value: any) {
+  cachedConfig = value;
+}
+
+function loadConfig(): any {
+  // __dirname correspond to root directory in bundle
+  const configFile = path.join(__dirname, 'config.json');
+  log.debug(`reading configuration in '${configFile}'`);
+  return readConfig(configFile);
 }
