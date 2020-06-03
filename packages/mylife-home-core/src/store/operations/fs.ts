740b8d2fd5bb93b3799db6f9d07612766339d11b
vincent-tr <vincent.tr@gmail.com>
2020-06-03 20:34:34 +0200

    cleanup

--- packages/mylife-home-core/src/store/operations/fs.ts
+++ packages/mylife-home-core/src/store/operations/fs.ts
@@ -1,25 +1,26 @@
-import { StoreOperations, StoreItem, StoreConfiguration } from '../common';
+import path from 'path';
 import { promises as fs } from 'fs';
+import { StoreOperations, StoreItem, StoreConfiguration } from '../common';
 
 interface FsStoreConfiguration extends StoreConfiguration {
   readonly path: string;
 }
 
 export class FsStoreOperations implements StoreOperations {
-  private readonly path: string;
+  private readonly storeFile: string;
 
   constructor(configuration: StoreConfiguration) {
     const typedConfig = configuration as FsStoreConfiguration;
-    this.path = typedConfig.path;
+    this.storeFile = path.resolve(typedConfig.path);
   }
 
   async load() {
-    const content = await fs.readFile(this.path, 'utf-8');
+    const content = await fs.readFile(this.storeFile, 'utf-8');
     return JSON.parse(content) as StoreItem[];
   }
 
   async save(items: StoreItem[]) {
     const content = JSON.stringify(items);
-    await fs.writeFile(this.path, content);
+    await fs.writeFile(this.storeFile, content);
   }
 }
