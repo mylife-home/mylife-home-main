a1e05735f9c05a769123c4ea21b645488877a947
vincent-tr <vincent.tr@gmail.com>
2020-06-04 21:06:44 +0200

    add defines

--- packages/mylife-home-core/src/bin.ts
+++ packages/mylife-home-core/src/bin.ts
@@ -1,5 +1,8 @@
 import 'source-map-support/register';
+import { tools } from 'mylife-home-common';
 import { Manager } from './manager';
+
+tools.setDefine('main-component', 'core');
 
 const manager = new Manager();
 
