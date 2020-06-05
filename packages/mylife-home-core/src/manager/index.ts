4f699b4627ba5f4f823972c4e350d1eac9de316c
vincent-tr <vincent.tr@gmail.com>
2020-06-05 17:45:40 +0200

    cleanup config

--- packages/mylife-home-core/src/manager/index.ts
+++ packages/mylife-home-core/src/manager/index.ts
@@ -2,18 +2,14 @@ import os from 'os';
 import { bus, components, tools } from 'mylife-home-common';
 import { loadPlugins } from './plugin-loader';
 
-interface Config {
-  readonly listenRemote: boolean;
-}
-
 export class Manager {
   private readonly transport: bus.Transport;
   private readonly registry: components.Registry
 
   constructor() {
-    const config = tools.getConfig() as Config;
-    this.transport = new bus.Transport({ presenceTracking: config.listenRemote });
-    this.registry = new components.Registry({ transport: this.transport, publishRemoteComponents: config.listenRemote });
+    const listenRemote = tools.getConfigItem<boolean>('listenRemote');
+    this.transport = new bus.Transport({ presenceTracking: listenRemote });
+    this.registry = new components.Registry({ transport: this.transport, publishRemoteComponents: listenRemote });
     
     loadPlugins(this.registry);
   }
