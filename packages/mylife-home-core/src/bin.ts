c0487874da03e98f37b932e63ab449eff3e0bed8
vincent-tr <vincent.tr@gmail.com>
2020-06-05 17:46:55 +0200

    setup instance-name

--- packages/mylife-home-core/src/bin.ts
+++ packages/mylife-home-core/src/bin.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register';
 import { tools } from 'mylife-home-common';
 import { Manager } from './manager';
 
-tools.setDefine('main-component', 'core');
+tools.init('core');
 
 const manager = new Manager();
 
