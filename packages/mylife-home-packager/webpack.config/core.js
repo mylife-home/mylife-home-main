4466eb83e26c3c9d5286e871521c1546086edbfe
vincent-tr <vincent.tr@gmail.com>
2020-06-08 21:58:09 +0200

    adapt dll stuff

--- packages/mylife-home-packager/webpack.config/core.js
+++ packages/mylife-home-packager/webpack.config/core.js
@@ -9,14 +9,18 @@ module.exports = (paths) => {
   
   return [{
     entry: {
-      'core/lib': ['mylife-home-core', 'mylife-home-common'],
+      'core/lib': ['mylife-home-core', 'mylife-home-common', 'mylife-home-core/dist/bin'],
+    },
+    output: {
+      libraryTarget: 'commonjs2'
     },
     plugins: [
       new DllPlugin({ 
+        name: 'CoreLib',
         path: libManifest
       })
-    ]
-  }, {
+    ],
+   }, {
     entry: {
       'core/bin': 'mylife-home-core/dist/bin',
     },
@@ -24,20 +28,28 @@ module.exports = (paths) => {
       new WaitPlugin(libManifest),
       new DllReferencePlugin({
         context: paths.base,
-        manifest: libManifest
+        manifest: libManifest,
+        sourceType: 'commonjs2',
+        name: './lib',
       }),
     ]
   }, {
     entry: {
       'core/plugins/irc': 'mylife-home-core-plugins-irc',
     },
+    output: {
+      libraryTarget: 'commonjs2'
+    },
     plugins: [
       new WaitPlugin(libManifest),
       new DllReferencePlugin({
         context: paths.base,
-        manifest: libManifest
+        manifest: libManifest,
+        sourceType: 'commonjs2',
+        name: '../lib',
       }),
       new DllPlugin({ 
+        name: 'PluginsIrc',
         path: path.join(paths.output, 'core/plugins/irc.js.manifest')
       })
     ],
