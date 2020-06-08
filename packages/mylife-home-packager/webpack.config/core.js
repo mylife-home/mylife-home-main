06696b937019f7e30af0aaa830bf42c1569ba48a
vincent-tr <vincent.tr@gmail.com>
2020-06-08 20:30:03 +0200

    do not split too much

--- packages/mylife-home-packager/webpack.config/core.js
+++ packages/mylife-home-packager/webpack.config/core.js
@@ -1,10 +1,17 @@
 'use strict';
 
-module.exports = {
+module.exports = [{
   entry: {
     'core/bin': 'mylife-home-core/dist/bin',
   },
   output: {
     filename: '[name].js',
   },
-};
+}, {
+  entry: {
+    'core/plugins/irc': 'mylife-home-core-plugins-irc',
+  },
+  output: {
+    filename: '[name].js',
+  },
+}];
