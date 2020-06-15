5153128e81af969460383f73c4f474109d8e841a
vincent-tr <vincent.tr@gmail.com>
2020-06-15 16:42:35 +0200

    wip move to ts

--- packages/mylife-home-ui/public/app/middlewares/socket.ts
+++ packages/mylife-home-ui/public/app/middlewares/socket.ts
@@ -1,30 +1,31 @@
 'use strict';
 
+import { Middleware } from 'redux';
 import io from 'socket.io-client';
 import { actionTypes } from '../constants';
 import { socketConnect, socketDisconnect } from '../actions/online';
 import { repositoryState, repositoryAdd, repositoryRemove, repositoryChange } from '../actions/repository';
 
-const middleware = (/*store*/) => next => {
+export const socketMiddleware: Middleware = store => next => {
   const socket = io();
 
-  socket.on('connect',    () => next(socketConnect()));
+  socket.on('connect', () => next(socketConnect()));
   socket.on('disconnect', () => next(socketDisconnect()));
 
-  socket.on('state',  (data) => next(repositoryState(data)));
-  socket.on('add',    (data) => next(repositoryAdd(data)));
-  socket.on('remove', (data) => next(repositoryRemove(data)));
-  socket.on('change', (data) => next(repositoryChange(data)));
+  // FIXME: types
+  socket.on('state', (data: any) => next(repositoryState(data)));
+  socket.on('add', (data: any) => next(repositoryAdd(data)));
+  socket.on('remove', (data: any) => next(repositoryRemove(data)));
+  socket.on('change', (data: any) => next(repositoryChange(data)));
 
   return action => {
-    next(action);
 
     switch (action.type) {
       case actionTypes.ACTION_COMPONENT:
         socket.emit('action', action.payload);
         break;
     }
+
+    return next(action);
   };
 };
-
-export default middleware;
--- packages/mylife-home-ui/public/app/middlewares/viewport.ts
+++ packages/mylife-home-ui/public/app/middlewares/viewport.ts
@@ -6,17 +6,17 @@ import { getWindow } from '../selectors';
 import { isMobile } from '../utils/detect-browser';
 import { setDimensions } from '../utils/viewport';
 
-export function createViewportMiddleware() : Middleware {
+export function createViewportMiddleware(): Middleware {
   // nothing to do on desktop
-  if(!isMobile) {
-    return (store) => next => action => next(action);
+  if (!isMobile) {
+    return store => next => action => next(action);
   }
 
   return store => next => action => {
     switch (action.type) {
       case actionTypes.VIEW_CHANGE: {
         const state = store.getState();
-        const window = getWindow(state, { window : action.payload });
+        const window = getWindow(state, { window: action.payload });
         setDimensions(window.width, window.height);
       }
     }
