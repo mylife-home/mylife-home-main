8fa02fb71b76e56fb2a6568a61eea7749abc204e
vincent-tr <vincent.tr@gmail.com>
2020-06-15 15:48:10 +0200

    cleanup

--- packages/mylife-home-ui/public/app/middlewares/resources.js
+++ packages/mylife-home-ui/public/app/middlewares/resources.js
@@ -2,7 +2,6 @@
 
 import request from 'superagent';
 import { actionTypes } from '../constants';
-import { getLocation } from '../utils/location';
 import { resourceGet } from '../actions/resources';
 
 const middleware = (/*store*/) => next => action => {
@@ -11,7 +10,7 @@ const middleware = (/*store*/) => next => action => {
   switch (action.type) {
     case actionTypes.RESOURCE_QUERY:
       request
-        .get(getLocation(`/resources/get/${action.payload.resource}`))
+        .get(`/resources/get/${action.payload.resource}`)
         .end((err, res) => {
           if (err) {
             return next(resourceGet(err instanceof Error ? err : new Error(res)));
