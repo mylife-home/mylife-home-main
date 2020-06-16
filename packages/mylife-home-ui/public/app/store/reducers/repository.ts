6170c2e434369ca11698f4b19c7ddcda25946d2a
vincent-tr <vincent.tr@gmail.com>
2020-06-16 20:50:28 +0200

    move constants

--- packages/mylife-home-ui/public/app/store/reducers/repository.ts
+++ packages/mylife-home-ui/public/app/store/reducers/repository.ts
@@ -1,7 +1,6 @@
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
 import { Map } from 'immutable';
-import * as actionTypes from '../constants/action-types';
-import { RepositoryReset, RepositoryAdd, RepositoryRemove, RepositoryChange, Attributes, AttributesState } from '../types/repository';
+import { RepositoryReset, RepositoryAdd, RepositoryRemove, RepositoryChange, Attributes, AttributesState, REPOSITORY_RESET, REPOSITORY_ADD, REPOSITORY_REMOVE, REPOSITORY_CHANGE } from '../types/repository';
 
 function createAttributes(raw: Attributes): AttributesState {
   return Map<string, string>().withMutations((map) => {
@@ -12,14 +11,14 @@ function createAttributes(raw: Attributes): AttributesState {
 }
 
 export default createReducer(Map<string, Map<string, string>>(), {
-  [actionTypes.REPOSITORY_STATE]: (state, action: PayloadAction<RepositoryReset>) =>
+  [REPOSITORY_RESET]: (state, action: PayloadAction<RepositoryReset>) =>
     state.clear().withMutations((map) => {
       for (const id of Object.keys(action.payload)) {
         map.set(id, createAttributes(action.payload[id]));
       }
     }),
 
-  [actionTypes.REPOSITORY_ADD]: (state, action: PayloadAction<RepositoryAdd>) => state.set(action.payload.id, createAttributes(action.payload.attributes)),
-  [actionTypes.REPOSITORY_REMOVE]: (state, action: PayloadAction<RepositoryRemove>) => state.delete(action.payload.id),
-  [actionTypes.REPOSITORY_CHANGE]: (state, action: PayloadAction<RepositoryChange>) => state.update(action.payload.id, (val) => val.set(action.payload.name, action.payload.value)),
+  [REPOSITORY_ADD]: (state, action: PayloadAction<RepositoryAdd>) => state.set(action.payload.id, createAttributes(action.payload.attributes)),
+  [REPOSITORY_REMOVE]: (state, action: PayloadAction<RepositoryRemove>) => state.delete(action.payload.id),
+  [REPOSITORY_CHANGE]: (state, action: PayloadAction<RepositoryChange>) => state.update(action.payload.id, (val) => val.set(action.payload.name, action.payload.value)),
 });
--- packages/mylife-home-ui/public/app/store/types/repository.ts
+++ packages/mylife-home-ui/public/app/store/types/repository.ts
@@ -1,5 +1,10 @@
 import { Map } from 'immutable';
 
+export const REPOSITORY_RESET = 'repository/reset';
+export const REPOSITORY_ADD = 'repository/add';
+export const REPOSITORY_REMOVE = 'repository/remove';
+export const REPOSITORY_CHANGE = 'repository/change';
+
 export interface Attributes {
   [id: string]: string;
 }
