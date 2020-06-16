9bf070b0c644ca4576f8ecbecd545b99243e1cbc
vincent-tr <vincent.tr@gmail.com>
2020-06-16 20:40:37 +0200

    move to types

--- packages/mylife-home-ui/public/app/store/reducers/repository.ts
+++ packages/mylife-home-ui/public/app/store/reducers/repository.ts
@@ -2,12 +2,10 @@ import { createReducer, PayloadAction } from '@reduxjs/toolkit';
 import { Map } from 'immutable';
 import * as actionTypes from '../constants/action-types';
 import { RepositoryReset, RepositoryAdd, RepositoryRemove, RepositoryChange, Attributes } from '../actions/repository';
-
-export type AttributesState = Map<string, string>;
-export type RepositoryState = Map<string, AttributesState>;
+import { AttributesState } from '../types/repository';
 
 function createAttributes(raw: Attributes): AttributesState {
-  return Map<string, string>().withMutations(map => {
+  return Map<string, string>().withMutations((map) => {
     for (const name of Object.keys(raw)) {
       map.set(name, raw[name]);
     }
@@ -15,17 +13,14 @@ function createAttributes(raw: Attributes): AttributesState {
 }
 
 export default createReducer(Map<string, Map<string, string>>(), {
-
-  [actionTypes.REPOSITORY_STATE]: (state, action: PayloadAction<RepositoryReset>) => state.clear().withMutations(map => {
-    for (const id of Object.keys(action.payload)) {
-      map.set(id, createAttributes(action.payload[id]));
-    }
-  }),
+  [actionTypes.REPOSITORY_STATE]: (state, action: PayloadAction<RepositoryReset>) =>
+    state.clear().withMutations((map) => {
+      for (const id of Object.keys(action.payload)) {
+        map.set(id, createAttributes(action.payload[id]));
+      }
+    }),
 
   [actionTypes.REPOSITORY_ADD]: (state, action: PayloadAction<RepositoryAdd>) => state.set(action.payload.id, createAttributes(action.payload.attributes)),
-
   [actionTypes.REPOSITORY_REMOVE]: (state, action: PayloadAction<RepositoryRemove>) => state.delete(action.payload.id),
-
-  [actionTypes.REPOSITORY_CHANGE]: (state, action: PayloadAction<RepositoryChange>) => state.update(action.payload.id, (val) => val.set(action.payload.name, action.payload.value))
-
+  [actionTypes.REPOSITORY_CHANGE]: (state, action: PayloadAction<RepositoryChange>) => state.update(action.payload.id, (val) => val.set(action.payload.name, action.payload.value)),
 });
--- packages/mylife-home-ui/public/app/store/reducers/view.ts
+++ packages/mylife-home-ui/public/app/store/reducers/view.ts
@@ -1,13 +1,9 @@
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
-import * as actionTypes from '../constants/action-types';
 import { List } from 'immutable';
+import * as actionTypes from '../constants/action-types';
 
 export default createReducer(List<string>(), {
-
-  [actionTypes.VIEW_POPUP] : (state, action: PayloadAction<string>) => state.push(action.payload),
-
-  [actionTypes.VIEW_CLOSE] : (state, action) => state.pop(),
-
-  [actionTypes.VIEW_CHANGE] : (state, action: PayloadAction<string>) => state.clear().push(action.payload)
-
+  [actionTypes.VIEW_POPUP]: (state, action: PayloadAction<string>) => state.push(action.payload),
+  [actionTypes.VIEW_CLOSE]: (state, action) => state.pop(),
+  [actionTypes.VIEW_CHANGE]: (state, action: PayloadAction<string>) => state.clear().push(action.payload),
 });
