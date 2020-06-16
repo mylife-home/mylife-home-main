2de39a6c910c0af43291b06199bd994b8084ffce
vincent-tr <vincent.tr@gmail.com>
2020-06-16 17:07:58 +0200

    move to ts

---
+++ packages/mylife-home-ui/public/app/store/reducers/resources.ts
@@ -0,0 +1,10 @@
+import { createReducer, PayloadAction } from '@reduxjs/toolkit';
+import { Map } from 'immutable';
+import * as actionTypes from '../constants/action-types';
+import { ResourceGet } from '../actions/resources';
+
+export default createReducer(Map<string, any>(), {
+
+  [actionTypes.RESOURCE_GET] :  (state, action: PayloadAction<ResourceGet>) => state.set(action.payload.resource, action.payload.content)
+
+});
