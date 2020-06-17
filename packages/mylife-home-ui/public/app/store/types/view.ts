a040c99bfc9397ada7cc6be3ed272cabdbc2bc9b
vincent-tr <vincent.tr@gmail.com>
2020-06-17 10:17:37 +0200

    move view to ts

--- packages/mylife-home-ui/public/app/store/types/view.ts
+++ packages/mylife-home-ui/public/app/store/types/view.ts
@@ -1,3 +1,10 @@
+import { VWindow } from './windows';
+
 export const VIEW_POPUP = 'view/popup';
 export const VIEW_CLOSE = 'view/close';
 export const VIEW_CHANGE = 'view/change';
+
+export interface VView {
+  readonly main: VWindow,
+  readonly popups: VWindow[];
+}
