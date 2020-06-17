d130c0763942a972daa8d56982e2bd4b84d2dc1c
vincent-tr <vincent.tr@gmail.com>
2020-06-17 09:33:07 +0200

    move to ts

--- packages/mylife-home-ui/public/app/store/actions/view.ts
+++ packages/mylife-home-ui/public/app/store/actions/view.ts
@@ -2,34 +2,34 @@
 
 import { createAction } from '@reduxjs/toolkit';
 import { push as routerPush } from 'react-router-redux';
+import { AppThunkAction, AppState, AppThunkDispatch } from '../types';
 import { VIEW_POPUP, VIEW_CLOSE, VIEW_CHANGE } from '../types/view';
 import { getView } from '../selectors/view';
 import { resourceQuery } from './resources';
 import { windowLoad } from './windows';
-
 import { isMobile } from '../../utils/detect-browser';
 
-const internalViewPopup  = createAction(VIEW_POPUP);
+const internalViewPopup  = createAction<string>(VIEW_POPUP);
 const internalViewClose  = createAction(VIEW_CLOSE);
-const internalViewChange = createAction(VIEW_CHANGE);
+const internalViewChange = createAction<string>(VIEW_CHANGE);
 
-function getPathView(state) {
-  const routingState = state.routing.locationBeforeTransitions;
+function getPathView(state: AppState) {
+  const routingState = state.routing.location;
   if(!routingState) { return null; }
   let { pathname } = routingState;
   pathname = pathname.substr(1);
   return pathname;
 }
 
-function getDefaultView(dispatch, done) {
+function getDefaultView(dispatch: AppThunkDispatch, done: (err: Error, defaultWindow?: string) => void) {
   return dispatch(resourceQuery({ resource: 'default_window', done: (err, data) => {
-    if(err) { return done(err); } // eslint-disable-line no-console
+    if(err) { return done(err); }
     const windows = JSON.parse(data);
     return done(null, isMobile ? windows.mobile : windows.desktop);
   }}));
 }
 
-export const viewInit = () => (dispatch, getState) => {
+export const viewInit = (): AppThunkAction => (dispatch, getState) => {
   const state = getState();
   if(getPathView(state)) {
     console.log('window already opened, not using default window'); // eslint-disable-line no-console
@@ -44,31 +44,31 @@ export const viewInit = () => (dispatch, getState) => {
   });
 };
 
-export const viewChange = (id) => (dispatch, getState) => {
+export const viewChange = (id: string): AppThunkAction => (dispatch, getState) => {
   const pathname = `/${id}`;
   const state = getState();
-  const routingState = state.routing.locationBeforeTransitions;
+  const routingState = state.routing.location;
   if(routingState && routingState.pathname === pathname) {
     return dispatch(viewNavigationChange(id)); // cannot change to same path
   }
   dispatch(routerPush(pathname));
 };
 
-export const viewNavigationChange = (id) => (dispatch) => {
+export const viewNavigationChange = (id: string): AppThunkAction => (dispatch) => {
   return dispatch(windowLoad(id, (err) => {
     if(err) { return console.error(err); } // eslint-disable-line no-console
     return dispatch(internalViewChange(id));
   }));
 };
 
-export const viewPopup = (id) => (dispatch) => {
+export const viewPopup = (id: string): AppThunkAction => (dispatch) => {
   return dispatch(windowLoad(id, (err) => {
     if(err) { return console.error(err); } // eslint-disable-line no-console
     return dispatch(internalViewPopup(id));
   }));
 };
 
-export const viewClose = () => (dispatch, getState) => {
+export const viewClose = (): AppThunkAction => (dispatch, getState) => {
   const state = getState();
   const view = getView(state);
   if(view.size <= 1) {
