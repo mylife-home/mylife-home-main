5b32b036068de4efece16ba550171776f0c009fa
vincent-tr <vincent.tr@gmail.com>
2020-06-17 15:12:09 +0200

    client prod build

--- packages/mylife-home-ui/webpack.config.js
+++ packages/mylife-home-ui/webpack.config.js
@@ -5,63 +5,54 @@ const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR   = path.resolve(__dirname, 'public/app');
 
 const config = {
-  entry: APP_DIR + '/main.js',
+  mode: 'production',
+  devtool: 'nosources-source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+  
+  entry: [ 'babel-polyfill', APP_DIR + '/main' ],
+
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
+
+  resolve: {
+    extensions: ['.wasm', '.mjs', '.js', '.ts', '.tsx', '.json']
+  },
+
   module : {
-    loaders : [
+    rules : [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel',
-        query: {
-          presets:[ 'es2015', 'react', 'stage-2' ]
-        }
+        test : /\.ts(x?)$/,
+        use : [{ loader : 'babel-loader', query : babelQuery }, 'ts-loader' ]
+      },
+      {
+        test : /\.js$/,
+        use : [{ loader : 'babel-loader', query : babelQuery }]
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        use: [ 'style-loader', 'css-loader' ]
       },
       {
         test: /\.less$/,
-        loader: "style!css!less"
+        use: [ 'style-loader', 'css-loader', 'less-loader' ]
       },
       {
-        test: /\.png$/,
-        loader: "url-loader?limit=100000"
-      },
-      {
-        test: /\.jpg$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
+        test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf|ico)$/,
+        use: [ 'file-loader' ]
       }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
 };
 
 module.exports = config;
