2297240545a539c2739e115bf6a30e7b56bae80b
vincent-tr <vincent.tr@gmail.com>
2020-06-17 10:11:59 +0200

    move windowcontent to ts

---
+++ packages/mylife-home-ui/public/app/components/window-content.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from 'react';
+import { VWindow } from '../store/types/windows';
+import Control from './control';
+
+type WindowContentProps = {
+  window: VWindow;
+  onActionPrimary: (window: string, control: string) => void;
+  onActionSecondary: (window: string, control: string) => void;
+};
+
+function getStyleSize(window: VWindow) {
+  const { height, width } = window;
+  return { height, width };
+}
+
+const WindowContent: FunctionComponent<WindowContentProps> = ({ window, onActionPrimary, onActionSecondary }) => (
+  <div style={getStyleSize(window)} className="mylife-window-container">
+    <img src={window.resource && `data:image/png;base64,${window.resource}`} />
+    {window.controls.map((control) => (
+      <Control
+        key={control.id}
+        control={control}
+        onActionPrimary={() => onActionPrimary(window.id, control.id)}
+        onActionSecondary={() => onActionSecondary(window.id, control.id)}
+      />
+    ))}
+  </div>
+);
+
+export default WindowContent;
