e0377e92a85c06f46353dac5f82ad5e6144d39b1
vincent-tr <vincent.tr@gmail.com>
2020-06-19 15:35:00 +0200

    cleanup

--- packages/mylife-home-ui/lib/server.js
+++ packages/mylife-home-ui/lib/server.js
@@ -6,31 +6,23 @@ const net          = require('./net');
 const web          = require('./web');
 
 class Session extends EventEmitter {
-  constructor(id, socket, netConfig, webConfig, netRepository) {
+  constructor(socket, netRepository) {
     super();
 
-    this._headerIp        = webConfig.headerIp;
     this._terminating     = false;
     this._socketConnected = true;
-    this._id              = id;
     this._socket          = socket;
-    this._net             = new net.Client(netConfig, this._createNick());
     this._netRepository   = netRepository;
     const self            = this;
 
     this._socket.on('disconnect', () => self._net.close(() => self.emit('close')));
-    this._socket.on('action', (data) => this._net.action(data.id, data.name, data.args));
+    this._socket.on('action', (data) => this._netRepository.action(data.id, data.name, data.args));
 
     this._netRepository.on('add', (id, obj) => self._socket.emit('add', { id: id, attributes: self._objAttributes(obj) }));
     this._netRepository.on('remove', (id) => self._socket.emit('remove', { id: id }));
     this._netRepository.on('change', (id, name, value) => self._socket.emit('change', { id: id, name: name, value: value }));
 
     this._sendState();
-  }
-
-  _createNick() {
-    return 'ui-session_' + this._id + '_' +
-      this.remoteAddress.replace(/\./g, '-').replace(/:/g, '-');
   }
 
   _objAttributes(obj) {
@@ -48,16 +40,6 @@ class Session extends EventEmitter {
       data[id] = this._objAttributes(obj);
     }
     this._socket.emit('state', data);
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  get remoteAddress() {
-    const header = this._socket.request.headers[this._headerIp];
-    if(header) { return header; }
-    return this._socket.request.connection.remoteAddress;
   }
 
   kill(cb) {
@@ -80,7 +62,7 @@ module.exports = class {
   _createSession(socket) {
     const self = this;
     const id = (++this._idGenerator).toString();
-    const session = new Session(id, socket, this._netConfig, this._webConfig, this._netRepository);
+    const session = new Session(socket, this._netRepository);
     this._sessions.set(id, session);
     session.on('close', () => self._sessions.delete(id));
   }
@@ -94,7 +76,7 @@ module.exports = class {
       (cb) => this._webServer.close(cb),
       (cb) => this._netAgent.close(cb)
     ];
-    for(let session of this._sessions.values()) {
+    for(const session of this._sessions.values()) {
       array.push(this._closeSession(session));
     }
 
