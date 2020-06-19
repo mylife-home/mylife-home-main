b7259a1ace7b80703be376625cf1d5c5cef78f33
vincent-tr <vincent.tr@gmail.com>
2020-06-19 21:27:50 +0200

    move to ts

--- packages/mylife-home-ui/bin/server.ts
+++ packages/mylife-home-ui/bin/server.ts
@@ -1,12 +1,12 @@
-'use strict';
-
-const path   = require('path');
-const Server = require('../lib/server').default;
+import path from 'path';
+import Server from '../lib/server';
 
 const dev = process.argv.includes('--dev');
 
 let configFile = path.join(__dirname, '../conf/config.json');
-if(process.argv[dev ? 3 : 2]) { configFile = process.argv[2]; }
+if (process.argv[dev ? 3 : 2]) {
+  configFile = process.argv[2];
+}
 configFile = path.resolve(configFile);
 const config = require(configFile);
 
