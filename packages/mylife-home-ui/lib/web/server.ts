18038176002eaadd626cd9e7417ac3dfe1ff9243
vincent-tr <vincent.tr@gmail.com>
2020-06-22 17:44:15 +0200

    remove dev flag

--- packages/mylife-home-ui/lib/web/server.ts
+++ packages/mylife-home-ui/lib/web/server.ts
@@ -8,16 +8,14 @@ import bodyParser from 'body-parser';
 import favicon from 'serve-favicon';
 import serveStatic from 'serve-static';
 import webpack, { Configuration } from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
 import { createRepository } from './repository';
 import { createResources } from './resources';
-import webpackConfig from '../../webpack.config.dev.js';
 import { Repository } from '../net';
 
 export default class WebServer extends EventEmitter {
   private _server: http.Server;
 
-  constructor(netRepository: Repository, sessionCreator: (socket: io.Socket) => void, webConfig: { port: number }, dev: boolean) {
+  constructor(netRepository: Repository, sessionCreator: (socket: io.Socket) => void, webConfig: { port: number }) {
     super();
     const app = express();
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,11 +23,6 @@ export default class WebServer extends EventEmitter {
 
     const publicDirectory = path.resolve(path.join(__dirname, '../../public'));
 
-    if (dev) {
-      console.log('setup webpack dev middleware'); // eslint-disable-line no-console
-      const typedConfig = webpackConfig as Configuration;
-      app.use(webpackMiddleware(webpack(typedConfig), { publicPath: typedConfig.output.publicPath }));
-    }
     app.use(favicon(path.join(publicDirectory, 'images/favicon.ico')));
     app.use('/repository', createRepository(netRepository));
     app.use('/resources', createResources());
