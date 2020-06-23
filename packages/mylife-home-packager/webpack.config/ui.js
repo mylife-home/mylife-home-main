c6c2061bfce0f02f02270a2c75376b1018919e31
vincent-tr <vincent.tr@gmail.com>
2020-06-23 18:14:31 +0200

    wip client config

--- packages/mylife-home-packager/webpack.config/ui.js
+++ packages/mylife-home-packager/webpack.config/ui.js
@@ -1,9 +1,26 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = (paths) => {
-  return [{
+  return [(env, argv) => {
+    const config = getClientConfig(env.mode);
+    config.output.path = path.join(paths.output, 'static');
+    return config;
+  }, {
     entry: {
       'ui/bin': 'mylife-home-ui/dist/bin',
     }
   }];
 };
+
+function getClientConfig(mode) {
+  switch(mode) {
+    case 'dev':
+      return require('mylife-home-ui/webpack.config.dev');
+    case 'prod':
+      return require('mylife-home-ui/webpack.config.prod');
+    default:
+      throw new Error(`Unsupported mode: '${mode}'`);
+  }
+}
