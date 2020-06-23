adee30f39bbe432951225d1e9ba095ae32061ba9
vincent-tr <vincent.tr@gmail.com>
2020-06-23 17:54:24 +0200

    cleanup

--- packages/mylife-home-packager/webpack.config/ui.js
+++ packages/mylife-home-packager/webpack.config/ui.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const path = require('path');
-const { DllPlugin, DllReferencePlugin } = require('webpack');
-const WaitPlugin = require('./wait-plugin');
-
 module.exports = (paths) => {
   return [{
     entry: {
