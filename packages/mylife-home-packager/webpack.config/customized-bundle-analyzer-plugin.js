ade972c7bb5100669d0f487d3635465935874b76
vincent-tr <vincent.tr@gmail.com>
2020-06-24 18:11:30 +0200

    try to customize properly

--- packages/mylife-home-packager/webpack.config/customized-bundle-analyzer-plugin.js
+++ packages/mylife-home-packager/webpack.config/customized-bundle-analyzer-plugin.js
@@ -4,7 +4,23 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = class CustomizedBundleAnalyzerPlugin extends BundleAnalyzerPlugin {
   apply(compiler) {
-    this.opts.reportFilename = Object.keys(compiler.options.entry)[0] + '.report.html';
+    const name = Object.keys(compiler.options.entry)[0];
+    this.opts.reportFilename = `${name}.report.html`;
+    this.opts.reportTitle = `${name} [${currentTime()}]`;
+    
     return super.apply(compiler);
   }
 };
+
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function currentTime() {
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = MONTHS[time.getMonth()];
+  const day = time.getDate();
+  const hour = `0${time.getHours()}`.slice(-2);
+  const minute = `0${time.getMinutes()}`.slice(-2);
+
+  return `${day} ${month} ${year} at ${hour}:${minute}`;
+}
