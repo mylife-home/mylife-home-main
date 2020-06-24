4efc34c0db6248c9527e801ea3265b656d7544c9
vincent-tr <vincent.tr@gmail.com>
2020-06-24 15:45:24 +0200

    wip move webpack stuff from ui to packager

--- packages/mylife-home-packager/webpack.config/ui.js
+++ packages/mylife-home-packager/webpack.config/ui.js
@@ -1,26 +1,100 @@
 'use strict';
 
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = (paths) => {
-  return [(env, argv) => {
-    const config = getClientConfig(env.mode);
-    config.output.path = path.join(paths.output, 'ui/static');
-    return config;
-  }, {
-    entry: {
-      'ui/bin': 'mylife-home-ui/dist/bin',
-    }
-  }];
+  return [
+    (env, argv) => createClientConfiguration(env, argv, paths),
+    {
+      entry: {
+        'ui/bin': 'mylife-home-ui/dist/bin',
+      },
+    },
+  ];
 };
 
-function getClientConfig(mode) {
-  switch(mode) {
-    case 'dev':
-      return require('mylife-home-ui/webpack.config.dev');
-    case 'prod':
-      return require('mylife-home-ui/webpack.config.prod');
-    default:
-      throw new Error(`Unsupported mode: '${mode}'`);
+function createClientConfiguration(env, argv, paths) {
+  const babelOptions = {
+    presets: [
+      [require.resolve('@babel/preset-env'), { targets: 'last 2 versions' }],
+      require.resolve('@babel/preset-react'),
+    ]
+  };
+
+  const base = {
+    entry: 'mylife-home-ui/public/app/main',
+
+    output: {
+      path: path.join(paths.output, 'ui/static'),
+      filename: 'bundle.js',
+    },
+
+    resolve: {
+      extensions: ['.wasm', '.mjs', '.js', '.ts', '.tsx', '.json'],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          use: [
+            { loader: 'babel-loader', options: babelOptions },
+            { loader: 'ts-loader', options: { configFile: require.resolve('mylife-home-ui/public/tsconfig.json') } },
+          ],
+        },
+        { test: /\.js$/, use: [{ loader: 'babel-loader', options: babelOptions }] },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+        { test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf|ico)$/, use: ['file-loader'] },
+      ],
+    },
+  };
+
+  const modes = {
+    dev: {
+      ...base,
+      mode: 'development',
+      devtool: 'inline-source-map',
+      /*
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      host: '0.0.0.0',
+      port: 8002,
+      disableHostCheck: true,
+      proxy: {
+        '/resources': {
+          target: 'http://localhost:8001'
+        },
+        '/socket.io': {
+          target: 'http://localhost:8001',
+          ws: true,
+        }
+      }
+    }
+    */
+    },
+    prod: {
+      ...base,
+      mode: 'production',
+      devtool: 'nosources-source-map',
+      optimization: {
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              keep_classnames: true,
+              keep_fnames: true,
+            },
+          }),
+        ],
+      },
+    },
+  };
+
+  const config = modes[env.mode];
+  if (!config) {
+    throw new Error(`Unsupported mode: '${env.mode}`);
   }
+
+  return config;
 }
