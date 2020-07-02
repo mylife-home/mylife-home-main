11f6af90f0d432c192bc3ef3057767da8f00dc06
vincent-tr <vincent.tr@gmail.com>
2020-07-02 09:39:32 +0200

    make webpack compiler "persistent"

--- packages/mylife-home-packager/gulp.config/webpack-build.ts
+++ packages/mylife-home-packager/gulp.config/webpack-build.ts
@@ -1,46 +1,45 @@
-import webpack, { Configuration } from 'webpack';
+import webpack, { Configuration, MultiCompiler } from 'webpack';
 import configFactory from '../webpack.config';
 
 export type Binary = string;
 export type Mode = 'prod' | 'dev';
 
-export function createWebpackTask(binary: Binary, mode: Mode) {
-  const config = configFactory({ binary, mode });
-  const task = async () => {
-    await compile(config);
-  };
+export class WebpackBuild {
+  private readonly compiler: MultiCompiler;
 
-  Object.assign(task, { displayName: `webpack-build - ${binary} (${mode})` });
+  constructor(binary: Binary, mode: Mode) {
+    const config = configFactory({ binary, mode });
+    this.compiler = webpack(config);
+    Object.assign(this.task, { displayName: `webpack-build - ${binary} (${mode})` });
+  }
 
-  return task;
-}
-
-async function compile(config: Configuration[]) {
-  return new Promise<void>((resolve, reject) => {
-    webpack(config, (err, stats) => {
-      if (err) {
-        return reject(err);
-      }
-
-      if (stats.hasErrors()) {
-        const errors: string[] = [];
-        for (const { compilation } of stats.stats) {
-          for (const error of compilation.errors) {
-            errors.push(error.toString());
+  readonly task = async () => {
+    return new Promise<void>((resolve, reject) => {
+      this.compiler.run((err, stats) => {
+        if (err) {
+          return reject(err);
+        }
+  
+        if (stats.hasErrors()) {
+          const errors: string[] = [];
+          for (const { compilation } of stats.stats) {
+            for (const error of compilation.errors) {
+              errors.push(error.toString());
+            }
+          }
+          return reject(new Error(errors.join('\n')));
+        }
+  
+        if (stats.hasWarnings) {
+          for (const { compilation } of stats.stats) {
+            for (const warning of compilation.warnings) {
+              console.log(warning.toString());
+            }
           }
         }
-        return reject(new Error(errors.join('\n')));
-      }
-
-      if (stats.hasWarnings) {
-        for (const { compilation } of stats.stats) {
-          for (const warning of compilation.warnings) {
-            console.log(warning.toString());
-          }
-        }
-      }
-
-      resolve();
+  
+        resolve();
+      });
     });
-  });
+  };
 }
