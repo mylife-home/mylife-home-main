f311c5c2c93095488d9686b95a8aebd52dcf5b21
vincent-tr <vincent.tr@gmail.com>
2020-07-02 16:45:39 +0200

    naming

--- packages/mylife-home-packager/gulp.config/ts-build.ts
+++ packages/mylife-home-packager/gulp.config/ts-build.ts
@@ -6,24 +6,24 @@ import ts from 'gulp-typescript';
 import merge from 'merge2';
 
 export class TsBuild {
-  private readonly modulePath: string;
+  private readonly packagePath: string;
   private readonly project: ts.Project;
 
-  constructor(moduleName: string) {
-    this.modulePath = path.dirname(require.resolve(`${moduleName}/package.json`));
-    this.project = ts.createProject(path.join(this.modulePath, 'tsconfig.json'));
-    Object.assign(this.task, { displayName: `ts-build - ${moduleName}` });
+  constructor(packageName: string) {
+    this.packagePath = path.dirname(require.resolve(`${packageName}/package.json`));
+    this.project = ts.createProject(path.join(this.packagePath, 'tsconfig.json'));
+    Object.assign(this.task, { displayName: `ts-build - ${packageName}` });
   }
 
   get globs() {
-    return this.project.config.include.map(item => path.join(this.modulePath, item));
+    return this.project.config.include.map(item => path.join(this.packagePath, item));
   }
 
   readonly task = () => {
 
     const result = this.project.src().pipe(this.project());
 
-    const outputPath = path.join(this.modulePath, 'dist');
+    const outputPath = path.join(this.packagePath, 'dist');
     return merge([
       result.dts.pipe(gulp.dest(outputPath)),
       result.js.pipe(gulp.dest(outputPath))
