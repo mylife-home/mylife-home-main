346d4bf0430c809df9261bcb419205d1d287a1d4
vincent-tr <vincent.tr@gmail.com>
2020-07-03 16:02:58 +0200

    publish configuration by names

--- packages/mylife-home-packager/webpack.config/types.ts
+++ packages/mylife-home-packager/webpack.config/types.ts
@@ -2,5 +2,5 @@ import { Configuration } from 'webpack';
 import { Context } from './context';
 
 export type ConfigurationFactory = (context: Context) => Configuration;
-
 export type ConfigurationByMode = { [mode: string]: Configuration; };
+export type ConfigurationFile = { [part: string]: ConfigurationFactory};
