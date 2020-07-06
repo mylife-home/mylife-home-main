2acdd6504e63f3a7f4e90c722a70bbd0c9512871
vincent-tr <vincent.tr@gmail.com>
2020-07-06 09:13:45 +0200

    split files

---
+++ packages/mylife-home-packager/gulp.config/definitions.ts
@@ -0,0 +1,64 @@
+import path from 'path';
+import { TsBuild } from './ts-build';
+import { WebpackBuild } from './webpack-build';
+
+export const projects = {
+  common : {
+    ts: new TsBuild('mylife-home-common')
+  },
+
+  ui : {
+    ts: new TsBuild('mylife-home-ui'),
+    bin: {
+      dev: new WebpackBuild('ui', 'bin', 'dev'),
+      prod: new WebpackBuild('ui', 'bin', 'prod'),
+    },
+    client: {
+      dev: new WebpackBuild('ui', 'client', 'dev'),
+      prod: new WebpackBuild('ui', 'client', 'prod'),
+    }
+  },
+
+  core : {
+    ts: new TsBuild('mylife-home-core'),
+    lib: {
+      dev: new WebpackBuild('core', 'lib', 'dev'),
+      prod: new WebpackBuild('core', 'lib', 'prod'),
+    },
+    bin: {
+      dev: new WebpackBuild('core', 'bin', 'dev'),
+      prod: new WebpackBuild('core', 'bin', 'prod'),
+    },
+    plugins: {
+      irc: {
+        ts: new TsBuild('mylife-home-core-plugins-irc'),
+        dev: new WebpackBuild('core', 'plugins-irc', 'dev'),
+        prod: new WebpackBuild('core', 'plugins-irc', 'prod'),
+      }
+      // other plugins
+    }
+  }
+};
+
+export const globs = {
+  dist: {
+    all: pathAsGlobs('dist'),
+    dev: pathAsGlobs('dist/dev'),
+    prod: pathAsGlobs('dist/prod'),
+  },
+  common: projects.common.ts.globs,
+  ui: {
+    bin: projects.ui.ts.globs,
+    client: packagePublicGlobs('mylife-home-ui')
+  }
+};
+
+function pathAsGlobs(part: string) {
+  const basePath = path.resolve(path.join(__dirname, '..'));
+  return [path.join(basePath, `${part}/**`)];
+}
+
+function packagePublicGlobs(packageName: string) {
+  const packagePath = path.dirname(require.resolve(`${packageName}/package.json`));
+  return [path.join(packagePath, 'public/**')];
+}
--- packages/mylife-home-packager/gulp.config/index.ts
+++ packages/mylife-home-packager/gulp.config/index.ts
@@ -1,59 +1,7 @@
-import path from 'path';
 import { series, parallel, watch } from 'gulp';
 import del from 'del';
-import { TsBuild } from './ts-build';
-import { WebpackBuild } from './webpack-build';
-
-const projects = {
-  common : {
-    ts: new TsBuild('mylife-home-common')
-  },
-
-  ui : {
-    ts: new TsBuild('mylife-home-ui'),
-    bin: {
-      dev: new WebpackBuild('ui', 'bin', 'dev'),
-      prod: new WebpackBuild('ui', 'bin', 'prod'),
-    },
-    client: {
-      dev: new WebpackBuild('ui', 'client', 'dev'),
-      prod: new WebpackBuild('ui', 'client', 'prod'),
-    }
-  },
-
-  core : {
-    ts: new TsBuild('mylife-home-core'),
-    lib: {
-      dev: new WebpackBuild('core', 'lib', 'dev'),
-      prod: new WebpackBuild('core', 'lib', 'prod'),
-    },
-    bin: {
-      dev: new WebpackBuild('core', 'bin', 'dev'),
-      prod: new WebpackBuild('core', 'bin', 'prod'),
-    },
-    plugins: {
-      irc: {
-        ts: new TsBuild('mylife-home-core-plugins-irc'),
-        dev: new WebpackBuild('core', 'plugins-irc', 'dev'),
-        prod: new WebpackBuild('core', 'plugins-irc', 'prod'),
-      }
-      // other plugins
-    }
-  }
-};
-
-const globs = {
-  dist: {
-    all: pathAsGlobs('dist'),
-    dev: pathAsGlobs('dist/dev'),
-    prod: pathAsGlobs('dist/prod'),
-  },
-  common: projects.common.ts.globs,
-  ui: {
-    bin: projects.ui.ts.globs,
-    client: packagePublicGlobs('mylife-home-ui')
-  }
-};
+import { argv } from 'yargs';
+import { projects, globs } from './definitions';
 
 const buildProd = parallel(
   projects.ui.client.prod.task,
@@ -119,13 +67,3 @@ export = {
 
   'build:prod': buildProd,
 };
-
-function pathAsGlobs(part: string) {
-  const basePath = path.resolve(path.join(__dirname, '..'));
-  return [path.join(basePath, `${part}/**`)];
-}
-
-function packagePublicGlobs(packageName: string) {
-  const packagePath = path.dirname(require.resolve(`${packageName}/package.json`));
-  return [path.join(packagePath, 'public/**')];
-}
