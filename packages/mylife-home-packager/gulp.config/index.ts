234dda807f2deb8410d5f886846c25a51066ebaa
vincent-tr <vincent.tr@gmail.com>
2020-07-06 09:53:24 +0200

    build dev core

--- packages/mylife-home-packager/gulp.config/index.ts
+++ packages/mylife-home-packager/gulp.config/index.ts
@@ -2,6 +2,8 @@ import { series, parallel, watch } from 'gulp';
 import del from 'del';
 import { argv } from 'yargs';
 import { projects, globs } from './definitions';
+import { TsBuild } from './ts-build';
+import { WebpackBuild } from './webpack-build';
 
 const buildProd = parallel(
   projects.ui.client.prod.task,
@@ -13,20 +15,19 @@ const buildProd = parallel(
         projects.ui.bin.prod.task
       ),
       series(
-        projects.core.ts.task, 
+        projects.core.ts.task,
         projects.core.lib.prod.task,
         projects.core.bin.prod.task,
-        parallel(
-          series(
-            projects.core.plugins.irc.ts.task,
-            projects.core.plugins.irc.prod.task
-          )
-          // other plugins
-        )
+        createProdCorePluginsTask()
       )
     )
   )
 );
+
+function createProdCorePluginsTask() {
+  const list = Object.values(projects.core.plugins).map(plugin => series(plugin.ts.task, plugin.prod.task));
+  return parallel(...list);
+}
 
 const buildDevUi = parallel(
   projects.ui.client.dev.task,
@@ -53,6 +54,37 @@ const watchUi = series(
   }
 );
 
+const buildDevCore = series(
+  projects.common.ts.task,
+  projects.core.ts.task,
+  projects.core.lib.prod.task,
+  projects.core.bin.prod.task,
+  createDevCorePluginsTask()
+);
+
+function createDevCorePluginsTask() {
+
+  type CorePluginDefinition = {
+    readonly ts: TsBuild;
+    readonly dev: WebpackBuild;
+    readonly prod: WebpackBuild;
+  };
+  
+  type CorePluginsDefinition = {
+    [name: string]: CorePluginDefinition
+  }
+  
+  const names = corePluginsListNames();
+  const plugins = projects.core.plugins as CorePluginsDefinition;
+
+  const list = names.map(name => {
+    const plugin = plugins[name];
+    return series(plugin.ts.task, plugin.prod.task);
+  });
+
+  return list.length ? parallel(...list) : noop;
+}
+
 export = {
   'clean': () => del(globs.dist.all),
   'clean:prod': () => del(globs.dist.prod),
@@ -62,8 +94,49 @@ export = {
   'build:dev:ui': buildDevUi,
   'watch:ui': watchUi,
 
-  'build:dev:core': null, // TODO
+  'build:dev:core': buildDevCore,
   'watch:core': null, // TODO
 
   'build:prod': buildProd,
 };
+
+function corePluginsListNames() {
+  const userPlugins = argv.plugins as string;
+  const noPlugin = argv.plugin === false;
+  const allPlugin = argv.allPlugins === true;
+  const list = new Set(Object.keys(projects.core.plugins));
+
+  let setCount = 0;
+  if (userPlugins) {
+    ++setCount;
+  }
+  if (noPlugin) {
+    ++setCount;
+  }
+  if (allPlugin) {
+    ++setCount;
+  }
+
+  if (setCount !== 1) {
+    throw new Error(`You must specify one of: '--no-plugin', '--all-plugins', '--plugins=plug1,plug2'`);
+  }
+
+  if (noPlugin) {
+    return [];
+  }
+
+  if (allPlugin) {
+    return Array.from(list);
+  }
+
+  const userList = userPlugins.split(',').map(item => item.trim());
+  for (const name of userList) {
+    if (!list.has(name)) {
+      throw new Error(`Unknown plugin '${name}'`);
+    }
+  }
+  return userList;
+}
+
+async function noop() {    
+}
