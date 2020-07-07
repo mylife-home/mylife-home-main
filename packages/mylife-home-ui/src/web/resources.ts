add4242972ca418f474b180a056b1c67f45dea2c
vincent-tr <vincent.tr@gmail.com>
2020-07-07 17:29:04 +0200

    merge files

--- packages/mylife-home-ui/src/web/resources.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { model } from '../model';
-
-export function createResources() {
-  const router = express.Router();
-  router.route('/enum').get((req, res) => res.json(Object.keys(model)));
-  router.route('/get/:key').get((req, res) => res.json(model[req.params.key]));
-  return router;
-};
--- packages/mylife-home-ui/src/web/server.ts
+++ packages/mylife-home-ui/src/web/server.ts
@@ -7,13 +7,12 @@ import bodyParser from 'body-parser';
 import favicon from 'serve-favicon';
 import serveStatic from 'serve-static';
 import { components } from 'mylife-home-common';
-import { createRepository } from './repository';
-import { createResources } from './resources';
+import { model } from '../model';
 
 export default class WebServer {
   private _server: http.Server;
 
-  constructor(registry: components.Registry, sessionCreator: (socket: io.Socket) => void, webConfig: { port: number, staticDirectory: string }) {
+  constructor(registry: components.Registry, sessionCreator: (socket: io.Socket) => void, webConfig: { port: number, staticDirectory: string; }) {
     const app = express();
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
@@ -36,4 +35,38 @@ export default class WebServer {
     this._server.close(callback);
     this._server.destroy();
   }
+}
+
+function createRepository(registry: components.Registry) {
+
+  const router = express.Router();
+
+  router.route('/action/:componentId/:actionName').get((req, res) => {
+    const { componentId, actionName } = req.params;
+    const component = registry.getComponent(componentId);
+    component.executeAction(actionName, true);
+    component.executeAction(actionName, false);
+    return res.status(200);
+  });
+
+  router.route('/components').get((req, res) => {
+    const result = Array.from(registry.getComponents()).map(component => component.id);
+    return res.json(result);
+  });
+
+  router.route('/state/:componentId').get((req, res) => {
+    const { componentId } = req.params;
+    const component = registry.getComponent(componentId);
+    const result = component.getStates();
+    return res.json(result);
+  });
+
+  return router;
+}
+
+function createResources() {
+  const router = express.Router();
+  router.route('/enum').get((req, res) => res.json(Object.keys(model)));
+  router.route('/get/:key').get((req, res) => res.json(model[req.params.key]));
+  return router;
 }
