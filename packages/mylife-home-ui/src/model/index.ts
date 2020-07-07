7e920f3314aaf6a94859bf7f68125fe458bcb92c
vincent-tr <vincent.tr@gmail.com>
2020-07-07 17:09:21 +0200

    adapt model

--- packages/mylife-home-ui/src/model/index.ts
+++ packages/mylife-home-ui/src/model/index.ts
@@ -8,6 +8,7 @@ export type Model = { [id: string]: string; };
 export const model: Model = {};
 
 for (const window of raw.Windows) {
+  prepareWindow(window);
   model[`window.${window.id}`] = JSON.stringify({ window });
 }
 
@@ -19,3 +20,40 @@ model.default_window = JSON.stringify({
   desktop: raw.DesktopDefaultWindow,
   mobile: raw.MobileDefaultWindow,
 });
+
+function prepareWindow(window: typeof raw.Windows[0]) {
+  for (const control of window.controls) {
+    // replace ids comp_id with comp-id
+    replaceId(control.primary_action?.component, 'component_id');
+    replaceId(control.secondary_action?.component, 'component_id');
+    replaceId(control.display, 'component_id');
+
+    const context = control.text?.context;
+    if (context) {
+      for (const item of context) {
+        replaceId(item, 'component_id');
+      }
+    }
+
+    // replace 'off' 'on' with true false
+    const map = control.display?.map;
+    if (map) {
+      for (const item of map) {
+        switch (item.value) {
+          case 'on':
+            item.value = true;
+            break;
+          case 'off':
+            item.value = false;
+            break;
+        }
+      }
+    }
+  }
+}
+
+function replaceId(obj: { [prop: string]: string; }, prop: string) {
+  if (obj && obj[prop]) {
+    obj[prop] = obj[prop].replace(/_/g, '-');
+  }
+}
