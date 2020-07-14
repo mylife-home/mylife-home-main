c501092c0ee57874ce24008c4bc29147852577ed
vincent-tr <vincent.tr@gmail.com>
2020-07-14 17:13:38 +0200

    wip use model as state

--- packages/mylife-home-ui/public/app/store/reducers/model.ts
+++ packages/mylife-home-ui/public/app/store/reducers/model.ts
@@ -1,60 +1,8 @@
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
-import { Map, List } from 'immutable';
-import { ControlText, Control, Window, MODEL_SET } from '../types/model';
-import * as shared from '../../../../shared/model';
+import { MODEL_SET, Window } from '../types/model';
 
-export default createReducer(Map<string, Window>(), {
-  [MODEL_SET]: (state, action: PayloadAction<shared.Window[]>) => {
-    return state.withMutations(map => {
-      map.clear();
-      for (const rawWindow of action.payload) {
-        const window = createWindow(rawWindow);
-        map.set(window.id, window);
-      }
-    });
-  }
+const DEFAULT_MODEL: Window[] = [];
+
+export default createReducer(DEFAULT_MODEL, {
+  [MODEL_SET]: (state, action: PayloadAction<Window[]>) => action.payload,
 });
-
-function createWindow(raw: shared.Window): Window {
-  const { controls, ...others } = raw;
-  return {
-    controls: Map(
-      controls.map((item) => {
-        const ctrl = createControl(item);
-        return [ctrl.id, ctrl];
-      })
-    ),
-    ...others,
-  };
-}
-
-function createControl(raw: shared.Control): Control {
-  const { text, ...others } = raw;
-  return {
-    text: createText(text),
-    ...others,
-  };
-}
-
-function createText(raw: shared.ControlText): ControlText {
-  if (!raw) {
-    return null;
-  }
-  const { context, format, ...others } = raw;
-
-  const argNames = context.map((item) => item.id).join(',');
-  let func: (args: string[]) => string;
-  try {
-    func = new Function(argNames, format) as (args: string[]) => string;
-  } catch (err) {
-    console.error(err); // eslint-disable-line no-console
-    func = () => err.message;
-  }
-
-  return {
-    context: List(context),
-    format,
-    func,
-    ...others,
-  };
-}
