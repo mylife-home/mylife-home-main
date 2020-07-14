86da380dd095c8f9849366fce720db66b9680b83
vincent-tr <vincent.tr@gmail.com>
2020-07-14 17:42:31 +0200

    immutable cleanup

--- packages/mylife-home-ui/public/app/store/reducers/registry.ts
+++ packages/mylife-home-ui/public/app/store/reducers/registry.ts
@@ -1,25 +1,24 @@
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
-import { Map } from 'immutable';
-import { Reset, ComponentAdd, ComponentRemove, StateChange, ComponentStates } from '../../../../shared/registry';
-import { AttributesState, REGISTRY_RESET, REGISTRY_COMPONENT_ADD, REGISTRY_COMPONENT_REMOVE, REGISTRY_ATTRIBUTE_CHANGE } from '../types/registry';
+import { Reset, ComponentAdd, ComponentRemove, StateChange } from '../../../../shared/registry';
+import { REGISTRY_RESET, REGISTRY_COMPONENT_ADD, REGISTRY_COMPONENT_REMOVE, REGISTRY_ATTRIBUTE_CHANGE, RepositoryState } from '../types/registry';
 
-function createAttributes(raw: ComponentStates): AttributesState {
-  return Map<string, any>().withMutations((map) => {
-    for (const name of Object.keys(raw)) {
-      map.set(name, raw[name]);
+const DEFAULT: RepositoryState = {};
+
+export default createReducer(DEFAULT, {
+  [REGISTRY_RESET]: (state, action: PayloadAction<Reset>) => action.payload,
+  [REGISTRY_COMPONENT_ADD]: (state, action: PayloadAction<ComponentAdd>) => ({ ...state, [action.payload.id]: action.payload.attributes }),
+  [REGISTRY_COMPONENT_REMOVE]: (state, action: PayloadAction<ComponentRemove>) => deleteObjectKey(state, action.payload.id),
+  [REGISTRY_ATTRIBUTE_CHANGE]: (state, action: PayloadAction<StateChange>) => ({
+    ...state,
+    [action.payload.id]: {
+      ...state[action.payload.id],
+      [action.payload.name]: action.payload.value
     }
-  });
-}
-
-export default createReducer(Map<string, Map<string, any>>(), {
-  [REGISTRY_RESET]: (state, action: PayloadAction<Reset>) =>
-    state.clear().withMutations((map) => {
-      for (const id of Object.keys(action.payload)) {
-        map.set(id, createAttributes(action.payload[id]));
-      }
-    }),
-
-  [REGISTRY_COMPONENT_ADD]: (state, action: PayloadAction<ComponentAdd>) => state.set(action.payload.id, createAttributes(action.payload.attributes)),
-  [REGISTRY_COMPONENT_REMOVE]: (state, action: PayloadAction<ComponentRemove>) => state.delete(action.payload.id),
-  [REGISTRY_ATTRIBUTE_CHANGE]: (state, action: PayloadAction<StateChange>) => state.update(action.payload.id, (val) => val.set(action.payload.name, action.payload.value)),
+  }),
 });
+
+function deleteObjectKey<T>(obj: {[id: string]: T}, key: string) :  {[id: string]: T} {
+  const { [key]: removed, ...others} = obj;
+  void removed;
+  return others;
+}
