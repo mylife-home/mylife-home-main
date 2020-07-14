228605ef80b000b7ae7b6f7f4021d59420d10bde
vincent-tr <vincent.tr@gmail.com>
2020-07-14 18:28:26 +0200

    use selectors

--- packages/mylife-home-ui/public/app/store/selectors/model.ts
+++ packages/mylife-home-ui/public/app/store/selectors/model.ts
@@ -1,5 +1,33 @@
+import { createSelector } from 'reselect';
 import { AppState } from '../types';
+import { Window, Control } from '../types/model';
 
 export const getWindows = (state: AppState) => state.model;
-export const getWindow = (state: AppState, { window }: { window: string }) => getWindows(state).find((w) => w.id === window); // TODO: index
-export const getWindowControl = (state: AppState, { window, control }: { window: string; control: string }) => getWindow(state, { window }).controls.find((c) => c.id === control); // TODO: index
+export const getWindow = (state: AppState, { window }: { window: string }) => getWindowById(state)(window);
+export const getWindowControl = (state: AppState, { window, control }: { window: string; control: string }) => getWindowControlById(state)(window, control);
+
+const getWindowById = createSelector(getWindows, (windows) => {
+  const windowsById: { [id: string]: Window } = {};
+  for (const window of windows) {
+    windowsById[window.id] = window;
+  }
+
+  return (windowId: string) => windowsById[windowId];
+});
+
+const getWindowControlById = createSelector(getWindows, (windows) => {
+  const createKey = (windowId: string, controlId: string) => windowId + '$' + controlId;
+  const controlsById: { [id: string]: Control } = {};
+
+  for (const window of windows) {
+    for (const control of window.controls) {
+      const id = createKey(window.id, control.id);
+      controlsById[id] = control;
+    }
+  }
+
+  return (windowId: string, controlId: string) => {
+    const id = createKey(windowId, controlId);
+    return controlsById[id];
+  };
+});
