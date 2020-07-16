9be1fa8ff1d371f84e866774c1fad010e8a4e7ac
vincent-tr <vincent.tr@gmail.com>
2020-07-16 16:47:56 +0200

    only use redux-logger in dev build

--- packages/mylife-home-ui/public/app/store/store.ts
+++ packages/mylife-home-ui/public/app/store/store.ts
@@ -1,6 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 
 import { socketMiddleware } from './middlewares/socket';
 import { resourcesMiddleware } from './middlewares/resources';
@@ -8,7 +7,14 @@ import { navigationMiddleware } from './middlewares/navigation';
 import { createViewportMiddleware } from './middlewares/viewport';
 import reducer from './reducers/index';
 
+const middlewares = [createViewportMiddleware(), navigationMiddleware, socketMiddleware, resourcesMiddleware, thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 export const store = createStore(
   reducer,
-  applyMiddleware(createViewportMiddleware(), navigationMiddleware, socketMiddleware, resourcesMiddleware, thunk, createLogger())
+  applyMiddleware(...middlewares)
 );
