48e04d98506c01b30653395d96b4ae95489f6c45
vincent-tr <vincent.tr@gmail.com>
2020-07-25 18:04:52 +0200

    studio skeleton

---
+++ packages/mylife-home-studio/src/bin.ts
@@ -0,0 +1,28 @@
+import 'source-map-support/register';
+import { tools } from 'mylife-home-common';
+import { Manager } from './manager';
+
+tools.init('studio');
+
+const manager = new Manager();
+
+init();
+
+process.on('SIGINT', terminate);
+process.on('SIGTERM', terminate);
+
+async function init() {
+  try {
+    await manager.init();
+  } catch(err) {
+    console.error('init error', err);
+  }
+}
+
+async function terminate() {
+  try {
+    await manager.terminate();
+  } catch(err) {
+    console.error('terminate error', err);
+  }
+}
