528a8d33742193e3c04b4b239bddeaa1da820557
vincent-tr <vincent@stoic.com>
2020-08-03 14:17:56 +0200

    wip mui

--- packages/mylife-home-studio/public/app/components/application.tsx
+++ packages/mylife-home-studio/public/app/components/application.tsx
@@ -1,7 +1,40 @@
 import React, { FunctionComponent } from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core';
+
+import blue from '@material-ui/core/colors/blue';
+import pink from '@material-ui/core/colors/pink';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+});
 
 const Application: FunctionComponent = () => (
-  <div>Application</div>
+  <ThemeProvider theme={theme}>
+
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+
+  </ThemeProvider>
 );
 
 export default Application;
