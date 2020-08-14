7a0ae53b50dd2a9d6c3e8c56ff63a507bf4e5b03
vincent-tr <vincent@stoic.com>
2020-08-14 16:14:16 +0200

    canvas image

--- packages/mylife-home-studio/public/app/components/core-designer/kcanvas.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/kcanvas.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, Fragment } from 'react';
 import { darken, useTheme, ThemeProvider } from '@material-ui/core/styles';
-import { Stage, Layer, Rect, Text, Group } from 'react-konva';
+import { Stage, Layer, Rect, Text, Group, Path } from 'react-konva';
 
 const GRID_STEP = 24;
 const COMPONENT_WIDTH = 10;
@@ -8,21 +8,46 @@ const SELECTION_WIDTH = 2;
 const FONT_FAMILY = '"Roboto", "Helvetica", "Arial", sans-serif';
 const TEXT_PADDING = 8;
 
+interface PathImageProps {
+  path: string;
+  x: number;
+  y: number;
+  size: number;
+}
+
+const PATH_SIZE = 24;
+
+const VISIBILITY_PATH = "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z";
+const INPUT_PATH = "M21 3.01H3c-1.1 0-2 .9-2 2V9h2V4.99h18v14.03H3V15H1v4.01c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98v-14c0-1.11-.9-2-2-2zM11 16l4-4-4-4v3H1v2h10v3z";
+
+const PathImage: FunctionComponent<PathImageProps> = ({ path, x, y, size }) => (
+  <Path
+    x={x}
+    y={y}
+    width={size}
+    height={size}
+    scaleX={size / PATH_SIZE}
+    scaleY={size / PATH_SIZE}
+    data={path}
+    fill={'black'}
+  />
+);
+
 interface LabelProps {
   x: number;
   y: number;
   height: number;
   width: number;
   text: string;
-  fontStyle?: string;
+  header?: boolean;
   backgroundColor: string;
 }
 
-const Label: FunctionComponent<LabelProps> =  ({x, y, height, width, text, fontStyle, backgroundColor }) => (
+const Label: FunctionComponent<LabelProps> = ({x, y, height, width, text, header, backgroundColor }) => (
   <>
     <Rect x={x} y={y} width={width} height={height} fill={backgroundColor} />
-    <Text x={x + TEXT_PADDING} y={y} width={width} height={height} 
-      text={text} fill={'black'} fontFamily={FONT_FAMILY} fontSize={GRID_STEP * 0.6} fontStyle={fontStyle}
+    <Text x={x + TEXT_PADDING + (header ? 0 : GRID_STEP) } y={y} width={width} height={height} 
+      text={text} fill={'black'} fontFamily={FONT_FAMILY} fontSize={GRID_STEP * 0.6} fontStyle={header && 'bold'}
       verticalAlign={'middle'} />
   </>
 );
@@ -67,14 +92,20 @@ export const KComponent: FunctionComponent<ComponentProps> = ({ x, y, title, sta
 
       <Rect x={0} y={0} width={width} height={height} fill={borderColor} />
 
-      <Label x={1} y={1} width={width-2} height={GRID_STEP - 2} backgroundColor={backgroundColor} fontStyle={'bold'} text={title} />
+      <Label x={1} y={1} width={width-2} height={GRID_STEP - 2} backgroundColor={backgroundColor} header text={title} />
 
       {states.map((state, index) => (
-        <Label key={index} x={1} y={GRID_STEP * (index + 1)} width={width-2} height={GRID_STEP - (index === states.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={state} />
+        <Fragment key={index}>
+          <Label x={1} y={GRID_STEP * (index + 1)} width={width-2} height={GRID_STEP - (index === states.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={state} />
+          <PathImage x={TEXT_PADDING} y={GRID_STEP * (index + 1) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} path={VISIBILITY_PATH} />
+        </Fragment>
       ))}
 
       {actions.map((action, index) => (
-        <Label key={index} x={1} y={GRID_STEP * (index + 1 + states.length)} width={width-2} height={GRID_STEP - (index === actions.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={action} />
+        <Fragment key={index}>
+          <Label x={1} y={GRID_STEP * (index + 1 + states.length)} width={width-2} height={GRID_STEP - (index === actions.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={action} />
+          <PathImage x={TEXT_PADDING} y={GRID_STEP * (index + 1 + states.length) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} path={INPUT_PATH} />
+        </Fragment>
       ))}
 
     </Group>
