5958db2cfe1dc61c3ee29548af561bad9364dabc
vincent-tr <vincent.tr@gmail.com>
2020-08-15 16:07:48 +0200

    split code

---
+++ packages/mylife-home-studio/public/app/components/core-designer/component/icon.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import { Path } from 'react-konva';
+
+const PATH_SIZE = 24;
+
+export type Image = 'visibility' | 'input';
+
+const images = {
+  visibility: "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z",
+  input: "M21 3.01H3c-1.1 0-2 .9-2 2V9h2V4.99h18v14.03H3V15H1v4.01c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98v-14c0-1.11-.9-2-2-2zM11 16l4-4-4-4v3H1v2h10v3z",
+};
+
+export interface IconProps {
+  image: Image;
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+}
+
+const Icon: FunctionComponent<IconProps> = ({ image, x, y, size, color }) => (
+  <Path
+    x={x}
+    y={y}
+    width={size}
+    height={size}
+    scaleX={size / PATH_SIZE}
+    scaleY={size / PATH_SIZE}
+    data={images[image]}
+    fill={color}
+  />
+);
+
+export default Icon;
--- packages/mylife-home-studio/public/app/components/core-designer/component/index.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/component/index.tsx
@@ -1,37 +1,14 @@
 import React, { FunctionComponent, Fragment } from 'react';
-import { darken, useTheme, ThemeProvider } from '@material-ui/core/styles';
-import { Stage, Layer, Rect, Text, Group, Path } from 'react-konva';
+import { darken, useTheme } from '@material-ui/core/styles';
+import { Rect, Text, Group, Path } from 'react-konva';
+
+import Icon from './icon';
 
 const GRID_STEP = 24;
 const COMPONENT_WIDTH = 10;
 const SELECTION_WIDTH = 2;
 const FONT_FAMILY = '"Roboto", "Helvetica", "Arial", sans-serif';
 const TEXT_PADDING = 8;
-
-interface PathImageProps {
-  path: string;
-  x: number;
-  y: number;
-  size: number;
-}
-
-const PATH_SIZE = 24;
-
-const VISIBILITY_PATH = "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z";
-const INPUT_PATH = "M21 3.01H3c-1.1 0-2 .9-2 2V9h2V4.99h18v14.03H3V15H1v4.01c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98v-14c0-1.11-.9-2-2-2zM11 16l4-4-4-4v3H1v2h10v3z";
-
-const PathImage: FunctionComponent<PathImageProps> = ({ path, x, y, size }) => (
-  <Path
-    x={x}
-    y={y}
-    width={size}
-    height={size}
-    scaleX={size / PATH_SIZE}
-    scaleY={size / PATH_SIZE}
-    data={path}
-    fill={'black'}
-  />
-);
 
 interface LabelProps {
   x: number;
@@ -97,14 +74,14 @@ const Component: FunctionComponent<ComponentProps> = ({ x, y, title, states, act
       {states.map((state, index) => (
         <Fragment key={index}>
           <Label x={1} y={GRID_STEP * (index + 1)} width={width-2} height={GRID_STEP - (index === states.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={state} />
-          <PathImage x={TEXT_PADDING} y={GRID_STEP * (index + 1) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} path={VISIBILITY_PATH} />
+          <Icon x={TEXT_PADDING} y={GRID_STEP * (index + 1) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} image='visibility' color='black' />
         </Fragment>
       ))}
 
       {actions.map((action, index) => (
         <Fragment key={index}>
           <Label x={1} y={GRID_STEP * (index + 1 + states.length)} width={width-2} height={GRID_STEP - (index === actions.length - 1 ? 1 : 0)} backgroundColor={backgroundColor} text={action} />
-          <PathImage x={TEXT_PADDING} y={GRID_STEP * (index + 1 + states.length) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} path={INPUT_PATH} />
+          <Icon x={TEXT_PADDING} y={GRID_STEP * (index + 1 + states.length) + ((GRID_STEP - (GRID_STEP * 0.6)) / 2)} size={GRID_STEP * 0.6} image='input' color='black' />
         </Fragment>
       ))}
 
