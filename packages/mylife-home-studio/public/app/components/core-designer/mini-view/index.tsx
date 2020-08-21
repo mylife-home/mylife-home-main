81c1ecfed54d653a7487f558b420848e81a4bf14
vincent-tr <vincent.tr@gmail.com>
2020-08-21 18:23:38 +0200

    wip miniview

--- packages/mylife-home-studio/public/app/components/core-designer/mini-view/index.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/mini-view/index.tsx
@@ -5,6 +5,7 @@ import Konva from 'konva';
 
 import SquareBox from './square-box';
 import Canvas from './canvas';
+import { Rect } from 'react-konva';
 
 export interface MiniViewProps {
   className?: string;
@@ -23,6 +24,7 @@ const MiniView: FunctionComponent<MiniViewProps> = ({ className, components }) =
   return (
     <SquareBox adjust='height' className={clsx(classes.container, className)}>
       <Canvas onSizeChange={(size) => console.log('onSizeChange', size)}>
+        <Rect x={0} y={0} fill={'blue'} width={10000} height={10000} />
         {/*
         {components.map((component, index) => (
           <Component key={index} {...component} selected={index === selectedIndex} onSelect={() => setSelectedIndex(index)} onMove={(pos: Konva.Vector2d) => handleMoveComponent(component.id, pos)} />  
