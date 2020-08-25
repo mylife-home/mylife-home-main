1d195042c330f0005774f3dd2ec2e299f30b2adf
vincent-tr <vincent.tr@gmail.com>
2020-08-25 10:02:38 +0200

    wip schema

--- packages/mylife-home-studio/public/app/files/schema.ts
+++ packages/mylife-home-studio/public/app/files/schema.ts
@@ -1,8 +1,26 @@
-import garden1 from './content/hw_rpi-home-garden1.json';
+import rawHWGarden1 from './content/hw_rpi-home-garden1.json';
+import rawHWSockets1 from './content/hw_rpi-home-sockets1.json';
+import rawHWGarden4 from './content/hw_rpi0-home-garden4.json';
+import rawHWCore from './content/hw_rpi2-home-core.json';
+import rawHWEPanel1 from './content/hw_rpi2-home-epanel1.json';
+import rawHWEPanel2 from './content/hw_rpi2-home-epanel2.json';
+import rawHWGarden2 from './content/hw_rpi2-home-garden2.json';
+import rawHWHallbox from './content/hw_rpi2-home-hallbox.json';
+import rawVPanelCore from './content/vpanel_rpi2-home-core.json';
 
-import core from './content/vpanel_rpi2-home-core.json';
+export const hwGarden1 = processFile(rawHWGarden1);
+export const hwSockets1 = processFile(rawHWSockets1);
+export const hwGarden4 = processFile(rawHWGarden4);
+export const hwCore = processFile(rawHWCore);
+export const hwEPanel1 = processFile(rawHWEPanel1);
+export const hwEPanel2 = processFile(rawHWEPanel2);
+export const hwGarden2 = processFile(rawHWGarden2);
+export const hwHallbox = processFile(rawHWHallbox);
+export const vpanelCore = processFile(rawVPanelCore);
 
-console.log(core);
+function processFile(file: File) {
+  return file;
+}
 
 interface File {
   readonly Name: string;
