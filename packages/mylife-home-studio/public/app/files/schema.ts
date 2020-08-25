3712e5e59ddce41600d35aaf616db42632ed3be3
vincent-tr <vincent.tr@gmail.com>
2020-08-25 09:58:00 +0200

    wip schema

--- packages/mylife-home-studio/public/app/files/schema.ts
+++ packages/mylife-home-studio/public/app/files/schema.ts
@@ -2,6 +2,56 @@ import garden1 from './content/hw_rpi-home-garden1.json';
 
 import core from './content/vpanel_rpi2-home-core.json';
 
-//core.Components[0].Component.
-
 console.log(core);
+
+interface File {
+  readonly Name: string;
+  readonly CreationDate: string;
+  readonly LastUpdate: string;
+  readonly Components: RawComponentContainer[];
+  readonly Toolbox: RawToolboxItem[];
+}
+
+interface RawComponentContainer {
+  readonly EntityName: string;
+  readonly Component: RawComponent;
+}
+
+interface RawComponent {
+  readonly id: string;
+  readonly library: string;
+  readonly type: string;
+  readonly bindings: RawComponentBinding[];
+  readonly config: RawComponentConfig[];
+  readonly designer: RawComponentDesigner[];
+}
+
+interface RawComponentBinding {
+  readonly local_action: string;
+  readonly remote_attribute: string;
+  readonly remote_id: string;
+}
+
+interface RawComponentConfig {
+  readonly Key: string;
+  readonly Value: string;
+}
+
+interface RawComponentDesigner {
+  readonly Key: string;
+  readonly Value: string;
+}
+
+interface RawToolboxItem {
+  readonly EntityName: string;
+  readonly Plugins: RawPlugin[];
+}
+
+interface RawPlugin {
+  readonly clazz: string;
+  readonly config: string;
+  readonly library: string;
+  readonly type: string;
+  readonly usage: number;
+  readonly version: string;
+}
