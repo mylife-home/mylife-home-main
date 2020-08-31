9d7302aae5bbe52ddf8bf936ffef1f8a9836be4b
vincent-tr <vincent.tr@gmail.com>
2020-08-31 11:45:31 +0200

    wip sidebar layout

--- packages/mylife-home-studio/public/app/components/core-designer/index.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/index.tsx
@@ -1,7 +1,11 @@
-import React, { FunctionComponent, useState, useMemo } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Divider from '@material-ui/core/Divider';
 
 import { Selection } from './types';
 import { ViewInfoProvider } from './drawing/view-info';
@@ -33,25 +37,51 @@ const initialBindings = schema.vpanelCore.bindings;
 }];
 */
 
+const useStyles = makeStyles((theme) => ({
+  tab: {
+    minWidth: 0,
+  },
+  miniViewContainer: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+  }
+}));
+
 const CoreDesigner: FunctionComponent = () => {
+  const classes = useStyles();
   const [selection, setSelection] = useState<Selection>(null);
   const [components, setComponents] = useState(initialComponents);
   const [bindings, setBindings] = useState(initialBindings);
+  const [sideBarTab, setSideBarTab] = useState('selection');
 
   return (
     <CanvasThemeProvider>
       <ViewInfoProvider>
-        <SplitPane split="vertical" defaultSize={300}>
+        <SplitPane split="vertical" defaultSize={300} minSize={300}>
 
-          <Box p={3}>
-            <Box display='flex' flexDirection='column'>
+          <Box display='flex' flexDirection='column'>
 
+            <div className={classes.miniViewContainer}>
               <MiniView components={components} selection={selection} />
               <ZoomSlider />
+            </div>
 
+            <Divider />
+
+            <Tabs value={sideBarTab} onChange={(e, value) => setSideBarTab(value)} textColor='primary' indicatorColor='primary' variant='fullWidth'>
+              <Tab classes={{root: classes.tab }} label='Sélection' value='selection' />
+              <Tab classes={{root: classes.tab }} label='Boîte à outils' value='toolbox' />
+            </Tabs>
+
+            <div role='tabpanel' hidden={sideBarTab !== 'selection'}>
               <Typography>Selection</Typography>
+            </div>
+
+            <div role='tabpanel' hidden={sideBarTab !== 'toolbox'}>
               <Typography>Toolbox</Typography>
-            </Box>
+            </div>
+
           </Box>
 
           <MainView
