9a1fd27cb34b687c42d3a7fecf226b0fd8006287
vincent-tr <vincent.tr@gmail.com>
2020-08-31 12:08:51 +0200

    wip binding

--- packages/mylife-home-studio/public/app/components/core-designer/selection-panel/binding.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/selection-panel/binding.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import { Selection } from '../types';
 
@@ -12,8 +13,26 @@ interface BindingProps {
 }
 
 const Binding: FunctionComponent<BindingProps> = ({ binding, setSelection }) => {
+  const handleSelectCenter = () => {
+    console.log('handleSelectCenter')
+  };
+
+  const handleSelectSource = () => {
+    setSelection({ type: 'component', index: 0 });
+  };
+
+  const handleSelectTarget = () => {
+    setSelection({ type: 'component', index: 0 });
+  };
+
   return (
-    <Typography>Selection {binding.sourceComponent} - {binding.targetComponent}</Typography>
+    <div>
+      <Button onClick={handleSelectCenter}>Binding</Button>
+      <Button onClick={handleSelectSource}>{binding.sourceComponent}</Button>
+      <Typography>{binding.sourceState}</Typography>
+      <Button onClick={handleSelectTarget}>{binding.targetComponent}</Button>
+      <Typography>{binding.targetAction}</Typography>
+    </div>
   );
 };
 
