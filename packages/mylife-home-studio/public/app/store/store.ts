629175ab1601b2e8782b5809d376dd017cf44508
vincent-tr <vincent.tr@gmail.com>
2020-09-15 09:54:00 +0200

    remove redux-thunk and add redux-observable

--- packages/mylife-home-studio/public/app/store/store.ts
+++ packages/mylife-home-studio/public/app/store/store.ts
@@ -1,10 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 
 import { socketMiddleware } from './middlewares/socket';
 import reducer from './reducer';
 
-const middlewares = [socketMiddleware, thunk];
+const middlewares = [socketMiddleware];
 
 if (process.env.NODE_ENV !== 'production') {
   const { createLogger } = require('redux-logger');
--- packages/mylife-home-studio/public/app/store/types.ts
+++ packages/mylife-home-studio/public/app/store/types.ts
@@ -1,8 +1,4 @@
 import { Action } from 'redux';
-import { ThunkDispatch, ThunkAction } from 'redux-thunk';
 import reducer from './reducer';
 
 export type AppState = ReturnType<typeof reducer>;
-
-export type AppThunkDispatch = ThunkDispatch<AppState, void, Action>;
-export type AppThunkAction<R = void> = ThunkAction<R, AppState, void, Action>;
