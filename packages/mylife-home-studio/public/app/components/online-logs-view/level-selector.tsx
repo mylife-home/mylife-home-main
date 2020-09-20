1c8f59975b6a09723024673ea33a482a8389e0af
vincent-tr <vincent.tr@gmail.com>
2020-09-20 18:47:36 +0200

    level selector wip

--- packages/mylife-home-studio/public/app/components/online-logs-view/level-selector.tsx
+++ packages/mylife-home-studio/public/app/components/online-logs-view/level-selector.tsx
@@ -6,12 +6,45 @@ import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
-import Button from '@material-ui/core/Button';
+import Button, { ButtonProps } from '@material-ui/core/Button';
 import Popper from '@material-ui/core/Popper';
 import Paper from '@material-ui/core/Paper';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
-const LevelSelector: FunctionComponent<{ min: number, max: number, set: (min: number, max: number) => void }> = ({ min, max, set }) => {
+type LevelSelectorButtonProps = Omit<ButtonProps, 'children'> & { min: number, max: number };
+
+const LevelSelectorButton: FunctionComponent<LevelSelectorButtonProps> = ({ min, max, ...props }) => {
+
+  return (
+    <Button {...props}>
+      {`${min} - ${max}`}
+    </Button>
+  );
+};
+
+type LevelSelectorProps = Omit<ButtonProps, 'onClick' | 'children'> & { min: number, max: number, set: (min: number, max: number) => void };
+
+interface LevelSelectorPopupProps {
+  min: number;
+  max: number;
+  set: (min: number, max: number) => void;
+  onClose: () => void;
+  anchorEl: HTMLElement;
+}
+
+const LevelSelectorPopup: FunctionComponent<LevelSelectorPopupProps> = ({ min, max, set, onClose, anchorEl }) => {
+  return (
+    <Popper open={!!anchorEl} anchorEl={anchorEl}>
+      <ClickAwayListener onClickAway={onClose}>
+        <Paper>
+          TODO
+        </Paper>
+      </ClickAwayListener>
+    </Popper>
+  );
+};
+
+const LevelSelector: FunctionComponent<LevelSelectorProps> = ({ min, max, set, ...props }) => {
 
   const [anchorEl, setAnchorEl] = useState<HTMLElement>(null);
 
@@ -25,17 +58,9 @@ const LevelSelector: FunctionComponent<{ min: number, max: number, set: (min: nu
 
   return (
     <>
-      <Popper open={!!anchorEl} anchorEl={anchorEl}>
-        <ClickAwayListener onClickAway={handleClickAway}>
-          <Paper>
-            TODO
-          </Paper>
-        </ClickAwayListener>
-      </Popper>
+      <LevelSelectorPopup min={min} max={max} set={set} anchorEl={anchorEl} onClose={handleClickAway} />
 
-      <Button onClick={handleClick}>
-        {`${min} - ${max}`}
-      </Button>
+      <LevelSelectorButton min={min} max={max} {...props} onClick={handleClick} />
     </>
   );
 };
