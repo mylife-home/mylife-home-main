55f58c9007014eb0e715325c387036448fd27fb0
vincent-tr <vincent.tr@gmail.com>
2020-09-21 11:19:10 +0200

    cleanup

--- packages/mylife-home-studio/public/app/components/online-logs-view/criteria.tsx
+++ packages/mylife-home-studio/public/app/components/online-logs-view/criteria.tsx
@@ -4,10 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Button from '@material-ui/core/Button';
 
-import { addLogItems } from '../../store/online-logs-view/actions';
-import { useActions } from '../lib/use-actions';
 import DebouncedTextField from '../lib/debounced-text-field';
 import LevelSelector from './level-selector';
 
@@ -57,7 +54,6 @@ interface CriteriaProps {
 
 const Criteria: FunctionComponent<CriteriaProps> = ({ className, criteria, setCriteria }) => {
   const classes = useStyles();
-  const insertTests = useTestLogsInsert();
 
   const [name, setName] = useTextValue(criteria, setCriteria, 'name');
   const [instance, setInstance] = useTextValue(criteria, setCriteria, 'instance');
@@ -68,7 +64,6 @@ const Criteria: FunctionComponent<CriteriaProps> = ({ className, criteria, setCr
 
   return (
     <div className={clsx(classes.container, className)}>
-      <Button onClick={insertTests}>Insert tests</Button>
       <DebouncedTextField label='Nom' className={classes.name} value={name} onChange={setName} />
       <DebouncedTextField label='Instance' className={classes.instance} value={instance} onChange={setInstance} />
       <DebouncedTextField label='Message' className={classes.message} value={message} onChange={setMessage} />
@@ -130,70 +125,4 @@ function computeNewMax(prevMax: number, newMin: number) {
 
 function computeNewMin(prevMin: number, newMax: number) {
   return prevMin === null || newMax === null || prevMin <= newMax ? prevMin : newMax;
-}
-
-
-// name with wildcards
-// instanceName
-// msg contains
-// !!err
-// level min-max
-
-let testIndex = 0;
-
-const stack = `at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/gulp.config/webpack-build.ts:29:25
-at finalCallback (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:354:32)
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:414:15
-at Hook.eval [as callAsync] (eval at create (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:411:23
-at Compiler.emitRecords (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:699:39)
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:403:11
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:681:14
-at Hook.eval [as callAsync] (eval at create (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:678:27
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/neo-async/async.js:2818:7
-at done (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/neo-async/async.js:3522:9)
-at Hook.eval [as callAsync] (eval at create (/home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/webpack/lib/Compiler.js:562:33
-at /home/vincent/workspace/mylife-home-main/packages/mylife-home-packager/node_modules/graceful-fs/graceful-fs.js:136:16
-at /home/vincent/.node_modules/lib/node_modules/gulp-cli/node_modules/graceful-fs/graceful-fs.js:61:14`;
-
-function useConnect() {
-  return useActions({ addLogItems });
-}
-
-function useTestLogsInsert() {
-  const { addLogItems } = useConnect();
-
-  return () => {
-    addLogItems([
-      { id: `test-${++testIndex}`, name: 'test:test', instanceName: 'test-instance', hostname: 'test-host', pid: 42, level: 3, msg: 'test level 3', time: new Date(), err: null },
-      { id: `test-${++testIndex}`, name: 'test:test', instanceName: 'test-instance', hostname: 'test-host', pid: 42, level: 10, msg: 'test trace', time: new Date(), err: null },
-      { id: `test-${++testIndex}`, name: 'test:test', instanceName: 'test-instance', hostname: 'test-host', pid: 42, level: 20, msg: 'test debug', time: new Date(), err: null },
-      { id: `test-${++testIndex}`, name: 'test:test', instanceName: 'test-instance', hostname: 'test-host', pid: 42, level: 30, msg: 'test info', time: new Date(), err: null },
-      { id: `test-${++testIndex}`, name: 'test:test', instanceName: 'test-instance', hostname: 'test-host', pid: 42, level: 40, msg: 'test warn', time: new Date(), err: null },
-      {
-        id: `test-${++testIndex}`,
-        name: 'test:test',
-        instanceName: 'test-instance',
-        hostname: 'test-host',
-        pid: 42,
-        level: 50,
-        msg: 'test error',
-        time: new Date(),
-        err: { message: 'test error', name: 'ErrorType', stack },
-      },
-      {
-        id: `test-${++testIndex}`,
-        name: 'test:test',
-        instanceName: 'test-instance',
-        hostname: 'test-host',
-        pid: 42,
-        level: 60,
-        msg: 'test fatal',
-        time: new Date(),
-        err: { message: 'test fatal', name: 'ErrorType', stack },
-      },
-    ]);
-  };
 }
