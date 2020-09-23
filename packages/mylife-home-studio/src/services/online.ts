8667c5e533fefb14d67cf9f657074a5b897c2ec2
vincent-tr <vincent.tr@gmail.com>
2020-09-23 13:06:03 +0200

    reuse notifiers stuff

--- packages/mylife-home-studio/src/services/online.ts
+++ packages/mylife-home-studio/src/services/online.ts
@@ -1,49 +1,86 @@
-import { logger } from 'mylife-home-common';
+import { logger, bus, tools } from 'mylife-home-common';
 import { Service, BuildParams } from './types';
 import { Services } from '.';
-import { Session, SessionNotifier, SessionFeature } from './session-manager';
+import { Session, SessionNotifierManager } from './session-manager';
 
 const log = logger.createLogger('mylife:home:studio:services:online');
-/*
-class SessionNotifiers implements SessionFeature {
-  private readonly notifierIds = new Set<string>();
-
-  private static getFromSession(session: Session, createIfNotExist = true) {
-    const FEATURE_NAME = 'logging/notifiers';
-    const existing = session.findFeature(FEATURE_NAME);
-    if(existing) {
-      return existing as SessionNotifiers;
-    }
-
-    const feature = new SessionNotifiers();
-    session.addFeature(FEATURE_NAME, feature);
-    return feature;
-  }
-
-  static addNotifierId(session: Session, id: string) {
-    SessionNotifiers.getFromSession(session).notifierIds.add(id);
-  }
-
-  static removeNotifierId(session: Session, id: string) {
-    SessionNotifiers.getFromSession(session).notifierIds.delete(id);
-  }
-
-  static getNotifierIds(session: Session) {
-    const feature = SessionNotifiers.getFromSession(session, false);
-    return feature ? [...feature.notifierIds] : [];
-  }
-}
-*/
 
 export class Online implements Service {
+  private readonly transport: bus.Transport;
+  private readonly instanceInfos = new Map<string, InstanceInfo>();
+  private readonly notifiers = new SessionNotifierManager('online/instance-info-notifiers', 'online/instance-info');
 
   constructor(params: BuildParams) {
+    this.transport = params.transport;
+    this.transport.presence.on('instanceChange', this.onInstanceChange);
+
+    for (const instanceName of this.transport.presence.getOnlines()) {
+      this.onInstanceOnline(instanceName);
+    }
   }
 
   async init() {
+    this.notifiers.init();
+    Services.instance.sessionManager.registerServiceHandler('online/start-notify-instance-info', this.startNotifyInstanceInfo);
+    Services.instance.sessionManager.registerServiceHandler('online/stop-notify-instance-info', this.stopNotifyInstanceInfo);
   }
 
   async terminate() {
+    this.transport.presence.off('instanceChange', this.onInstanceChange);
   }
 
+  private readonly onInstanceChange = (instanceName: string, online: boolean) => {
+    if (online) {
+      this.onInstanceOnline(instanceName);
+    } else {
+      this.onInstanceOffline(instanceName);
+    }
+  };
+
+  private onInstanceOnline(instanceName: string) {
+    tools.fireAsync(async () => {
+      const view = await this.transport.metadata.createView(instanceName);
+      this.instanceInfos.set(instanceName, new InstanceInfo(view));
+    });
+    this.notifiers.notifyAll({ newInstance: instanceName });
+  }
+
+  private onInstanceOffline(instanceName: string) {
+    const instanceInfo = this.instanceInfos.get(instanceName);
+    if (!instanceInfo) {
+      log.error(`Instance '${instanceName}' is going offline but we were not aware of it`);
+      return;
+    }
+
+    this.instanceInfos.delete(instanceName);
+    tools.fireAsync(() => this.transport.metadata.closeView(instanceInfo.view));
+    this.notifiers.notifyAll({ removeInstance: instanceName });
+  }
+
+  private startNotifyInstanceInfo = async (session: Session) => {
+    const notifier = this.notifiers.createNotifier(session);
+
+    // send infos after we reply
+    setImmediate(() => {
+      for (const instanceInfo of this.instanceInfos.values()) {
+        notifier.notify({ newInstance: instanceInfo.name });
+      }
+    });
+
+    return { notifierId: notifier.id };
+  };
+
+  private stopNotifyInstanceInfo = async (session: Session, { notifierId }: { notifierId: string; }) => {
+    this.notifiers.removeNotifier(session, notifierId);
+  };
 }
+
+class InstanceInfo {
+  constructor(public readonly view: bus.RemoteMetadataView) {
+  }
+
+  get name() {
+    return this.view.remoteInstanceName;
+  }
+}
+
