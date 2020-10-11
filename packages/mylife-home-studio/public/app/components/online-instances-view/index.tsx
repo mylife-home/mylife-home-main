28c26ab1c8f3a21870d715a8fe0d3b2fce51d2be
vincent-tr <vincent.tr@gmail.com>
2020-10-11 15:23:23 +0200

    sort wip

--- packages/mylife-home-studio/public/app/components/online-instances-view/index.tsx
+++ packages/mylife-home-studio/public/app/components/online-instances-view/index.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
@@ -16,31 +17,37 @@ import { getInstancesInfos } from '../../store/online-instances-view/selectors';
 // TODO: display capabilities
 // TODO: display versions
 
+interface Sort {
+  column: string;
+  direction: 'asc' | 'desc';
+}
+
 const OnlineInstancesView: FunctionComponent = () => {
   const { data } = useConnect();
+
+  const [sort, setSort] = useState<Sort>({ column: 'instanceName', direction: 'asc' });
+
   return (
     <TableContainer>
       <Table>
 
         <TableHead>
           <TableRow>
-            <TableCell>Instance</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Nom d'hôte</TableCell>
-            <TableCell>Matériel</TableCell>
-            <TableCell>Uptime système</TableCell>
-            <TableCell>Uptime instance</TableCell>
-            <TableCell>Fonctionalités</TableCell>
-            <TableCell>Versions</TableCell>
+            <TableHeader sort={sort} setSort={setSort} column='instanceName' title='Instance' />
+            <TableHeader sort={sort} setSort={setSort} column='type' title='Type' />
+            <TableHeader sort={sort} setSort={setSort} column='hostname' title={'Nom d\'hôte'} />
+            <TableHeader sort={sort} setSort={setSort} column='hardware' title='Matériel' />
+            <TableHeader sort={sort} setSort={setSort} column='systemBootTime' title='Uptime système' />
+            <TableHeader sort={sort} setSort={setSort} column='instanceBootTime' title='Uptime instance' />
+            <TableHeader sort={sort} setSort={setSort} column='capabilities' title='Fonctionalités' />
+            <TableHeader sort={sort} setSort={setSort} column='versions' title='Versions' />
           </TableRow>
         </TableHead>
 
         <TableBody>
           {Object.entries(data).map(([instanceName, instanceInfo]) => (
             <TableRow key={instanceName}>
-              <TableCell component="th" scope="row">
-                {instanceName}
-              </TableCell>
+              <TableCell>{instanceName}</TableCell>
               <TableCell>{instanceInfo.type}</TableCell>
               <TableCell>{instanceInfo.hostname}</TableCell>
               <TableCell>{instanceInfo.hardware}</TableCell>
@@ -64,3 +71,25 @@ function useConnect() {
     data: useSelector(getInstancesInfos)
   };
 }
+
+interface TableHeaderProps {
+  column: string;
+  title: string;
+  sort: Sort;
+  setSort: (newSort: Sort) => void;
+}
+
+const TableHeader: FunctionComponent<TableHeaderProps> = ({ column, title, sort, setSort }) => {
+  const onClick = () => {
+    const isAsc = sort.column === column && sort.direction === 'asc';
+    setSort({ column, direction: isAsc ? 'desc' : 'asc' });
+  };
+
+  return (
+    <TableCell sortDirection={sort.column === column ? sort.direction : false}>
+      <TableSortLabel active={sort.column === column} direction={sort.column === column ? sort.direction : undefined} onClick={onClick}>
+        {title}
+      </TableSortLabel>
+    </TableCell>
+  );
+};
