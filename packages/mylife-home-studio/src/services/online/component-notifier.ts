d2af3c653c6d5f3882a70dbd8dc330c8e2024138
vincent-tr <vincent.tr@gmail.com>
2020-10-12 18:31:41 +0200

    emit changes

--- packages/mylife-home-studio/src/services/online/component-notifier.ts
+++ packages/mylife-home-studio/src/services/online/component-notifier.ts
@@ -1,6 +1,5 @@
-import { EventEmitter } from 'events';
-import { logger, bus, tools, components } from 'mylife-home-common';
-import { SetPluginData, SetComponentData, Component, SetStateData, State } from '../../../shared/online';
+import { logger, bus, components } from 'mylife-home-common';
+import { SetPluginData, SetComponentData, Component, SetStateData, State, ClearData } from '../../../shared/online';
 import { Session, SessionNotifierManager } from '../session-manager';
 
 const log = logger.createLogger('mylife:home:studio:services:online:component-notifier');
@@ -8,9 +7,15 @@ const log = logger.createLogger('mylife:home:studio:services:online:component-no
 export class ComponentNotifier {
   private readonly registry: components.Registry;
   private readonly notifiers = new SessionNotifierManager('online/component-notifiers', 'online/component');
+  private readonly componentListeners = new Map<components.Component, (name: string, value: any) => void>();
 
   constructor(private readonly transport: bus.Transport) {
     this.registry = new components.Registry({ transport, publishRemoteComponents: true });
+
+    this.registry.on('plugin.add', this.onPluginAdd);
+    this.registry.on('plugin.remove', this.onPluginRemove);
+    this.registry.on('component.add', this.onComponentAdd);
+    this.registry.on('component.remove', this.onComponentRemove);
   }
 
   init() {
@@ -18,6 +23,16 @@ export class ComponentNotifier {
   }
 
   terminate() {
+    this.registry.off('plugin.add', this.onPluginAdd);
+    this.registry.off('plugin.remove', this.onPluginRemove);
+    this.registry.off('component.add', this.onComponentAdd);
+    this.registry.off('component.remove', this.onComponentRemove);
+
+    for(const [component, listener] of this.componentListeners.entries()) {
+      component.off('state', listener);
+    }
+    this.componentListeners.clear();
+
     this.registry.close();
   }
 
@@ -53,5 +68,48 @@ export class ComponentNotifier {
 
   async stopNotify(session: Session, { notifierId }: { notifierId: string; }) {
     this.notifiers.removeNotifier(session, notifierId);
+  };
+
+  private onPluginAdd = (instanceName: string, plugin: components.metadata.Plugin) => {
+    const update: SetPluginData = { type: 'plugin', operation: 'set', instanceName, data: components.metadata.encodePlugin(plugin) };
+    this.notifiers.notifyAll(update);
+  };
+
+  private onPluginRemove = (instanceName: string, plugin: components.metadata.Plugin) => {
+    const update: ClearData = { type: 'plugin', operation: 'clear', instanceName, id: plugin.id };
+    this.notifiers.notifyAll(update);
+  };
+
+  private onComponentAdd = (instanceName: string, component: components.Component) => {
+    const data: Component = { id: component.id, plugin: component.plugin.id };
+    const update: SetComponentData = { type: 'component', operation: 'set', instanceName: instanceName, data };
+    this.notifiers.notifyAll(update);
+
+    for (const [name, value] of Object.entries(component.getStates())) {
+      const data: State = { component: component.id, name, value };
+      const update: SetStateData = { type: 'state', operation: 'set', instanceName, data };
+      this.notifiers.notifyAll(update);
+    }
+
+    const listener = (name: string, value: any) => {
+      this.onStateChange(instanceName, component.id, name, value);
+    };
+
+    component.on('state', listener);
+    this.componentListeners.set(component, listener);
+  };
+
+  private onComponentRemove = (instanceName: string, component: components.Component) => {
+    const update: ClearData = { type: 'component', operation: 'clear', instanceName, id: component.id };
+    this.notifiers.notifyAll(update);
+
+    const listener = this.componentListeners.get(component);
+    component.off('state', listener);
+  };
+
+  private onStateChange(instanceName: string, component: string, name: string, value: any) {
+    const data: State = { component, name, value };
+    const update: SetStateData = { type: 'state', operation: 'set', instanceName, data };
+    this.notifiers.notifyAll(update);
   };
 }
