a4a9db7227a7d15eab3e23325b3143119217d63c
vincent-tr <vincent.tr@gmail.com>
2020-10-13 09:11:17 +0200

    wip treeview + selectors

--- packages/mylife-home-studio/public/app/components/online-components-view/index.tsx
+++ packages/mylife-home-studio/public/app/components/online-components-view/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
@@ -8,6 +9,13 @@ import FormLabel from '@material-ui/core/FormLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import { AppState } from '../../store/types';
+import { getPluginsIds, getPlugin } from '../../store/online-components-view/selectors';
 
 const OnlineComponentsView: FunctionComponent = () => {
   const [value, setValue] = React.useState('instances-plugins-components');
@@ -16,18 +24,63 @@ const OnlineComponentsView: FunctionComponent = () => {
     setValue((event.target as HTMLInputElement).value);
   };
 
+  const [expanded, setExpanded] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState<string[]>([]);
+
+  const handleToggle = (event: React.ChangeEvent, nodeIds: string[]) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event: React.ChangeEvent, nodeIds: string[]) => {
+    setSelected(nodeIds);
+  };
+
+  const pluginsIds = useSelector(getPluginsIds);
+
   return (
     <Box p={3}>
-      <Typography>Components</Typography>
-
       <RadioGroup value={value} onChange={handleChange} row>
         <FormControlLabel value='instances-plugins-components' control={<Radio color='primary' />} label='instances / plugins / components' />
         <FormControlLabel value='instances-components' control={<Radio color='primary' />} label='instances / components' />
         <FormControlLabel value='plugins-components' control={<Radio color='primary' />} label='plugins / components' />
         <FormControlLabel value='components' control={<Radio color='primary' />} label='components' />
       </RadioGroup> 
+
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        expanded={expanded}
+        selected={selected}
+        onNodeToggle={handleToggle}
+        onNodeSelect={handleSelect}
+      >
+        {pluginsIds.map(({ instanceName, id }) => (
+          <Plugin instanceName={instanceName} pluginId={id} />
+        ))}
+        <TreeItem nodeId="1" label="Applications">
+          <TreeItem nodeId="2" label="Calendar" />
+          <TreeItem nodeId="3" label="Chrome" />
+          <TreeItem nodeId="4" label="Webstorm" />
+        </TreeItem>
+        <TreeItem nodeId="5" label="Documents">
+          <TreeItem nodeId="6" label="Material-UI">
+            <TreeItem nodeId="7" label="src">
+              <TreeItem nodeId="8" label="index.js" />
+              <TreeItem nodeId="9" label="tree-view.js" />
+            </TreeItem>
+          </TreeItem>
+        </TreeItem>
+      </TreeView>
     </Box>
   );
 };
 
 export default OnlineComponentsView;
+
+const Plugin: FunctionComponent<{ instanceName: string, pluginId: string }> = ({ instanceName, pluginId }) => {
+  const plugin = useSelector((state: AppState) => getPlugin(state, instanceName, pluginId));
+  return (
+    <TreeItem nodeId={`${instanceName}$${pluginId}`} label={plugin.id}>
+    </TreeItem>
+  );
+};
--- packages/mylife-home-studio/public/app/store/online-components-view/selectors.ts
+++ packages/mylife-home-studio/public/app/store/online-components-view/selectors.ts
@@ -3,42 +3,100 @@ import { AppState } from '../types';
 import { getTabsCount } from '../tabs/selectors';
 import { TabType } from '../tabs/types';
 import { buildUid } from './helpers';
+import { Plugin, Component } from './types';
+
+export interface InstanceName {
+  instanceName: string;
+  pluginsIds: string[];
+  componentsIds: string[];
+}
+
+export interface InstanceAndId {
+  instanceName: string;
+  id: string;
+}
+
+export interface PluginData extends Plugin {
+  componentsIds: string[];
+}
+
+export interface ComponentData extends Component {
+  pluginsIds: string[];
+}
 
 export const hasOnlineComponentsViewTab = (state: AppState) => getTabsCount(state, TabType.ONLINE_COMPONENTS_VIEW) > 0;
 
 const getOnlineComponentsView = (state: AppState) => state.onlineComponentsView;
 export const getNotifierId = (state: AppState) => getOnlineComponentsView(state).notifierId;
 
-export const getPlugin = (state: AppState, instanceName: string, pluginId: string) => getOnlineComponentsView(state).plugins[buildUid(instanceName, pluginId)];
-export const getComponent = (state: AppState, instanceName: string, componentId: string) => getOnlineComponentsView(state).components[buildUid(instanceName, componentId)];
+export const getPlugin = (state: AppState, instanceName: string, pluginId: string) => {
+  const plugin: PluginData = {
+    ...getOnlineComponentsView(state).plugins[buildUid(instanceName, pluginId)],
+    componentsIds: []
+  };
+  // TODO
+  return plugin;
+}
 
-export const getInstanceNames = createSelector(
+export const getComponent = (state: AppState, instanceName: string, componentId: string) => {
+  const component: ComponentData = {
+    ...getOnlineComponentsView(state).components[buildUid(instanceName, componentId)],
+    pluginsIds: []
+  };
+  // TODO
+  return component;
+}
+
+export const getInstancesNames = createSelector(
   (state: AppState) => getOnlineComponentsView(state).plugins,
-  (plugins) => {
+  (state: AppState) => getOnlineComponentsView(state).components,
+  (plugins, components) => {
     const set = new Set<string>();
     for (const plugin of Object.values(plugins)) {
       set.add(plugin.instanceName);
     }
-    return Array.from(set);
+
+    const instancesNames = Array.from(set).sort().map(instanceName => ({
+      instanceName,
+      pluginsIds: [],
+      componentsIds: []
+    } as InstanceName));
+
+    const map = new Map<string, InstanceName>();
+    for (const item of instancesNames) {
+      map.set(item.instanceName, item);
+    }
+
+    for(const plugin of Object.values(plugins)) {
+      const instanceName = map.get(plugin.instanceName);
+      instanceName.pluginsIds.push(plugin.id);
+    }
+
+    for(const component of Object.values(components)) {
+      const instanceName = map.get(component.instanceName);
+      instanceName.componentsIds.push(component.id);
+    }
+
+    return instancesNames;
   }
 );
 
-export const getPlugins = createSelector(
+export const getPluginsIds = createSelector(
   (state: AppState) => getOnlineComponentsView(state).plugins,
-  (plugins) => Object.values(plugins).sort(comparer)
+  (plugins) => Object.values(plugins).map(mapId).sort(comparer)
 );
 
-export const getComponents = createSelector(
+export const getComponentsIds = createSelector(
   (state: AppState) => getOnlineComponentsView(state).components,
-  (components) => Object.values(components).sort(comparer)
+  (components) => Object.values(components).map(mapId).sort(comparer)
 );
 
-interface InstanceAndId {
-  instanceName: string;
-  id: string;
+function mapId<T extends InstanceAndId>(obj: T) {
+  const { instanceName, id } = obj;
+  return { instanceName, id };
 }
 
-function comparer<T extends InstanceAndId>(a: T, b: T) {
+function comparer(a: InstanceAndId, b: InstanceAndId) {
   if (a.instanceName < b.instanceName) {
     return -1;
   }
