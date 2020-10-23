4dde21c37808743b6bbb5fdc769981077dba22b2
vincent-tr <vincent.tr@gmail.com>
2020-10-23 14:32:29 +0200

    plugin details

--- packages/mylife-home-studio/public/app/components/online-components-view/detail/plugin.tsx
+++ packages/mylife-home-studio/public/app/components/online-components-view/detail/plugin.tsx
@@ -1,16 +1,20 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
 
-import { ConfigItem } from '../../../../../shared/component-model';
+import { ConfigItem, Member, MemberType, Plugin } from '../../../../../shared/component-model';
 import { AppState } from '../../../store/types';
 import { getPlugin } from '../../../store/online-components-view/selectors';
 import { StateIcon, ActionIcon, ConfigIcon } from '../../lib/icons';
 import { Title, SectionDivider, SectionTitle, Item, Count, NameValue } from './layout';
 
+type UiConfigItem = { name: string; } & ConfigItem;
+type UiMember = { name: string; } & Omit<Member, 'memberType'>;
+
 const Plugin: FunctionComponent<{ id: string }> = ({ id }) => {
   const plugin = useSelector((state: AppState) => getPlugin(state, id));
+  const { config, states, actions } = usePluginProps(plugin);
   return (
     <Box p={3}>
       <Title type='plugin' title={plugin.display} />
@@ -26,21 +30,25 @@ const Plugin: FunctionComponent<{ id: string }> = ({ id }) => {
 
       <SectionTitle icon={ConfigIcon} value='Configuration' />
 
-      {Object.entries(plugin.config).map(([name, item]) => (
-        <ConfigItem key={name} name={name} item={item} />
+      {config.map(configItem => (
+        <ConfigItem key={configItem.name} configItem={configItem} />
       ))}
 
       <SectionDivider />
 
       <SectionTitle icon={StateIcon} value='États' />
 
-      <Item value='TODO' />
+      {states.map(member => (
+        <MemberItem key={member.name} member={member} />
+      ))}
 
       <SectionDivider />
 
       <SectionTitle icon={ActionIcon}  value='Actions' />
 
-      <Item value='TODO' />
+      {actions.map(member => (
+        <MemberItem key={member.name} member={member} />
+      ))}
 
       <SectionDivider />
 
@@ -51,13 +59,54 @@ const Plugin: FunctionComponent<{ id: string }> = ({ id }) => {
 
 export default Plugin;
 
-const ConfigItem: FunctionComponent<{ name: string, item: ConfigItem }> = ({ name, item }) => {
-  let value = item.valueType as string;
-  if (item.description) {
-    value += ` (${item.description})`;
+const ConfigItem: FunctionComponent<{ configItem: UiConfigItem }> = ({ configItem }) => {
+  let value = configItem.valueType as string;
+  if (configItem.description) {
+    value += ` (${configItem.description})`;
   }
 
   return (
-    <NameValue name={name} value={value} />
+    <NameValue name={configItem.name} value={value} />
   )
+}
+
+const MemberItem: FunctionComponent<{ member: UiMember }> = ({ member }) => {
+  let value = member.valueType as string;
+  if (member.description) {
+    value += ` (${member.description})`;
+  }
+
+  return (
+    <NameValue name={member.name} value={value} />
+  )
+}
+
+function usePluginProps(plugin: Plugin) {
+  return useMemo(() => {
+    const config: UiConfigItem[] = [];
+    const states: UiMember[] = [];
+    const actions: UiMember[] = [];
+
+    for(const [name, item] of Object.entries(plugin.config)) {
+      config.push({ name, ...item });
+    }
+
+    for(const [name, item] of Object.entries(plugin.members)) {
+      switch(item.memberType) {
+        case MemberType.STATE: {
+          const { memberType: _, ... values } = item;
+          states.push({ name, ...values });
+          break;
+        }
+
+        case MemberType.ACTION: {
+          const { memberType: _, ... values } = item;
+          actions.push({ name, ...values });
+          break;
+        }
+      }
+    }
+
+    return { config, states, actions };
+  }, [plugin]);
 }
