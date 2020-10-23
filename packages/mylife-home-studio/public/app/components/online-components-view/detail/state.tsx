227c36c3e533ea6b8716260908fda3fa451f83f6
vincent-tr <vincent.tr@gmail.com>
2020-10-23 15:46:04 +0200

    state detail

--- packages/mylife-home-studio/public/app/components/online-components-view/detail/state.tsx
+++ packages/mylife-home-studio/public/app/components/online-components-view/detail/state.tsx
@@ -1,18 +1,46 @@
 import React, { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
 
 import { AppState } from '../../../store/types';
-import { getState } from '../../../store/online-components-view/selectors';
-import { Title } from './layout';
+import { getPlugin, getComponent, getState } from '../../../store/online-components-view/selectors';
+import { ComponentIcon, PluginIcon, StateIcon } from '../../lib/icons';
+import { Title, SectionDivider, SectionTitle, NameValue } from './layout';
 
 const State: FunctionComponent<{ id: string }> = ({ id }) => {
   const state = useSelector((state: AppState) => getState(state, id));
+  console.log(state);
+  const component = useSelector((appState: AppState) => getComponent(appState, state.component));
+  const plugin = useSelector((appState: AppState) => getPlugin(appState, component.plugin));
+  const member = plugin.members[state.name];
+
   return (
     <Box p={3}>
       <Title type='state' title={state.name} />
+
+      <SectionTitle icon={ComponentIcon} value="Composant" />
+
+      <NameValue name="ID" value={component.display} />
+      <NameValue name="Instance" value={component.instance} />
+
+      <SectionDivider />
+
+      <SectionTitle icon={PluginIcon} value="Plugin" />
+
+      <NameValue name="Nom" value={plugin.display} />
+      <NameValue name="Utilisation" value={plugin.usage} />
+      <NameValue name="Version" value={plugin.version} />
+      <NameValue name="Description" value={plugin.description} />
+
+      <SectionDivider />
+
+      <SectionTitle icon={StateIcon} value="Etat" />
+
+      <NameValue name="Nom" value={state.name} />
+      <NameValue name="Valeur" value={JSON.stringify(state.value)} />
+      <NameValue name="Type" value={member.valueType} />
+      <NameValue name="Description" value={member.description} />
     </Box>
   );
 };
