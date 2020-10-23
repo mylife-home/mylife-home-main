cfd168bbb6290d5ce31661b06099e4c006172805
vincent-tr <vincent.tr@gmail.com>
2020-10-23 16:05:42 +0200

    online history skeleton

---
+++ packages/mylife-home-studio/src/services/online/history-notifier.ts
@@ -0,0 +1,23 @@
+import { logger, bus, components } from 'mylife-home-common';
+import { Component } from '../../../shared/component-model';
+import { Session, SessionNotifierManager } from '../session-manager';
+
+const log = logger.createLogger('mylife:home:studio:services:online:history-notifier');
+
+export class HistoryNotifier {
+  constructor(transport: bus.Transport) {
+  }
+
+  init() {
+  }
+
+  terminate() {
+  }
+
+  async startNotify(session: Session) {
+    return { notifierId: 'TODO' };
+  };
+
+  async stopNotify(session: Session, { notifierId }: { notifierId: string; }) {
+  };
+}
--- packages/mylife-home-studio/src/services/online/index.ts
+++ packages/mylife-home-studio/src/services/online/index.ts
@@ -2,28 +2,35 @@ import { Service, BuildParams } from '../types';
 import { Services } from '..';
 import { InstanceNotifier } from './instance-notifier';
 import { ComponentNotifier } from './component-notifier';
+import { HistoryNotifier } from './history-notifier';
 
 export class Online implements Service {
   private readonly instanceNotifier: InstanceNotifier;
   private readonly componentNotifier: ComponentNotifier;
+  private readonly historyNotifier: HistoryNotifier;
 
   constructor(params: BuildParams) {
     this.instanceNotifier = new InstanceNotifier(params.transport);
     this.componentNotifier = new ComponentNotifier(params.transport);
+    this.historyNotifier = new HistoryNotifier(params.transport);
   }
 
   async init() {
     await this.instanceNotifier.init();
     await this.componentNotifier.init();
+    await this.historyNotifier.init();
 
     Services.instance.sessionManager.registerServiceHandler('online/start-notify-instance-info', session => this.instanceNotifier.startNotify(session));
     Services.instance.sessionManager.registerServiceHandler('online/stop-notify-instance-info', (session, payload: any) => this.instanceNotifier.stopNotify(session, payload));
     Services.instance.sessionManager.registerServiceHandler('online/start-notify-component', session => this.componentNotifier.startNotify(session));
     Services.instance.sessionManager.registerServiceHandler('online/stop-notify-component', (session, payload: any) => this.componentNotifier.stopNotify(session, payload));
+    Services.instance.sessionManager.registerServiceHandler('online/start-notify-history', session => this.historyNotifier.startNotify(session));
+    Services.instance.sessionManager.registerServiceHandler('online/stop-notify-history', (session, payload: any) => this.historyNotifier.stopNotify(session, payload));
   }
 
   async terminate() {
     await this.instanceNotifier.terminate();
     await this.componentNotifier.terminate();
+    await this.historyNotifier.terminate();
   }
 }
