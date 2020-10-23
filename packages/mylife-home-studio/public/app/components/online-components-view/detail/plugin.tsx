7e5dee7fa76a0b6c83ece7966db89fe129d8a682
vincent-tr <vincent.tr@gmail.com>
2020-10-23 14:53:02 +0200

    format

--- packages/mylife-home-studio/public/app/components/online-components-view/detail/plugin.tsx
+++ packages/mylife-home-studio/public/app/components/online-components-view/detail/plugin.tsx
@@ -9,50 +9,50 @@ import { getPlugin } from '../../../store/online-components-view/selectors';
 import { StateIcon, ActionIcon, ConfigIcon } from '../../lib/icons';
 import { Title, SectionDivider, SectionTitle, Count, NameValue } from './layout';
 
-type UiConfigItem = { name: string; } & ConfigItem;
-type UiMember = { name: string; } & Omit<Member, 'memberType'>;
+type UiConfigItem = { name: string } & ConfigItem;
+type UiMember = { name: string } & Omit<Member, 'memberType'>;
 
 const Plugin: FunctionComponent<{ id: string }> = ({ id }) => {
   const plugin = useSelector((state: AppState) => getPlugin(state, id));
   const { config, states, actions } = usePluginProps(plugin);
   return (
     <Box p={3}>
-      <Title type='plugin' title={plugin.display} />
+      <Title type="plugin" title={plugin.display} />
 
-      <NameValue name='Nom' value={plugin.name} />
-      <NameValue name='Module' value={plugin.module} />
-      <NameValue name='Utilisation' value={plugin.usage} />
-      <NameValue name='Instance' value={plugin.instance} />
-      <NameValue name='Version' value={plugin.version} />
-      <NameValue name='Description' value={plugin.description} />
+      <NameValue name="Nom" value={plugin.name} />
+      <NameValue name="Module" value={plugin.module} />
+      <NameValue name="Utilisation" value={plugin.usage} />
+      <NameValue name="Instance" value={plugin.instance} />
+      <NameValue name="Version" value={plugin.version} />
+      <NameValue name="Description" value={plugin.description} />
 
       <SectionDivider />
 
-      <SectionTitle icon={ConfigIcon} value='Configuration' />
+      <SectionTitle icon={ConfigIcon} value="Configuration" />
 
-      {config.map(configItem => (
+      {config.map((configItem) => (
         <ConfigItem key={configItem.name} configItem={configItem} />
       ))}
 
       <SectionDivider />
 
-      <SectionTitle icon={StateIcon} value='États' />
+      <SectionTitle icon={StateIcon} value="États" />
 
-      {states.map(member => (
+      {states.map((member) => (
         <MemberItem key={member.name} member={member} />
       ))}
 
       <SectionDivider />
 
-      <SectionTitle icon={ActionIcon}  value='Actions' />
+      <SectionTitle icon={ActionIcon} value="Actions" />
 
-      {actions.map(member => (
+      {actions.map((member) => (
         <MemberItem key={member.name} member={member} />
       ))}
 
       <SectionDivider />
 
-      <Count value={plugin.components.length} singular='component' plural='components' />
+      <Count value={plugin.components.length} singular="component" plural="components" />
     </Box>
   );
 };
@@ -65,10 +65,8 @@ const ConfigItem: FunctionComponent<{ configItem: UiConfigItem }> = ({ configIte
     value += ` (${configItem.description})`;
   }
 
-  return (
-    <NameValue name={configItem.name} value={value} />
-  )
-}
+  return <NameValue name={configItem.name} value={value} />;
+};
 
 const MemberItem: FunctionComponent<{ member: UiMember }> = ({ member }) => {
   let value = member.valueType as string;
@@ -76,15 +74,16 @@ const MemberItem: FunctionComponent<{ member: UiMember }> = ({ member }) => {
     value += ` (${member.description})`;
   }
 
-  return (
-    <NameValue name={member.name} value={value} />
-  )
-}
+  return <NameValue name={member.name} value={value} />;
+};
 
 function usePluginProps(plugin: Plugin) {
-  return useMemo(() => ({
-    config: plugin.configIds.map(name => ({ name, ... plugin.config[name] })),
-    states: plugin.stateIds.map(name => ({ name, ... plugin.members[name] })),
-    actions: plugin.actionIds.map(name => ({ name, ... plugin.members[name] })),
-  }), [plugin]);
+  return useMemo(
+    () => ({
+      config: plugin.configIds.map((name) => ({ name, ...plugin.config[name] })),
+      states: plugin.stateIds.map((name) => ({ name, ...plugin.members[name] })),
+      actions: plugin.actionIds.map((name) => ({ name, ...plugin.members[name] })),
+    }),
+    [plugin]
+  );
 }
