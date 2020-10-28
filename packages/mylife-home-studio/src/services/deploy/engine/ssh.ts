5e5e1b3acfb7d147561d38de8bfc8c5dcd64caf6
vincent-tr <vincent.tr@gmail.com>
2020-10-28 19:28:12 +0100

    wip ts

--- packages/mylife-home-studio/src/services/deploy/engine/ssh.ts
+++ packages/mylife-home-studio/src/services/deploy/engine/ssh.ts
@@ -1,12 +1,114 @@
-const { Client } = require('ssh2');
+import { Client, ConnectConfig, ExecOptions, SFTPWrapper } from 'ssh2';
 
-class SSHClient {
+export { ConnectConfig };
 
-  constructor() {
-    this._conn = new Client();
+interface ExecResult {
+  stdout: string;
+  stderr: string;
+  code: number;
+  signal: string;
+}
 
-    const createSftpCall = name => async (...args) => {
-      const sftp = await this._getSftp();
+export class SSHClient {
+  private readonly conn = new Client();
+  public readonly sftp = new SFTP(this.conn);
+
+  async connect(options: ConnectConfig) {
+    return await new Promise<void>((resolve, reject) => {
+
+      const removeListeners = () => {
+        this.conn.removeListener('error', onError);
+        this.conn.removeListener('ready', onReady);
+      };
+
+      const onError = (err: Error) => {
+        removeListeners();
+        reject(err);
+      };
+
+      const onReady = () => {
+        removeListeners();
+        resolve();
+      };
+
+      this.conn.once('error', onError);
+      this.conn.once('ready', onReady);
+
+      this.conn.connect(options);
+    });
+  }
+
+  end() {
+    this.conn.end();
+  }
+
+  async exec(command: string, env?: { [key: string]: string; }) {
+    const ret = await this.execImpl(command, { env });
+    if (ret.stderr) {
+      throw new Error(`Command has error output : '${ret.stderr}'`);
+    }
+
+    if (ret.signal) {
+      throw new Error(`Command received signal ${ret.signal}`);
+    }
+
+    if (ret.code) {
+      throw new Error(`Command returned error code ${ret.code}`);
+    }
+
+    return ret.stdout;
+  }
+
+  private async execImpl(command: string, options: ExecOptions = {}) {
+    return await new Promise<ExecResult>((resolve, reject) => {
+      this.conn.exec(command, options, (err, stream) => {
+        if(err) {
+          return reject(err);
+        }
+
+        let stdout = Buffer.alloc(0);
+        let stderr = Buffer.alloc(0);
+
+        const ret: ExecResult = {
+          stdout: null,
+          stderr: null,
+          code: null,
+          signal: null
+        };
+
+        stream.on('exit', (code, signal) => {
+          ret.code   = code;
+          ret.signal = signal;
+        });
+
+        stream.on('close', () => {
+          ret.stdout = stdout.toString();
+          ret.stderr = stderr.toString();
+
+          resolve(ret);
+        });
+
+        stream.on('data', (data: Buffer) => {
+          stdout = Buffer.concat([ stdout, data ]);
+        });
+
+        stream.stderr.on('data', (data: Buffer) => {
+          stderr = Buffer.concat([ stderr, data ]);
+        });
+
+      });
+    });
+  }
+
+
+}
+
+export class SFTP {
+  private sftpImpl: SFTPWrapper;
+
+  constructor(private readonly conn: Client) {
+    const createSftpCall = (name ) => async (...args) => {
+      const sftp = await this.getSftp();
 
       return await new Promise((resolve, reject) => {
         sftp[name](...args, (err, result) => {
@@ -36,96 +138,19 @@ class SSHClient {
     this._sftpRead  = createSftpCall('read');
     this._sftpWrite = createSftpCall('write');
   }
+  
+  private async getSftp() {
+    if (this.sftpImpl) {
+      return this.sftpImpl;
+    }
 
-  async connect(options) {
-    return await new Promise((resolve, reject) => {
-
-      const removeListeners = () => {
-        this._conn.removeListener('error', onError);
-        this._conn.removeListener('ready', onReady);
-      };
-
-      const onError = err => {
-        removeListeners();
-        reject(err);
-      };
-
-      const onReady = () => {
-        removeListeners();
-        resolve();
-      };
-
-      this._conn.once('error', onError);
-      this._conn.once('ready', onReady);
-
-      this._conn.connect(options);
-    });
-  }
-
-  end() {
-    this._conn.end();
-  }
-
-  async _exec(command, options = {}) {
-    return await new Promise((resolve, reject) => {
-
-      this._conn.exec(command, options, (err, stream) => {
-        if(err) {
+    return await new Promise<SFTPWrapper>((resolve, reject) => {
+      this.conn.sftp((err, sftp) => {
+        if (err) {
           return reject(err);
         }
 
-        const ret = {
-          stdout : Buffer.alloc(0),
-          stderr : Buffer.alloc(0)
-        };
-
-        stream.on('close', (code, signal) => {
-          ret.code   = code;
-          ret.signal = signal;
-          ret.stdout = ret.stdout.toString();
-          ret.stderr = ret.stderr.toString();
-
-          resolve(ret);
-        });
-
-        stream.on('data', data => {
-          ret.stdout = Buffer.concat([ ret.stdout, data ]);
-        });
-
-        stream.stderr.on('data', data => {
-          ret.stderr = Buffer.concat([ ret.stderr, data ]);
-        });
-
-      });
-    });
-  }
-
-  async exec(command, env) {
-    const ret = await this._exec(command, { env });
-    if(ret.stderr) {
-      throw new Error(`Command has error output : '${ret.stderr}'`);
-    }
-    if(ret.signal) {
-      throw new Error(`Command received signal ${ret.signal}`);
-    }
-    if(ret.code) {
-      throw new Error(`Command returned error code ${ret.code}`);
-    }
-    return ret.stdout;
-  }
-
-  async _getSftp() {
-    if(this._sftp) {
-      return this._sftp;
-    }
-
-    return await new Promise((resolve, reject) => {
-      this._conn.sftp((err, sftp) => {
-        if(err) {
-          return reject(err);
-        }
-
-        this._sftp = sftp;
+        this.sftpImpl = sftp;
         resolve(sftp);
       });
     });
@@ -151,7 +176,4 @@ class SSHClient {
       await this._sftpClose(handle);
     }
   }
-
 }
-
-exports.SSHClient = SSHClient;
