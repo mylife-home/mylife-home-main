1d683cdfdb0bf3247fbb7344b16c1151dac5d45e
vincent-tr <vincent.tr@gmail.com>
2020-10-29 19:32:52 +0100

    wip recipe

--- packages/mylife-home-studio/src/services/deploy/engine/recipe.ts
+++ packages/mylife-home-studio/src/services/deploy/engine/recipe.ts
@@ -1,47 +1,83 @@
 import fs from 'fs';
 import path from 'path';
-const tasks       = require('./tasks');
-const utils       = require('./tasks-utils');
+import tasks from './tasks';
+import utils from './tasks-utils';
 const directories = require('../directories');
 
-class Step {
+interface StepConfig {
+  type: string;
+}
 
-  async execute(/*context*/) {
-    throw new Error('not implemented');
+interface TaskStepConfig extends StepConfig {
+  name: string;
+}
+
+interface RecipeStepConfig extends StepConfig {
+  name: string;
+}
+
+interface Context {}
+
+export class Recipe {
+  private readonly steps: Step[] = [];
+
+  constructor(public readonly name: string) {
+    const fullname = path.join(directories.recipes(), name + '.json');
+    if (!fs.existsSync(fullname)) {
+      throw new Error(`not such recipe : ${name}`);
+    }
+
+    const { steps } = JSON.parse(fs.readFileSync(fullname, 'utf8')) as { steps: StepConfig[] };
+
+    for (const step of steps) {
+      const StepType = STEP_BY_TYPE[step.type];
+      if (!StepType) {
+        throw new Error(`Unknown step type : ${step.type}`);
+      }
+
+      this.steps.push(new StepType(step));
+    }
+  }
+
+  async execute(context: Context) {
+    const log = utils.createLogger(context, 'recipe');
+    log.info(`begin '${this.name}'`);
+
+    for (const step of this.steps) {
+      await step.execute(context);
+    }
+
+    log.info(`end '${this.name}'`);
   }
 }
 
-function makeUpperCamelCase(name) {
-  return name
-    .split('-')
-    .map(part => part[0].toUpperCase() + part.substring(1))
-    .join('');
+abstract class Step {
+  abstract execute(context: Context): Promise<void>;
 }
 
 class TaskStep extends Step {
-
-  constructor(config) {
+  constructor(config: TaskStepConfig) {
     super();
 
     const task = tasks[makeUpperCamelCase(config.name)];
-    if(!task) {
+    if (!task) {
       throw new Error(`Task does not exist : ${config.name}`);
     }
 
     this.task = task;
     this.parameters = Object.assign({}, config.parameters);
 
-    for(const def of task.metadata.parameters) {
+    for (const def of task.metadata.parameters) {
       const type = typeof this.parameters[def.name];
-      if(type !== 'undefined') {
-        if(type !== def.type) {
+      if (type !== 'undefined') {
+        if (type !== def.type) {
           throw new Error(`Bad parameter type : ${def.name}`);
         }
 
         continue;
       }
 
-      if(typeof def.default !== 'undefined') {
+      if (typeof def.default !== 'undefined') {
         this.parameters[def.name] = def.default;
         continue;
       }
@@ -50,17 +86,16 @@ class TaskStep extends Step {
     }
   }
 
-  async execute(context) {
-
+  async execute(context: Context) {
     const replaces = Object.entries(context.variables || {}).map(([name, value]) => ({
-      regex : new RegExp(`\\$\\{${name}\\}`, 'g'),
-      value
+      regex: new RegExp(`\\$\\{${name}\\}`, 'g'),
+      value,
     }));
 
     const formattedParams = {};
-    for(const [name, value] of Object.entries(this.parameters)) {
+    for (const [name, value] of Object.entries(this.parameters)) {
       let newValue = value;
-      for(const rep of replaces) {
+      for (const rep of replaces) {
         newValue = newValue.replace(rep.regex, rep.value);
       }
       formattedParams[name] = newValue;
@@ -68,59 +103,32 @@ class TaskStep extends Step {
 
     await this.task.execute(context, formattedParams);
   }
-
 }
 
 class RecipeStep extends Step {
+  private readonly recipe: Recipe;
 
-  constructor(config) {
+  constructor(config: RecipeStepConfig) {
     super();
 
     this.recipe = new Recipe(config.name);
   }
 
-  async execute(context) {
+  async execute(context: Context) {
     await this.recipe.execute(context);
   }
 }
 
-const stepByType = {
-  task   : TaskStep,
-  recipe : RecipeStep
+type StepConstructor = new (config: any) => Step;
+
+const STEP_BY_TYPE: { [key: string]: StepConstructor } = {
+  task: TaskStep,
+  recipe: RecipeStep,
 };
 
-class Recipe {
-
-  constructor(name) {
-    this.name = name;
-    const fullname = path.join(directories.recipes(), name + '.json');
-    if(!fs.existsSync(fullname)) {
-      throw new Error(`not such recipe : ${name}`);
-    }
-    const { steps } = JSON.parse(fs.readFileSync(fullname, 'utf8'));
-
-    this.steps = [];
-
-    for(const step of steps) {
-      const StepType = stepByType[step.type];
-      if(!StepType) {
-        throw new Error(`Unknown step type : ${step.type}`);
-      }
-
-      this.steps.push(new StepType(step));
-    }
-  }
-
-  async execute(context) {
-    const log = utils.createLogger(context, 'recipe');
-    log.info(`begin '${this.name}'`);
-
-    for(const step of this.steps) {
-      await step.execute(context);
-    }
-
-    log.info(`end '${this.name}'`);
-  }
+function makeUpperCamelCase(name: string) {
+  return name
+    .split('-')
+    .map((part) => part[0].toUpperCase() + part.substring(1))
+    .join('');
 }
-
-module.exports = Recipe;
