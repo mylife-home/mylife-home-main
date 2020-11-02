d038c709f9af4f8701676a25742aa69d75adaf53
vincent-tr <vincent.tr@gmail.com>
2020-11-02 15:12:23 +0100

    directories config

---
+++ packages/mylife-home-studio/src/services/deploy/config.ts
@@ -0,0 +1,4 @@
+export interface Config {
+  filesPath: string;
+  recipesPath: string;
+}
--- packages/mylife-home-studio/src/services/deploy/directories.ts
+++ packages/mylife-home-studio/src/services/deploy/directories.ts
@@ -1,16 +1,22 @@
 import path from 'path';
 import { tools } from 'mylife-home-common';
+import { Config } from './config';
 
-let config: Config;
+interface Directories {
+  files: string;	
+  recipes: string;	
+}	
 
-interface Config {
-  filesPath: string;
-  recipesPath: string;
-}
+const directories: Directories = {
+  files: null,
+  recipes: null,
+};
 
 export function configure() {
-  config = tools.getConfigItem<Config>('deploy');
+  const config = tools.getConfigItem<Config>('deploy');
+  directories.files = path.resolve(config.filesPath);
+  directories.recipes = path.resolve(config.recipesPath);
 }
 
-export const files = () => config.filesPath;
-export const recipes = () => config.recipesPath;
+export const files = () => directories.files;
+export const recipes = () => directories.recipes;
