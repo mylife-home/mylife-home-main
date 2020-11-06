022f0503fea9b6840ca48b724249a21df9af28e8
vincent-tr <vincent.tr@gmail.com>
2020-11-06 18:03:03 +0100

    selector

--- packages/mylife-home-studio/public/app/store/deploy/selectors.ts
+++ packages/mylife-home-studio/public/app/store/deploy/selectors.ts
@@ -1,11 +1,17 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { AppState } from '../types';
 import { getTabsCount } from '../tabs/selectors';
 import { TabType } from '../tabs/types';
-// import { ComponentHistoryItem, CriteriaDefinition, HistoryItem, InstanceHistoryItem, StateHistoryItem } from './types';
 
 export const hasDeployTab = (state: AppState) => getTabsCount(state, TabType.DEPLOY) > 0;
 
 const getDeploy = (state: AppState) => state.deploy;
 export const getNotifierId = (state: AppState) => getDeploy(state).notifierId;
-// const getItemsTable = (state: AppState) => getDeploy(state).items;
-// export const getItem = (state: AppState, id: string) => getItemsTable(state).byId[id];
+
+export const getRecipesIds = (state: AppState) => getDeploy(state).recipes.allIds;
+export const getRecipe = (state: AppState, id: string) => getDeploy(state).recipes.byId[id];
+
+export const getPinnedRecipesIds = createSelector(
+  (state: AppState) => getDeploy(state).recipes,
+  (recipes) => recipes.allIds.filter(id => recipes.byId[id].pinned)
+);
