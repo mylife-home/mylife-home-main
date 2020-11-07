ac8ff6564c02a6d0191d08d9e3d975ec8c925674
vincent-tr <vincent.tr@gmail.com>
2020-11-07 16:04:23 +0100

    wip sidebar

--- packages/mylife-home-studio/public/app/components/deploy/sidebar.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/sidebar.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,8 +9,10 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { RecipeIcon, StartIcon, RunsIcon, FileIcon } from './icons';
+import { getPinnedRecipesIds } from '../../store/deploy/selectors';
 
 const useStyles = makeStyles((theme) => ({
   section: {},
@@ -25,55 +29,107 @@ const SideBar: FunctionComponent = () => {
   const classes = useStyles();
   return (
     <List>
-      <ListItem button>
-        <ListItemIcon>
-          <RecipeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Recettes" primaryTypographyProps={{ variant: 'h6' }} />
-      </ListItem>
-      <ListItem button className={classes.item}>
-        <ListItemIcon>
-          <RecipeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Recette 1" primaryTypographyProps={{ variant: 'body1' }} />
-        <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="Démarrer">
-            <StartIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-      <ListItem button className={classes.item}>
-        <ListItemIcon>
-          <RecipeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Recette 2" primaryTypographyProps={{ variant: 'body1' }} />
-      </ListItem>
-      <ListItem button className={classes.item}>
-        <ListItemIcon>
-          <RecipeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Recette 3" primaryTypographyProps={{ variant: 'body1' }} />
-      </ListItem>
-
+      <Recipes />
       <Divider className={classes.divider} />
-
-      <ListItem button>
-        <ListItemIcon>
-          <RunsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Executions" primaryTypographyProps={{ variant: 'h6' }} />
-      </ListItem>
-
+      <Runs />
       <Divider className={classes.divider} />
-
-      <ListItem button>
-        <ListItemIcon>
-          <FileIcon />
-        </ListItemIcon>
-        <ListItemText primary="Fichiers" primaryTypographyProps={{ variant: 'h6' }} />
-      </ListItem>
+      <Files />
     </List>
   );
 };
 
 export default SideBar;
+
+const Recipes: FunctionComponent = () => {
+  const { recipesIds } = useConnectRecipes();
+  return (
+    <>
+      <Section title="Recettes" icon={RecipeIcon} onClick={() => console.log('recipes')} />
+      {recipesIds.map((id) => (
+        <Item
+          key={id}
+          title={id}
+          icon={RecipeIcon}
+          onClick={() => console.log(`recipe ${id}`)}
+          secondary={{ tooltip: 'Démarrer la recette', icon: StartIcon, onClick: () => console.log(`start recipe ${id}`) }}
+        />
+      ))}
+    </>
+  );
+};
+
+function useConnectRecipes() {
+  return {
+    recipesIds: useSelector(getPinnedRecipesIds),
+  };
+}
+
+const Runs: FunctionComponent = () => {
+  return (
+    <>
+      <Section title="Exécutions" icon={RunsIcon} onClick={() => console.log('runs')} />
+    </>
+  );
+};
+
+const Files: FunctionComponent = () => {
+  return (
+    <>
+      <Section title="Fichiers" icon={FileIcon} onClick={() => console.log('files')} />
+    </>
+  );
+};
+
+interface BaseItemProps {
+  title: string;
+  icon: typeof SvgIcon;
+  onClick: () => void;
+}
+
+interface SectionProps extends BaseItemProps {}
+
+interface ItemProps extends BaseItemProps {
+  secondary?: {
+    tooltip: string;
+    icon: typeof SvgIcon;
+    onClick: () => void;
+  };
+}
+
+const Section: FunctionComponent<SectionProps> = ({ title, icon, onClick }) => {
+  const classes = useStyles();
+  const Icon = icon;
+
+  return (
+    <ListItem button className={classes.section} onClick={onClick}>
+      <ListItemIcon>
+        <Icon />
+      </ListItemIcon>
+      <ListItemText primary={title} primaryTypographyProps={{ variant: 'h6' }} />
+    </ListItem>
+  );
+};
+
+const Item: FunctionComponent<ItemProps> = ({ title, icon, onClick, secondary }) => {
+  const classes = useStyles();
+  const Icon = icon;
+  const SecondaryIcon = secondary?.icon;
+
+  return (
+    <ListItem button className={classes.item} onClick={onClick}>
+      <ListItemIcon>
+        <Icon />
+      </ListItemIcon>
+      <ListItemText primary={title} primaryTypographyProps={{ variant: 'body1' }} />
+      {secondary && (
+        <ListItemSecondaryAction>
+          <Tooltip title={secondary.tooltip}>
+            <IconButton edge="end" onClick={secondary.onClick}>
+              <SecondaryIcon />
+            </IconButton>
+          </Tooltip>
+        </ListItemSecondaryAction>
+      )}
+    </ListItem>
+  );
+};
