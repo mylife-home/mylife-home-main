ef060df868106c290816a4a0c6f3439add9aec22
vincent-tr <vincent.tr@gmail.com>
2020-11-07 16:07:02 +0100

    layout

--- packages/mylife-home-studio/public/app/components/deploy/index.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/index.tsx
@@ -5,7 +5,7 @@ import SplitPane from '../lib/split-pane';
 import SideBar from './sidebar';
 
 const Deploy: FunctionComponent = () => (
-  <SplitPane split="vertical" defaultSize={400} minSize={400}>
+  <SplitPane split="vertical" defaultSize={400} minSize={300}>
     <SideBar />
     <Box>Main</Box>
   </SplitPane>
