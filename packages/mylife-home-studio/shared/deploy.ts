85a8f3cf70a83fd8b975aa13db07da843ea401ba
vincent-tr <vincent.tr@gmail.com>
2020-11-08 18:40:17 +0100

    add recipe description

--- packages/mylife-home-studio/shared/deploy.ts
+++ packages/mylife-home-studio/shared/deploy.ts
@@ -24,6 +24,7 @@ export interface RunLog {
 export type RunLogSeverity = 'debug' | 'info' | 'warning' | 'error';
 
 export interface RecipeConfig {
+  description: string;
   steps: StepConfig[];
 }
 
