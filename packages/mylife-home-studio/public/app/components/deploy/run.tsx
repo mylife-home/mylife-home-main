72a5d636aec48763d0b7ec63682f38ff6a41d448
vincent-tr <vincent.tr@gmail.com>
2020-11-10 10:29:40 +0100

    wip run

--- packages/mylife-home-studio/public/app/components/deploy/run.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/run.tsx
@@ -1,17 +1,35 @@
 import React, { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 import { AppState } from '../../store/types';
-import { RunningIcon, SuccessIcon, FailureIcon } from './icons';
-import { useResetSelectionIfNull } from './selection';
-import { Title } from './layout';
 import { Run } from '../../store/deploy/types';
 import { getRun } from '../../store/deploy/selectors';
+import { humanizeDuration } from '../lib/durations';
+import { RunningIcon, SuccessIcon, FailureIcon } from './icons';
+import { useSelection, useResetSelectionIfNull } from './selection';
+import { Container, Title } from './layout';
+
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    width: 900,
+    margin: theme.spacing(3),
+  },
+}));
+
+const STATUSES = {
+  created: 'Créé',
+  running: "En cours d'exécution",
+  ended: 'Terminé',
+};
 
 const RunDisplay: FunctionComponent<{ id: string }> = ({ id }) => {
+  const classes = useStyles();
   const run = useSelector((state: AppState) => getRun(state, id));
+  const { select } = useSelection();
 
   // handle run that becomes null
   useResetSelectionIfNull(run);
@@ -21,15 +39,67 @@ const RunDisplay: FunctionComponent<{ id: string }> = ({ id }) => {
   }
 
   return (
-    <Box p={3}>
-      <Title text={getRunTitle(run)} icon={getRunIcon(run)} />
-    </Box>
+    <Container title={<Title text={getRunTitle(run)} icon={getRunIcon(run)} />}>
+      <Grid container spacing={3} className={classes.gridContainer}>
+        <Grid item xs={2}>
+          <Typography>État</Typography>
+        </Grid>
+        <Grid item xs={10}>
+          <Typography>{STATUSES[run.status]}</Typography>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Typography>Recette</Typography>
+        </Grid>
+        <Grid item xs={10}>
+          <Link component="button" variant="body1" onClick={() => select({ type: 'recipe', id: run.recipe })}>
+            {run.recipe}
+          </Link>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Typography>Début</Typography>
+        </Grid>
+        <Grid item xs={10}>
+          <Typography>{run.creation.toLocaleString('fr-FR')}</Typography>
+        </Grid>
+
+        {run.end && (
+          <>
+            <Grid item xs={2}>
+              <Typography>Fin</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography>{run.end.toLocaleString('fr-FR')}</Typography>
+            </Grid>
+
+            <Grid item xs={2}>
+              <Typography>Durée</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography>{humanizeDuration(run.end.valueOf() - run.creation.valueOf())}</Typography>
+            </Grid>
+          </>
+        )}
+
+        {run.err && (
+          <>
+            <Grid item xs={2}>
+              <Typography>Erreur</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography>TODO</Typography>
+            </Grid>
+          </>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
 export default RunDisplay;
 
-const useStyles = makeStyles((theme) => ({
+const useIconsStyles = makeStyles((theme) => ({
   successIcon: {
     color: theme.palette.success.main,
   },
@@ -39,12 +109,12 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ColoredSuccessIcon: FunctionComponent = () => {
-  const classes = useStyles();
+  const classes = useIconsStyles();
   return <SuccessIcon className={classes.successIcon} />;
 };
 
 const ColoredFailureIcon: FunctionComponent = () => {
-  const classes = useStyles();
+  const classes = useIconsStyles();
   return <FailureIcon className={classes.failureIcon} />;
 };
 
