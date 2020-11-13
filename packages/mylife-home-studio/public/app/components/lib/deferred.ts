5d4fcc1665b9ace070527233bdc95036c430cf60
vincent-tr <vincent.tr@gmail.com>
2020-11-13 17:52:17 +0100

    deferred

---
+++ packages/mylife-home-studio/public/app/components/lib/deferred.ts
@@ -0,0 +1,21 @@
+export class Deferred<T> {
+  public promise: Promise<T>;
+
+  private _resolve: (value: T) => void;
+  private _reject: (reason: Error) => void;
+
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+
+  resolve(value: T) {
+    this._resolve(value);
+  }
+
+  reject(reason: Error) {
+    this._reject(reason);
+  }
+}
