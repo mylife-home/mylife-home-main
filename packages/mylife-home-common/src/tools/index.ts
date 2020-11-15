2e1491f38e1fe132608343d87583a5f1126649b4
vincent-tr <vincent.tr@gmail.com>
2020-11-15 09:54:57 +0100

    add deferred tools

--- packages/mylife-home-common/src/tools/index.ts
+++ packages/mylife-home-common/src/tools/index.ts
@@ -33,3 +33,25 @@ export function init(mainComponent: string) {
 
   instanceInfo.init();
 }
+
+export class Deferred<T> {
+  public promise: Promise<T>;
+
+  private _resolve: (value: T) => void;
+  private _reject: (reason: Error) => void;
+
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+
+  resolve(value: T) {
+    this._resolve(value);
+  }
+
+  reject(reason: Error) {
+    this._reject(reason);
+  }
+}
