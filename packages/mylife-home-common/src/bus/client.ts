1943c618e71ca62f797c2c885e342a2fc8827f44
vincent-tr <vincent.tr@gmail.com>
2020-11-15 09:15:39 +0100

    cosmetic

--- packages/mylife-home-common/src/bus/client.ts
+++ packages/mylife-home-common/src/bus/client.ts
@@ -31,7 +31,7 @@ export class Client extends EventEmitter {
 
     this.client.on('connect', () =>
       fireAsync(async () => {
-        // given the spec, it is unclear if LWT should be executed in case of takeover, so we run it to be sure
+        // given the spec, it is unclear if LWT should be executed in case of client takeover, so we run it to be sure
         await this.publish(this.buildTopic('online'), Buffer.allocUnsafe(0), true);
 
         await this.clearResidentState();
