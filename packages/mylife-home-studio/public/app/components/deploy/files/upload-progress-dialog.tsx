37992aba4c39d545b55b94bafc2e0fc455363b0a
vincent-tr <vincent.tr@gmail.com>
2020-11-15 18:01:26 +0100

    file upload dialog

--- packages/mylife-home-studio/public/app/components/deploy/files/upload-progress-dialog.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/files/upload-progress-dialog.tsx
@@ -1,12 +1,17 @@
 import React, { FunctionComponent, useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import humanize from 'humanize-plus';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import CheckIcon from '@material-ui/icons/Check';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 import { getUploadFilesProgress } from '../../../store/deploy/selectors';
 import { UploadFileProgress } from '../../../store/deploy/types';
@@ -14,21 +19,36 @@ import { UploadFileProgress } from '../../../store/deploy/types';
 const UploadProgressDialog: FunctionComponent = () => {
   const progress = useSelector(getUploadFilesProgress);
   const open = !!progress;
-  const { totalSize, doneSize, currentIndex, files } = useProgressInfo(progress);
+  const { totalSize, doneSize, percent, doneFiles, files } = useProgressInfo(progress);
 
   return (
     <Dialog aria-labelledby="dialog-title" open={open} scroll="paper" maxWidth="sm" fullWidth>
-      <DialogTitle id="dialog-title">{`Upload en cours ...`}</DialogTitle>
+      <DialogTitle id="dialog-title">{`Upload en cours (${percent.toFixed()} %)`}</DialogTitle>
 
       <DialogContent dividers>
-        <DialogContentText>{`${doneSize} / ${totalSize} - ${currentIndex} / ${files.length}`}</DialogContentText>
+        <Typography gutterBottom>
+          {`Transfert : ${humanize.fileSize(doneSize)} / ${humanize.fileSize(totalSize)}`}
+        </Typography>
+        <Typography gutterBottom>
+          {`Fichiers : ${doneFiles} / ${files.length}`}
+        </Typography>
 
         <List>
-          {files.map(file => (
-            <ListItem key={file.name}>
-              <ListItemText primary={file.name} />
-            </ListItem>
-          ))}
+          {files.map(file => {
+            const Icon = getStatusIcon(file.status);
+            const totalSize = humanize.fileSize(file.totalSize);
+            const doneSize = humanize.fileSize(file.doneSize);
+            const secondary = file.status === 'executing' ? `${doneSize} / ${totalSize}` : totalSize;
+            return (
+              <ListItem key={file.name}>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+
+                <ListItemText primary={file.name} secondary={secondary} />
+              </ListItem>
+            );
+          })}
         </List>
       </DialogContent>
 
@@ -38,8 +58,21 @@ const UploadProgressDialog: FunctionComponent = () => {
 
 export default UploadProgressDialog;
 
+function getStatusIcon(status: FileStatus) {
+  switch (status) {
+    case 'pending':
+      return HourglassEmptyIcon;
+    case 'done':
+      return CheckIcon;
+    case 'executing':
+      return PlayArrowIcon;
+  }
+}
+
+type FileStatus = 'pending' | 'done' | 'executing';
+
 interface DisplayFile extends UploadFileProgress {
-  status: 'pending' | 'done' | 'executing';
+  status: FileStatus;
 }
 
 function useProgressInfo(progress: UploadFileProgress[]) {
@@ -48,14 +81,16 @@ function useProgressInfo(progress: UploadFileProgress[]) {
       return {
         totalSize: 0,
         doneSize: 0,
-        currentIndex: 0,
-        files: [],
+        percent: 0,
+        doneFiles: 0,
+        files: [] as DisplayFile[],
       };
     }
 
-    const totalSize = progress.reduce((acc, curr) => acc + curr.totalSize, 0);
-    const doneSize = progress.reduce((acc, curr) => acc + curr.doneSize, 0);
-    const currentIndex = progress.findIndex(file => file.doneSize < file.totalSize);
+    const totalSize = progress.reduce((acc, file) => acc + file.totalSize, 0);
+    const doneSize = progress.reduce((acc, file) => acc + file.doneSize, 0);
+    const percent = doneSize / totalSize * 100;
+    const doneFiles = progress.filter(file => file.doneSize === file.totalSize).length;
   
     const files = progress.map(file => ({
       ...file,
@@ -65,13 +100,14 @@ function useProgressInfo(progress: UploadFileProgress[]) {
     return {
       totalSize,
       doneSize,
-      currentIndex,
+      percent,
+      doneFiles,
       files,
     };
   }, [progress]);
 }
 
-function getStatus(file: UploadFileProgress) {
+function getStatus(file: UploadFileProgress): FileStatus {
   if (file.doneSize === 0) {
     return 'pending';
   } else if (file.doneSize === file.totalSize) {
