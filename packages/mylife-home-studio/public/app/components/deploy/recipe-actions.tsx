8a9b841268f8b908236cea6512320d500f9228d0
vincent-tr <vincent.tr@gmail.com>
2020-11-17 14:57:33 +0100

    share code

---
+++ packages/mylife-home-studio/public/app/components/deploy/recipe-actions.tsx
@@ -0,0 +1,122 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+
+import DeleteButton from '../lib/delete-button';
+import { useFireAsync } from '../lib/use-error-handling';
+import { useActions } from '../lib/use-actions';
+import { useInputDialog } from '../dialogs/input';
+import { getRecipesIds, getRecipe } from '../../store/deploy/selectors';
+import { clearRecipe, pinRecipe, startRecipe, setRecipe } from '../../store/deploy/actions';
+import { AppState } from '../../store/types';
+import { StartIcon, PinIcon, UnpinIcon } from './icons';
+
+const useStyles = makeStyles((theme) => ({
+  deleteButton: {
+    color: theme.palette.error.main,
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: fade(theme.palette.text.primary, theme.palette.action.hoverOpacity), // fade = alpha
+    },
+  },
+  startButton: {
+    color: theme.palette.success.main,
+  },
+}));
+
+const RecipeActions: FunctionComponent<{ id: string }> = ({ id }) => {
+  const classes = useStyles();
+  const { recipe, clear, pin, start, setRecipe } = useRecipeConnect(id);
+  const fireAsync = useFireAsync();
+  const showNewNameDialog = useNewNameDialog();
+
+  const onDuplicate = () =>
+    fireAsync(async () => {
+      const { status, id } = await showNewNameDialog();
+      if (status === 'ok') {
+        const config = clone(recipe.config);
+        setRecipe({ id, config });
+      }
+    });
+
+  return (
+    <>
+      {recipe.pinned ? (
+        <Tooltip title="Désépingler">
+          <IconButton onClick={() => pin(false)}>
+            <UnpinIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Épingler">
+          <IconButton onClick={() => pin(true)}>
+            <PinIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+
+      <Tooltip title="Démarrer">
+        <IconButton className={classes.startButton} onClick={() => start()}>
+          <StartIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Tooltip title="Dupliquer">
+        <IconButton onClick={onDuplicate}>
+          <FileCopyIcon />
+        </IconButton>
+      </Tooltip>
+
+      <DeleteButton icon tooltip="Supprimer" className={classes.deleteButton} onConfirmed={() => clear()} />
+    </>
+  );
+};
+
+export default RecipeActions;
+
+function useRecipeConnect(id: string) {
+  const dispatch = useDispatch();
+  return {
+    recipe: useSelector((state: AppState) => getRecipe(state, id)),
+    ...useMemo(
+      () => ({
+        clear: () => dispatch(clearRecipe(id)),
+        pin: (value: boolean) => dispatch(pinRecipe({ id, value })),
+        start: () => dispatch(startRecipe(id)),
+      }),
+      [dispatch, id]
+    ),
+    ...useActions({ setRecipe }),
+  };
+}
+
+export function useNewNameDialog() {
+  const showDialog = useInputDialog();
+  const recipesIds = useSelector(getRecipesIds);
+
+  const options = {
+    title: 'Nouveau nom',
+    message: 'Entrer un nom de recette',
+    initialText: 'Nouvelle recette',
+    validator(newId: string) {
+      if (!newId) {
+        return 'Nom vide';
+      }
+      if (recipesIds.includes(newId)) {
+        return 'Ce nom existe déjà';
+      }
+    }
+  };
+
+  return async () => {
+    const { status, text: id } = await showDialog(options);
+    return { status, id };
+  };
+}
+
+function clone<T>(obj: T) {
+  return JSON.parse(JSON.stringify(obj)) as T;
+}
--- packages/mylife-home-studio/public/app/components/deploy/recipes.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/recipes.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useMemo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { makeStyles, fade } from '@material-ui/core/styles';
+import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -8,19 +8,17 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import AddIcon from '@material-ui/icons/Add';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
 
-import DeleteButton from '../lib/delete-button';
 import { useFireAsync } from '../lib/use-error-handling';
 import { useActions } from '../lib/use-actions';
-import { useInputDialog } from '../dialogs/input';
 import { getRecipesIds, getRecipe } from '../../store/deploy/selectors';
-import { clearRecipe, pinRecipe, startRecipe, setRecipe } from '../../store/deploy/actions';
+import { setRecipe } from '../../store/deploy/actions';
 import { RecipeConfig } from '../../store/deploy/types';
 import { AppState } from '../../store/types';
-import { RecipeIcon, StartIcon, PinIcon, UnpinIcon } from './icons';
+import { RecipeIcon } from './icons';
 import { useSelection } from './selection';
 import { Container, Title, CustomizedListItemText } from './layout';
+import RecipeActions, { useNewNameDialog } from './recipe-actions';
 
 const useStyles = makeStyles((theme) => ({
   newButton: {
@@ -28,16 +26,6 @@ const useStyles = makeStyles((theme) => ({
   },
   list: {
     width: 900,
-  },
-  deleteButton: {
-    color: theme.palette.error.main,
-    backgroundColor: 'transparent',
-    '&:hover': {
-      backgroundColor: fade(theme.palette.text.primary, theme.palette.action.hoverOpacity), // fade = alpha
-    },
-  },
-  startButton: {
-    color: theme.palette.success.main,
   },
 }));
 
@@ -86,20 +74,8 @@ const Recipes: FunctionComponent = () => {
 export default Recipes;
 
 const RecipeItem: FunctionComponent<{ id: string }> = ({ id }) => {
-  const classes = useStyles();
   const { select } = useSelection();
-  const { recipe, clear, pin, start, setRecipe } = useRecipeConnect(id);
-  const fireAsync = useFireAsync();
-  const showNewNameDialog = useNewNameDialog();
-
-  const onDuplicate = () =>
-    fireAsync(async () => {
-      const { status, id } = await showNewNameDialog();
-      if (status === 'ok') {
-        const config = clone(recipe.config);
-        setRecipe({ id, config });
-      }
-    });
+  const recipe = useSelector((state: AppState) => getRecipe(state, id));
 
   return (
     <ListItem button onClick={() => select({ type: 'recipe', id })}>
@@ -110,33 +86,7 @@ const RecipeItem: FunctionComponent<{ id: string }> = ({ id }) => {
       <CustomizedListItemText primary={id} secondary={recipe.config.description || null} />
 
       <ListItemSecondaryAction>
-        {recipe.pinned ? (
-          <Tooltip title="Désépingler">
-            <IconButton onClick={() => pin(false)}>
-              <UnpinIcon />
-            </IconButton>
-          </Tooltip>
-        ) : (
-          <Tooltip title="Épingler">
-            <IconButton onClick={() => pin(true)}>
-              <PinIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-
-        <Tooltip title="Démarrer">
-          <IconButton className={classes.startButton} onClick={() => start()}>
-            <StartIcon />
-          </IconButton>
-        </Tooltip>
-
-        <Tooltip title="Dupliquer">
-          <IconButton onClick={onDuplicate}>
-            <FileCopyIcon />
-          </IconButton>
-        </Tooltip>
-
-        <DeleteButton icon tooltip="Supprimer" className={classes.deleteButton} onConfirmed={() => clear()} />
+        <RecipeActions id={id} />
       </ListItemSecondaryAction>
     </ListItem>
   );
@@ -147,48 +97,4 @@ function useRecipesConnect() {
     recipesIds: useSelector(getRecipesIds),
     ...useActions({ setRecipe })
   };
-}
-
-function useRecipeConnect(id: string) {
-  const dispatch = useDispatch();
-  return {
-    recipe: useSelector((state: AppState) => getRecipe(state, id)),
-    ...useMemo(
-      () => ({
-        clear: () => dispatch(clearRecipe(id)),
-        pin: (value: boolean) => dispatch(pinRecipe({ id, value })),
-        start: () => dispatch(startRecipe(id)),
-      }),
-      [dispatch, id]
-    ),
-    ...useActions({ setRecipe }),
-  };
-}
-
-function useNewNameDialog() {
-  const showDialog = useInputDialog();
-  const recipesIds = useSelector(getRecipesIds);
-
-  const options = {
-    title: 'Nouveau nom',
-    message: 'Entrer un nom de recette',
-    initialText: 'Nouvelle recette',
-    validator(newId: string) {
-      if (!newId) {
-        return 'Nom vide';
-      }
-      if (recipesIds.includes(newId)) {
-        return 'Ce nom existe déjà';
-      }
-    }
-  };
-
-  return async () => {
-    const { status, text: id } = await showDialog(options);
-    return { status, id };
-  };
-}
-
-function clone<T>(obj: T) {
-  return JSON.parse(JSON.stringify(obj)) as T;
 }
