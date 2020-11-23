f855273f47aed5a5f68be75e526f3584f08cb5df
vincent-tr <vincent.tr@gmail.com>
2020-11-23 18:27:59 +0100

    fix recipe steps management

--- packages/mylife-home-studio/public/app/components/deploy/recipe/index.tsx
+++ packages/mylife-home-studio/public/app/components/deploy/recipe/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -11,7 +11,6 @@ import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
 
 import DebouncedTextField from '../../lib/debounced-text-field';
-import { useDebounced } from '../../lib/use-debounced';
 import { SortableList } from '../../lib/sortable-list';
 import { useResetSelectionIfNull } from '../selection';
 import { RecipeIcon } from '../icons';
@@ -19,11 +18,8 @@ import { Container, Title } from '../layout';
 import RecipeActions from '../recipe-actions';
 import StepEditor, { SetStepConfig } from './step-editor';
 import { AppState } from '../../../store/types';
-import { setRecipe } from '../../../store/deploy/actions';
 import { getRecipe } from '../../../store/deploy/selectors';
-import { RecipeConfig, TaskStepConfig } from '../../../store/deploy/types';
-
-// TODO: JSON.stringify(step) cannot be used as step id (because we can create a same step twice), we need to define unique ids on steps
+import { useRecipeConfigState, useStepOperations, RecipeConfigWithIds, SetRecipeConfig } from './use-recipe-config-state';
 
 const Recipe: FunctionComponent<{ id: string }> = ({ id }) => {
   const recipe = useSelector((state: AppState) => getRecipe(state, id));
@@ -52,8 +48,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-type SetRecipeConfig = React.Dispatch<React.SetStateAction<RecipeConfig>>;
-
 const RecipePanel: FunctionComponent<{ id: string }> = ({ id }) => {
   const classes = useStyles();
   const [config, setConfig] = useRecipeConfigState(id);
@@ -69,19 +63,6 @@ const RecipePanel: FunctionComponent<{ id: string }> = ({ id }) => {
   );
 };
 
-function useRecipeConfigState(id: string): [RecipeConfig, SetRecipeConfig] {
-  const recipe = useSelector((state: AppState) => getRecipe(state, id));
-  const dispatch = useDispatch();
-  const persistRecipeConfig = useCallback(
-    (config: RecipeConfig) => {
-      dispatch(setRecipe({ id, config }));
-    },
-    [dispatch]
-  );
-  const { componentValue, componentChange } = useDebounced(recipe.config, persistRecipeConfig);
-  return [componentValue, componentChange];
-}
-
 const useHeaderPanelStyles = makeStyles((theme) => ({
   container: {
     width: 900,
@@ -89,7 +70,7 @@ const useHeaderPanelStyles = makeStyles((theme) => ({
   },
 }));
 
-const HeaderPanel: FunctionComponent<{ className?: string; id: string; config: RecipeConfig; setConfig: SetRecipeConfig }> = ({ className, id, config, setConfig }) => {
+const HeaderPanel: FunctionComponent<{ className?: string; id: string; config: RecipeConfigWithIds; setConfig: SetRecipeConfig }> = ({ className, id, config, setConfig }) => {
   const classes = useHeaderPanelStyles();
 
   const updateDescription = useCallback(
@@ -118,40 +99,18 @@ const useConfigPanelStyles = makeStyles((theme) => ({
   },
 }));
 
-const ConfigPanel: FunctionComponent<{ className?: string; config: RecipeConfig; setConfig: SetRecipeConfig }> = ({ className, config, setConfig }) => {
+const ConfigPanel: FunctionComponent<{ className?: string; config: RecipeConfigWithIds; setConfig: SetRecipeConfig }> = ({ className, config, setConfig }) => {
   const classes = useConfigPanelStyles();
-
-  const moveStep = useCallback(
-    (from: number, to: number) =>
-      setConfig((config) => {
-        const movedStep = config.steps[from];
-        const newSteps = [...config.steps];
-
-        newSteps.splice(from, 1);
-        newSteps.splice(to, 0, movedStep);
-
-        return { ...config, steps: newSteps };
-      }),
-    [setConfig]
-  );
+  const { moveStep, deleteStep, setStep } = useStepOperations(setConfig);
 
   return (
     <SortableList disablePadding className={clsx(className, classes.list)} moveItem={moveStep}>
       {config.steps.map((step, index) => {
-        const setStep: SetStepConfig = (newStep) =>
-          setConfig((config) => {
-            const newSteps = [...config.steps];
-            newSteps[index] = newStep;
-            return { ...config, steps: newSteps };
-          });
+        const onSetStep: SetStepConfig = (newStep) => setStep(index, newStep);
+        const onDelete = () => deleteStep(index);
+        const key = config.stepIds[index];
 
-          const onDelete = () => setConfig((config) => {
-            const newSteps = [...config.steps];
-            newSteps.splice(index, 1);
-            return { ...config, steps: newSteps };
-          });
-
-        return <StepEditor key={JSON.stringify(step)} step={step} setStep={setStep} onDelete={onDelete} />;
+        return <StepEditor key={key} step={step} setStep={onSetStep} onDelete={onDelete} />;
       })}
 
       <AddStepItem setConfig={setConfig} />
@@ -167,18 +126,13 @@ const useAddStepItemStyles = makeStyles((theme) => ({
 
 const AddStepItem: FunctionComponent<{ setConfig: SetRecipeConfig }> = ({ setConfig }) => {
   const classes = useAddStepItemStyles();
-
-  const onNew = () => setConfig((config) => {
-    const newStep: TaskStepConfig = { type: 'task', task: null, parameters: {} };
-    const newSteps = [...config.steps, newStep];
-    return { ...config, steps: newSteps };
-});
+  const { newStep } = useStepOperations(setConfig);
 
   return (
     <ListItem>
       <ListItemIcon>
         <Tooltip title="Nouvelle étape">
-          <IconButton className={classes.newButton} onClick={onNew}>
+          <IconButton className={classes.newButton} onClick={newStep}>
             <AddIcon />
           </IconButton>
         </Tooltip>
---
+++ packages/mylife-home-studio/public/app/components/deploy/recipe/use-recipe-config-state.ts
@@ -0,0 +1,95 @@
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useDebounced } from '../../lib/use-debounced';
+import { AppState } from '../../../store/types';
+import { setRecipe } from '../../../store/deploy/actions';
+import { getRecipe } from '../../../store/deploy/selectors';
+import { RecipeConfig, StepConfig, TaskStepConfig } from '../../../store/deploy/types';
+
+export interface RecipeConfigWithIds extends RecipeConfig {
+  stepIds: number[];
+}
+
+export type SetRecipeConfig = React.Dispatch<React.SetStateAction<RecipeConfigWithIds>>;
+
+export function useRecipeConfigState(id: string): [RecipeConfigWithIds, SetRecipeConfig] {
+  const recipe = useSelector((state: AppState) => getRecipe(state, id));
+  const dispatch = useDispatch();
+  const persistRecipeConfig = useCallback(
+    (config: RecipeConfigWithIds) => {
+      console.log(config);
+      dispatch(setRecipe({ id, config: withoutIds(config) }));
+    },
+    [dispatch]
+  );
+
+  const config = useMemo(() => withIds(recipe.config), [recipe.config]);
+
+  const { componentValue, componentChange } = useDebounced(config, persistRecipeConfig);
+  return [componentValue, componentChange];
+}
+
+function withIds(config: RecipeConfig) {
+  const finalConfig: RecipeConfigWithIds = { stepIds: [], ...config };
+  for (const step of config.steps) {
+    finalConfig.stepIds.push(newId());
+  }
+  return finalConfig;
+}
+
+function withoutIds(config: RecipeConfigWithIds): RecipeConfig {
+  const { stepIds, ...trimmedConfig } = config;
+  return trimmedConfig;
+}
+
+let idGenerator: number = 0;
+
+function newId() {
+  return ++idGenerator;
+}
+
+export function useStepOperations(setConfig: SetRecipeConfig) {
+  const newStep = useCallback(() => setConfig((config) => {
+    const newStep: TaskStepConfig = { type: 'task', task: null, parameters: {} };
+    const newSteps = [...config.steps, newStep];
+    const newStepIds = [...config.stepIds, newId()];
+    return { ...config, steps: newSteps, stepIds: newStepIds };
+  }), [setConfig]);
+
+  const moveStep = useCallback(
+    (from: number, to: number) =>
+      setConfig((config) => {
+        const movedStep = config.steps[from];
+        const newSteps = [...config.steps];
+        newSteps.splice(from, 1);
+        newSteps.splice(to, 0, movedStep);
+
+        const movedStepId = config.stepIds[from];
+        const newStepIds = [...config.stepIds];
+        newStepIds.splice(from, 1);
+        newStepIds.splice(to, 0, movedStepId);
+
+        return { ...config, steps: newSteps, stepIds: newStepIds };
+      }),
+    [setConfig]
+  );
+
+  const deleteStep = useCallback((index: number) => setConfig((config) => {
+    const newSteps = [...config.steps];
+    newSteps.splice(index, 1);
+
+    const newStepIds = [...config.stepIds];
+    newStepIds.splice(index, 1);
+
+    return { ...config, steps: newSteps, stepIds: newStepIds };
+  }), [setConfig]);
+
+  const setStep = useCallback((index: number, newStep: StepConfig) => setConfig((config) => {
+    const newSteps = [...config.steps];
+    newSteps[index] = newStep;
+    return { ...config, steps: newSteps };
+  }), [setConfig]);
+
+  return { newStep, moveStep, deleteStep, setStep };
+}
