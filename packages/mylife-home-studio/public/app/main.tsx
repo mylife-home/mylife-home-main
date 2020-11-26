f3c901a58edb68368ccaf29d2d3358b3d8fff5de
vincent-tr <vincent.tr@gmail.com>
2020-11-26 10:36:17 +0100

    fix import order

--- packages/mylife-home-studio/public/app/main.tsx
+++ packages/mylife-home-studio/public/app/main.tsx
@@ -1,8 +1,9 @@
+import 'regenerator-runtime/runtime.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Application from './components/root/application';
 
-import 'regenerator-runtime/runtime.js';
+import Application from './components/root/application';
 
 ReactDOM.render(
   <Application />,
