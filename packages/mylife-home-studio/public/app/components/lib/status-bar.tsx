96365e77d48d20d649b03d7dd78a5d24dfce72a9
vincent-tr <vincent.tr@gmail.com>
2020-11-26 12:04:57 +0100

    layout

--- packages/mylife-home-studio/public/app/components/lib/status-bar.tsx
+++ packages/mylife-home-studio/public/app/components/lib/status-bar.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Toolbar from '@material-ui/core/Toolbar';
 
-const STATUS_HEIGHT = 36;
+const STATUS_HEIGHT = 24;
 
 const useStyles = makeStyles((theme) => ({
   root: {
