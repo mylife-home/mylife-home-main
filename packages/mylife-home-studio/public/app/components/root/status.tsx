214d59027a8861e245ec43e990b1396f201daae8
vincent-tr <vincent.tr@gmail.com>
2020-11-26 12:04:15 +0100

    wip status

--- packages/mylife-home-studio/public/app/components/root/status.tsx
+++ packages/mylife-home-studio/public/app/components/root/status.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import StatusBar, { StatusSeparator } from '../lib/status-bar';
+
+import { version } from '../../../../package.json';
 
 const useStyles = makeStyles((theme) => ({
 }));
@@ -10,11 +13,26 @@ const useStyles = makeStyles((theme) => ({
 const Status: FunctionComponent<{className ?: string; }> = ({ className }) => {
   return (
     <StatusBar className={className}>
-      TODO left
+      <Version />
+
       <StatusSeparator />
-      TODO right
+
+      <RunningRequests />
+      <Connection />
     </StatusBar>
   );
 };
 
 export default Status;
+
+const Version: FunctionComponent = () => {
+  return <Typography>{`v${version}`}</Typography>;
+}
+
+const Connection: FunctionComponent = () => {
+  return <Typography>Connection</Typography>;
+}
+
+const RunningRequests: FunctionComponent = () => {
+  return <Typography>RunningRequests</Typography>;
+}
