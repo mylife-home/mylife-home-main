a136be97bffcb5be4960c0edec9e1b3dc59fce60
vincent-tr <vincent.tr@gmail.com>
2020-11-27 17:49:17 +0100

    wip project manager

--- packages/mylife-home-studio/src/services/project-manager/index.ts
+++ packages/mylife-home-studio/src/services/project-manager/index.ts
@@ -1,32 +1,19 @@
-import fs from 'fs-extra';
 import { components, tools } from 'mylife-home-common';
 import { Service, BuildParams } from '../types';
-import { Git } from './git';
 
 interface Config {
-  localPath: string;
-  githubUrl: string;
-  githubToken: string;
+  uiPath: string;
+  corePath: string;
 }
 
 export class ProjectManager implements Service {
-  private readonly git: Git;
   private readonly config: Config;
 
   constructor(params: BuildParams) {
     this.config = tools.getConfigItem<Config>('projectManager');
-    this.git = new Git(this.config.githubToken);
   }
 
   async init() {
-    /*
-    const finalPath = path.resolve(this.config.localPath);
-    await fs.emptyDir(finalPath);
-    await this.git.clone({
-      dir: finalPath,
-      url: this.config.githubUrl,
-    });
-    */
   }
 
   async terminate() {
