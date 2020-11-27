2722cb6bc0a0fde97401699dac6507aa93155e81
vincent-tr <vincent.tr@gmail.com>
2020-11-27 17:43:58 +0100

    ui-model

--- packages/mylife-home-studio/shared/ui-model.ts
+++ packages/mylife-home-studio/shared/ui-model.ts
@@ -1,3 +1,16 @@
 // direct import to avoid require all common in ui
 // we depend on mylife-home-ui only for this
 export * from 'mylife-home-ui/dist/shared/model';
+import { Model } from 'mylife-home-ui/dist/shared/model';
+import { Component, Plugin } from './component-model';
+
+export interface UiProject {
+  name: string;
+  model: Model;
+  componentData: ComponentData;
+}
+
+export interface ComponentData {
+  components: Component[]; // plugin points to plugin instanceName:module.name
+  plugins: { [instanceName: string]: Plugin[]; };
+}
