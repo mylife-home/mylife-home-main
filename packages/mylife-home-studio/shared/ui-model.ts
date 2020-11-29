bbd4a37fbdf70c3aa12ccefe59957a847db08bcc
vincent-tr <vincent.tr@gmail.com>
2020-11-29 09:40:45 +0100

    import ui definition stuff

--- packages/mylife-home-studio/shared/ui-model.ts
+++ packages/mylife-home-studio/shared/ui-model.ts
@@ -1,6 +1,7 @@
 // direct import to avoid require all common in ui
 // we depend on mylife-home-ui only for this
 export * from 'mylife-home-ui/dist/shared/model';
+export * from 'mylife-home-ui/dist/src/model/definition';
 import { Model } from 'mylife-home-ui/dist/shared/model';
 import { Component, Plugin } from './component-model';
 
