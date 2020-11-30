681760c8f2c5982c1e1f346d069da685d7d667b4
vincent-tr <vincent.tr@gmail.com>
2020-11-30 21:58:00 +0100

    wip store

--- packages/mylife-home-studio/src/services/project-manager/index.ts
+++ packages/mylife-home-studio/src/services/project-manager/index.ts
@@ -1,13 +1,41 @@
+import { Services } from '..';
+import { Session, SessionNotifier, SessionNotifierManager } from '../session-manager';
 import { Service, BuildParams } from '../types';
+import * as directories from './directories';
+import { Store } from './store';
 
 export class ProjectManager implements Service {
+  private readonly listNotifiers = new SessionNotifierManager('project-manager/list-notifiers', 'project-manager/list');
+  private readonly uiStore = new Store();
+  private readonly coreStore = new Store();
 
   constructor(params: BuildParams) {
   }
 
   async init() {
+    directories.configure();
+    await this.uiStore.init(directories.ui());
+    await this.coreStore.init(directories.core());
+    this.listNotifiers.init();
+
+    Services.instance.sessionManager.registerServiceHandler('project-manager/start-notify-list', this.startNotifyList);
+    Services.instance.sessionManager.registerServiceHandler('project-manager/stop-notify-list', this.stopNotifyList);
   }
 
   async terminate() {
   }
+
+  private readonly startNotifyList = async (session: Session) => {
+    const notifier = this.listNotifiers.createNotifier(session);
+
+    setImmediate(() => {
+      // TODO
+    });
+
+    return { notifierId: notifier.id };
+  };
+
+  private readonly stopNotifyList = async (session: Session, { notifierId }: { notifierId: string; }) => {
+    this.listNotifiers.removeNotifier(session, notifierId);
+  };
 }
---
+++ packages/mylife-home-studio/src/services/project-manager/store.ts
@@ -0,0 +1,107 @@
+import path from 'path';
+import { EventEmitter } from 'events';
+import fs from 'fs-extra';
+import { Mutex } from 'async-mutex';
+import { logger } from 'mylife-home-common';
+
+const log = logger.createLogger('mylife:home:studio:services:project-manager:store');
+
+export interface ProjectBase {
+  name: string;
+}
+
+export abstract class Store<TProject extends ProjectBase> extends EventEmitter {
+  private directory: string;
+  private readonly projects = new Map<string, TProject>();
+  private readonly mutex = new Mutex();
+
+  async init(directory: string) {
+    this.directory = directory;
+
+    log.debug(`init store from: ${this.directory}`);
+
+    await fs.ensureDir(this.directory);
+
+    for (const file of await fs.readdir(this.directory)) {
+      const fullPath = path.join(this.directory, file);
+      const content = await fs.readFile(fullPath, 'utf-8');
+      const project = JSON.parse(content) as TProject;
+      this.projects.set(project.name, project);
+    }
+
+    log.info(`${this.projects.size} projects loaded in store`);
+  }
+
+  async create(name: string) {
+    await this.mutex.runExclusive(async () => {
+      if (this.projects.has(name)) {
+        throw new Error(`A project named '${name}' already exists`);
+      }
+
+      const project = this.initNew(name);
+      project.name = name;
+
+      await this.save(project);
+      this.projects.set(name, project);
+      this.emit('created', name);
+    });
+  }
+
+  async rename(oldName: string, newName: string) {
+    await this.mutex.runExclusive(async () => {
+      const project = this.projects.get(oldName);
+      if (!project) {
+        throw new Error(`Project named '${oldName}' does not exist`);
+      }
+      if (this.projects.has(newName)) {
+        throw new Error(`A project named '${newName}' already exists`);
+      }
+
+      this.projects.delete(oldName);
+      this.projects.set(newName, project);
+      project.name = newName;
+
+      // we change the name inside, so we cannot move
+      await fs.unlink(this.projectFullPath(oldName));
+      await this.save(project);
+      this.emit('renamed', oldName, newName);
+    });
+  }
+
+  async delete(name: string) {
+    await this.mutex.runExclusive(async () => {
+      if (!this.projects.has(name)) {
+        throw new Error(`Project named '${name}' does not exist`);
+      }
+
+      await fs.unlink(this.projectFullPath(name));
+      this.projects.delete(name);
+      this.emit('deleted', name);
+    });
+  }
+
+  protected async update(name: string, updater: (project: TProject) => void) {
+    await this.mutex.runExclusive(async () => {
+      const project = this.projects.get(name);
+      if (!project) {
+        throw new Error(`Project named '${name}' does not exist`);
+      }
+
+      updater(project);
+      await this.save(project);
+      this.emit('updated', name);
+    });
+  }
+
+  private async save(project: TProject) {
+    const fullPath = this.projectFullPath(project.name);
+    const content = JSON.stringify(project);
+    await fs.writeFile(fullPath, content);
+  }
+
+  private projectFullPath(name: string) {
+    return path.join(this.directory, name + '.json');
+  }
+
+  protected abstract initNew(name: string): TProject;
+}
