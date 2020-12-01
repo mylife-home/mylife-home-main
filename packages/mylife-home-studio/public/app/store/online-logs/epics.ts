4a1440bd492e72234eb94c2c826036765df8d7fa
vincent-tr <vincent.tr@gmail.com>
2020-12-01 10:21:53 +0100

    use common notifier

--- packages/mylife-home-studio/public/app/store/online-logs/epics.ts
+++ packages/mylife-home-studio/public/app/store/online-logs/epics.ts
@@ -1,75 +1,20 @@
-import { Action } from 'redux';
-import { Observable } from 'rxjs';
-import { filter, map, mergeMap, withLatestFrom } from 'rxjs/operators';
-import { combineEpics, ofType, StateObservable } from 'redux-observable';
-
 import { LogRecord } from '../../../../shared/logging';
-import { socket } from '../common/rx-socket';
-import { AppState } from '../types';
-import { ActionTypes as TabActionTypes } from '../tabs/types';
+import { createNotifierEpic } from '../common/notifier-epic';
 import { setNotification, clearNotification, addLogItems } from './actions';
 import { hasOnlineLogsTab, getNotifierId } from './selectors';
-import { bufferDebounceTime, filterNotification, handleError, withSelector } from '../common/rx-operators';
 import { LogItem } from './types';
 
-const startNotifyLogsEpic = (action$: Observable<Action>, state$: StateObservable<AppState>) => action$.pipe(
-  filterNotifyChange(state$),
-  withSelector(state$, getNotifierId),
-  filter(([, notifierId]) => !notifierId),
-  mergeMap(() => startCall().pipe(
-    map(({ notifierId }) => setNotification(notifierId)),
-    handleError()
-  ))
-);
-
-const stopNotifyLogsEpic = (action$: Observable<Action>, state$: StateObservable<AppState>) => action$.pipe(
-  filterNotifyChange(state$),
-  withSelector(state$, getNotifierId),
-  filter(([, notifierId]) => !!notifierId),
-  mergeMap(([, notifierId]) => stopCall({ notifierId }).pipe(
-    map(() => clearNotification()),
-    handleError()
-  ))
-);
-
-const fetchLogsEpic = (action$: Observable<Action>, state$: StateObservable<AppState>) => {
-  const notification$ = socket.notifications();
-  return notification$.pipe(
-    filterNotification('logging/logs'),
-    withSelector(state$, getNotifierId),
-    filter(([notification, notifierId]) => notification.notifierId === notifierId),
-    map(([notification]) => parseLogRecord(notification.data)),
-    bufferDebounceTime(100), // debounce to avoid multiple store updates
-    map((items) => addLogItems(items)),
-  );
-};
-
-export default combineEpics(startNotifyLogsEpic, stopNotifyLogsEpic, fetchLogsEpic);
-
-function filterNotifyChange(state$: StateObservable<AppState>) {
-  return (source: Observable<Action>) => source.pipe(
-    ofType(TabActionTypes.NEW, TabActionTypes.CLOSE),
-    withLatestFrom(state$),
-    filter(([, state]) => {
-      const hasTab = hasOnlineLogsTab(state);
-      const hasNotifications = !!getNotifierId(state);
-      return xor(hasTab, hasNotifications);
-    }),
-    map(([action]) => action)
-  );
-}
-
-function xor(a: boolean, b: boolean) {
-  return a && !b || !a && b;
-}
-
-function startCall() {
-  return socket.call('logging/start-notify-logs', null) as Observable<{ notifierId: string; }>;
-}
-
-function stopCall({ notifierId }: { notifierId: string; }) {
-  return socket.call('logging/stop-notify-logs', { notifierId }) as Observable<void>;
-}
+export default createNotifierEpic({
+  notificationType: 'logging/logs',
+  startNotifierService: 'logging/start-notify-logs',
+  stopNotifierService: 'logging/stop-notify-logs',
+  getNotifierId,
+  hasTypedTab: hasOnlineLogsTab,
+  setNotification,
+  clearNotification,
+  applyUpdates: addLogItems,
+  parseUpdate: parseLogRecord,
+});
 
 let idGenerator = 0;
 
