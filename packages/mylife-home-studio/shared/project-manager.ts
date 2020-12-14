8fc888b5ca670ae948722138716cb8f9cd0a4113
vincent-tr <vincent.tr@gmail.com>
2020-12-14 10:37:57 +0100

    wip notification model

--- packages/mylife-home-studio/shared/project-manager.ts
+++ packages/mylife-home-studio/shared/project-manager.ts
@@ -1,4 +1,4 @@
-import { Definition } from './ui-model';
+import { Window, Definition, DefinitionResource } from './ui-model';
 import { Component, Plugin } from './component-model';
 
 export interface UiProject {
@@ -55,14 +55,45 @@ export interface UiProjectInfo extends ProjectInfo {
 }
 
 export interface CoreProjectInfo extends ProjectInfo {
-  
+
 }
 
 export interface UpdateProjectNotification {
-  operation: 'set-name';
+  operation: 'set-name'
+  | 'set-ui-default-window' | 'set-ui-component-data' | 'set-ui-resource' | 'clear-ui-resource' | 'set-ui-window' | 'clear-ui-window';
 }
 
 export interface SetNameProjectNotification extends UpdateProjectNotification {
   operation: 'set-name';
   name: string;
+}
+
+export interface SetUiDefaultWindowProjectNotification extends UpdateProjectNotification {
+  operation: 'set-ui-default-window';
+  defaultWindow: { [type: string]: string; };
+}
+
+export interface SetUiComponentDataNotification extends UpdateProjectNotification {
+  operation: 'set-ui-component-data';
+  componentData: ComponentData;
+}
+
+export interface SetUiResourceNotification extends UpdateProjectNotification {
+  operation: 'set-ui-resource';
+  resource: DefinitionResource;
+}
+
+export interface ClearUiResourceNotification extends UpdateProjectNotification {
+  operation: 'clear-ui-resource';
+  id: string;
+}
+
+export interface SetUiWindowNotification extends UpdateProjectNotification {
+  operation: 'set-ui-window';
+  window: Window;
+}
+
+export interface ClearUiWindowNotification extends UpdateProjectNotification {
+  operation: 'clear-ui-window';
+  id: string;
 }
