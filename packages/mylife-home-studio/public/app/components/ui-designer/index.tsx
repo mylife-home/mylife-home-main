b40663a2860c1be1aed688df05cfc3e67102d564
vincent-tr <vincent.tr@gmail.com>
2021-01-03 12:03:25 +0100

    cleanup

--- packages/mylife-home-studio/public/app/components/ui-designer/index.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/index.tsx
@@ -14,9 +14,3 @@ const UiDesigner: FunctionComponent = () => (
 );
 
 export default UiDesigner;
-
-// TODO
-// - state+debounce comme recipe au niveau d'une fenêtre
-// - les ressources en live
-// - l'update de composants sur le serveur
-// - rename resource/window
