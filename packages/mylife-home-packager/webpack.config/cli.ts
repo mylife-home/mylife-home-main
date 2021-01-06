b07f4dc449292e89c9745dea4df1597f07761e92
vincent-tr <vincent.tr@gmail.com>
2021-01-06 12:22:42 +0100

    experiment direct webpack watch

--- packages/mylife-home-packager/webpack.config/cli.ts
+++ packages/mylife-home-packager/webpack.config/cli.ts
@@ -1,9 +1,14 @@
 import { Configuration } from 'webpack';
-import { createContext, getConfigurationFactories } from '.';
+import { createContext, getConfigurationFactories, getConfigurationFactory } from '.';
 
 export default (env: { [name: string]: any }) => {
   const { mode, ...options } = env;
   const context = createContext(mode, options);
+
+  if (env.binary && env.part) {
+    const configurationFactory = getConfigurationFactory(env.binary, env.part);
+    return configurationFactory(context);
+  }
 
   const configurations: Configuration[] = [];
   for (const configurationFactory of getConfigurationFactories()) {
