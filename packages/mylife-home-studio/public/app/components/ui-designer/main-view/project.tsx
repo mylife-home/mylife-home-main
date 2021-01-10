b4cc01d4ff190c9360b67c84ab41d086bfda1b6c
vincent-tr <vincent.tr@gmail.com>
2021-01-10 14:35:44 +0100

    wip project

--- packages/mylife-home-studio/public/app/components/ui-designer/main-view/project.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/project.tsx
@@ -1,11 +1,40 @@
 import React, { FunctionComponent } from 'react';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Project: FunctionComponent = () => (
-  <Box>
-    <Typography>Project</Typography>
-  </Box>
-);
+import { Container, Title } from '../../lib/main-view-layout';
+import { ProjectIcon } from '../../lib/icons';
+import { useTabSelector } from '../../lib/use-tab-selector';
+import { getDefaultWindow } from '../../../store/ui-designer/selectors';
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+
+    display: 'flex',
+    flexDirection: 'row',
+  },
+}));
+
+const Project: FunctionComponent = () => {
+  const classes = useStyles();
+  const defaultWindow = useTabSelector(getDefaultWindow);
+  
+  return (
+    <Container
+      title={
+        <Title text="Projet" icon={ProjectIcon} />
+      }
+    >
+      <div className={classes.wrapper}>
+        TODO
+        {JSON.stringify(defaultWindow)}
+      </div>
+    </Container>
+  );
+};
 
 export default Project;
