409af4c99ac1ff43f276a8ad4913b66efc6a701b
vincent-tr <vincent.tr@gmail.com>
2021-01-11 11:12:06 +0100

    resources selector

--- packages/mylife-home-studio/public/app/components/ui-designer/main-view/common/resource-selector.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/common/resource-selector.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Autocomplete, { AutocompleteRenderInputParams } from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 import { useTabSelector } from '../../../lib/use-tab-selector';
 import { getResourcesIds } from '../../../../store/ui-designer/selectors';
@@ -14,8 +15,8 @@ const useStyles = makeStyles((theme) => ({
   image: {
     width: 16,
     height: 16,
-    marginRight: theme.spacing(1)
-  }
+    marginRight: theme.spacing(1),
+  },
 }));
 
 export interface ResourceSelectorProps {
@@ -23,49 +24,51 @@ export interface ResourceSelectorProps {
   label?: string;
   value: string;
   onChange: (value: string) => void;
-} 
+}
 
-const ResourceSelector: FunctionComponent<ResourceSelectorProps> = ({nullable = false,  label, value, onChange }) => {
-  const classes= useStyles();
+const ResourceSelector: FunctionComponent<ResourceSelectorProps> = ({ nullable = false, label, value, onChange }) => {
+  const classes = useStyles();
   const resourcesIds = useTabSelector(getResourcesIds);
-  
+
   return (
     <Autocomplete
       disableClearable={!nullable}
       options={resourcesIds}
       className={classes.component}
-      renderOption={(option) => (<OptionRenderer option={option} />)}
-      renderInput={(params) => (<InputRenderer params={params} label={label} value={value} />)}
+      renderOption={(option) => <OptionRenderer option={option} />}
+      renderInput={(params) => <InputRenderer params={params} label={label} value={value} />}
       value={value}
-      onChange={(event: any, newValue: string) => { onChange(newValue); }}
+      onChange={(event: any, newValue: string) => {
+        onChange(newValue);
+      }}
     />
   );
 };
 
 export default ResourceSelector;
 
-const InputRenderer: FunctionComponent<{ params: AutocompleteRenderInputParams; label?: string; value: string; }> = ({ params, label, value }) => {
-  const editorValue = (params.inputProps as { value: string; })?.value;
+const InputRenderer: FunctionComponent<{ params: AutocompleteRenderInputParams; label?: string; value: string }> = ({ params, label, value }) => {
+  const classes = useStyles();
+  const editorValue = (params.inputProps as { value: string })?.value;
   const showImage = value && editorValue === value; // do not show image while editing
 
-  return (
-    <>
-      {showImage && <InputImageRenderer value={value} />}
-      <TextField {...params} label={label} variant="outlined" />
-    </>
-  )
+  params.InputProps.startAdornment = (
+    <InputAdornment position="start">
+      {showImage ? (
+        <Image resource={value} className={classes.image} />
+      ) : (
+        <div className={classes.image} />
+      )}
+    </InputAdornment>
+  );
+
+  console.log(params);
+
+  return <TextField {...params} label={label} variant="outlined" />;
 };
 
-const InputImageRenderer: FunctionComponent<{ value: string; }> = ({ value }) => {
-  const classes= useStyles();
-
-  return (
-    <Image resource={value} className={classes.image} />
-  );
-}
-
-const OptionRenderer: FunctionComponent<{ option: string; }> = ({ option }) => {
-  const classes= useStyles();
+const OptionRenderer: FunctionComponent<{ option: string }> = ({ option }) => {
+  const classes = useStyles();
 
   return (
     <>
@@ -74,4 +77,3 @@ const OptionRenderer: FunctionComponent<{ option: string; }> = ({ option }) => {
     </>
   );
 };
-
