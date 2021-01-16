12b671611990d59300252603510f228fa841a3ae
vincent-tr <vincent.tr@gmail.com>
2021-01-16 22:07:58 +0100

    wip editor dialog

--- packages/mylife-home-studio/public/app/components/ui-designer/main-view/window/properties/code-editor.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/window/properties/code-editor.tsx
@@ -4,6 +4,13 @@ import 'codemirror/mode/javascript/javascript.js';
 import React, { FunctionComponent } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import { EditorConfiguration } from 'codemirror';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  component: {
+    fontSize: 12,
+  },
+}), { name: 'properties-code-editor'});
 
 const options: EditorConfiguration = {
   mode: 'javascript',
@@ -12,8 +19,9 @@ const options: EditorConfiguration = {
 }
 
 const CodeEditor: FunctionComponent<{ value: string; onChange: (newValue: string) => void; }> = ({ value, onChange }) => {
+  const classes = useStyles();
   return (
-    <CodeMirror options={options} value={value} onBeforeChange={(editor, data, value) => onChange(value)} />
+    <CodeMirror className={classes.component} options={options} value={value} onBeforeChange={(editor, data, value) => onChange(value)} />
   );
 };
 
--- packages/mylife-home-studio/public/app/components/ui-designer/main-view/window/properties/control-text-format-editor-dialog.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/window/properties/control-text-format-editor-dialog.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useModal } from 'react-modal-hook';
 import { Transition } from 'react-transition-group'; // used by material-ui
+import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -10,11 +11,18 @@ import Button from '@material-ui/core/Button';
 import { ControlText, ControlTextContextItem } from '../../../../../../../shared/ui-model';
 import CodeEditor from './code-editor';
 
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    height: 'calc(100% - 64px)', // same than fullWidth
+  },
+}), { name: 'properties-format-editor-dialog'});
+
 type TransitionProps = Transition<HTMLElement>['props'];
 
 export type DialogResult = { status: 'ok' | 'cancel'; format?: string };
 
 export function useFormatEditorDialog() {
+  const classes = useStyles();
   const [format, setFormat] = useState<string>();
   const [context, setContext] = useState<ControlTextContextItem[]>();
   const [onResult, setOnResult] = useState<(value: DialogResult) => void>();
@@ -31,11 +39,11 @@ export function useFormatEditorDialog() {
     };
 
     return (
-      <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={cancel} scroll="paper" maxWidth="sm" fullWidth>
+      <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={cancel} maxWidth="xl" fullWidth classes={{ paper: classes.dialog }}>
         <DialogTitle id="dialog-title">Format</DialogTitle>
 
         <DialogContent dividers>
-          TODO: code editor (display code mirror + pouvoir tester la sortie en fournissant des valeurs de context)
+          TODO: pouvoir tester la sortie en fournissant des valeurs de context
           <CodeEditor value={format} onChange={setFormat} />
         </DialogContent>
 
