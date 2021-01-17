b28fed200a19d7068b66a3ec4eefd8282c4cc537
vincent-tr <vincent.tr@gmail.com>
2021-01-17 15:40:15 +0100

    wip test value editor

---
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/window/properties/test-value-editor.tsx
@@ -0,0 +1,107 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { parseType, Range, Enum } from './member-types';
+
+export interface TestValueEditorProps {
+  valueType: string;
+  value: any;
+  onChange: (newValue: any) => void;
+}
+
+const TestValueEditor: FunctionComponent<TestValueEditorProps> = ({ valueType, value, onChange }) => {
+  const type = useType(valueType);
+
+  switch (type.name) {
+    case 'range': {
+      const rangeType = type as Range;
+      return (
+        <TextField
+          value={formatNumber(value)}
+          onChange={(e) => onChange(parseNumber(e.target.value, 'int'))}
+          type="number"
+          inputProps={{ min: rangeType.min, max: rangeType.max }}
+        />
+      );
+    }
+
+    case 'text':
+      return (
+        <TextField
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      );
+
+    case 'float':
+      return (
+        <TextField
+          value={formatNumber(value)}
+          onChange={(e) => onChange(parseNumber(e.target.value, 'float'))}
+          type="number"
+        />
+      );
+
+    case 'bool':
+      return (
+        <Checkbox
+          color="primary"
+          checked={(value as boolean) || false}
+          onChange={(e) => onChange(e.target.checked)}
+        />
+      );
+
+    case 'enum': {
+      const enumType = type as Enum;
+      return (
+        <Select value={value} onChange={e => onChange(e.target.value)}>
+          {enumType.values.map(option => (
+            <MenuItem key={option} value={option}>{option}</MenuItem>
+          ))}
+        </Select>
+      );
+    }
+
+    case 'complex':
+      return (
+        <TextField
+          value={formatComplex(value)}
+          onChange={(e) => onChange(parseComplex(e.target.value))}
+        />
+      );
+
+    default:
+      throw new Error(`Unsupported type: ${type.name}`)
+  }
+};
+
+export default TestValueEditor;
+
+function useType(value: string) {
+  return useMemo(() => (value ? parseType(value) : null), [value]);
+}
+
+function formatNumber(value: number) {
+  return value === null ? '' : value.toString(10);
+}
+
+function parseNumber(value: string, type: 'float' | 'int') {
+  const number = type === 'float' ? parseFloat(value) : parseInt(value, 10);
+  return isNaN(number) ? null : number;
+}
+
+function formatComplex(value: any) {
+  return value === null ? '' : JSON.stringify(value);
+}
+
+function parseComplex(value: string) {
+  try {
+    return JSON.parse(value);
+  } catch(err) {
+    console.log('Error parsing JSON', err);
+    return null;
+  }
+}
