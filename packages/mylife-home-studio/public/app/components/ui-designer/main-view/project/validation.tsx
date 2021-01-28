20dea4b88d72088a61d67f6ba7a43be32c07c332
vincent-tr <vincent.tr@gmail.com>
2021-01-28 15:33:45 +0100

    wip validation display

--- packages/mylife-home-studio/public/app/components/ui-designer/main-view/project/validation.tsx
+++ packages/mylife-home-studio/public/app/components/ui-designer/main-view/project/validation.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from 'react-modal-hook';
 import { Transition } from 'react-transition-group'; // used by material-ui
@@ -8,16 +8,18 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 import { DialogText } from '../../../dialogs/common';
 import { useTabPanelId } from '../../../lib/tab-panel';
 import { useFireAsync } from '../../../lib/use-error-handling';
 import { validateProject } from '../../../../store/ui-designer/actions';
-import ElementPathBreadcrumbs from '../common/element-path-breadcrumbs';
-import { UiValidationError } from '../../../../../../shared/project-manager';
+import { ElementPathNode } from '../common/element-path-breadcrumbs';
+import { UiValidationError, UiElementPath, UiElementPathNode } from '../../../../../../shared/project-manager';
 import { useSnackbar } from '../../../dialogs/snackbar';
 
 type TransitionProps = Transition<HTMLElement>['props'];
@@ -39,7 +41,7 @@ export function useProjectValidation() {
 
   return useCallback(() => {
     fireAsync(async () => {
-      const errors = await dispatch(validateProject({ id: tabId })) as unknown as UiValidationError[];
+      const errors = ((await dispatch(validateProject({ id: tabId }))) as unknown) as UiValidationError[];
       if (errors.length === 0) {
         enqueueSnackbar('Le projet a été validé sans erreur.', { variant: 'success' });
       } else {
@@ -51,62 +53,148 @@ export function useProjectValidation() {
 
 function useShowDialog() {
   const classes = useStyles();
-  const [errors, setErrors] = useState<UiValidationError[]>();
+  const [treeData, setTreeData] = useState<TreeData>();
   const [onClose, setOnClose] = useState<() => void>();
 
-  const [showModal, hideModal] = useModal(({ in: open, onExited }: TransitionProps) => {
+  const [showModal, hideModal] = useModal(
+    ({ in: open, onExited }: TransitionProps) => {
+      const close = () => {
+        hideModal();
+        onClose();
+      };
 
-    const close = () => {
-      hideModal();
-      onClose();
-    };
+      const handleKeyDown = (e: React.KeyboardEvent) => {
+        switch (e.key) {
+          case 'Enter':
+          case 'Escape':
+            close();
+            break;
+        }
+      };
 
-    const handleKeyDown = (e: React.KeyboardEvent) => {
-      switch (e.key) {
-        case 'Enter':
-        case 'Escape':
-          close();
-          break;
+      return (
+        <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={close} scroll="paper" maxWidth="lg" fullWidth onKeyDown={handleKeyDown}>
+          <DialogTitle id="dialog-title">Erreurs de validation</DialogTitle>
+
+          <DialogContent dividers>
+            <DialogText value={'Le projet a les erreurs de validation suivantes :'} />
+
+            <TreeView className={classes.list} defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />} defaultExpanded={treeData.nodeIds}>
+              {treeData.roots.map((node) => (
+                <TreeNode node={node} key={node.id} />
+              ))}
+            </TreeView>
+
+            <DialogText value={`${treeData.errorsCount} erreurs`} />
+          </DialogContent>
+
+          <DialogActions>
+            <Button color="primary" onClick={close}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      );
+    },
+    [treeData, onClose]
+  );
+
+  return (errors: UiValidationError[]) =>
+    new Promise<void>((resolve) => {
+      setTreeData(buildTreeData(errors));
+      setOnClose(() => resolve); // else useState think resolve is a state updater
+
+      showModal();
+    });
+}
+
+const TreeNode: FunctionComponent<{ node: Node }> = ({ node }) => {
+  const { id, children } = node;
+
+  return (
+    <TreeItem nodeId={id} label={label(node)}>
+      {children.map((node) => (
+        <TreeNode node={node} key={node.id} />
+      ))}
+    </TreeItem>
+  );
+};
+
+function label(node: Node) {
+  switch (node.type) {
+    case 'path': {
+      const pathNode = node as PathNode;
+      return <ElementPathNode node={pathNode.node} />;
+    }
+
+    case 'message': {
+      const messageNode = node as MessageNode;
+      return <Typography color="error">{messageNode.message}</Typography>;
+    }
+  }
+}
+
+interface TreeData {
+  roots: Node[];
+  nodeIds: string[];
+  errorsCount: number;
+}
+
+interface Node {
+  id: string;
+  type: 'path' | 'message';
+  children: Node[];
+}
+
+interface MessageNode extends Node {
+  type: 'message';
+  message: string;
+}
+
+interface PathNode extends Node {
+  type: 'path';
+  node: UiElementPathNode;
+}
+
+function buildTreeData(errors: UiValidationError[]) {
+  // TODO: merge common paths
+  const nodeIds: string[] = [];
+  const errorsCount = errors.length;
+
+  const roots = errors.map((error, index) => {
+    let root: Node;
+    let current: Node;
+
+    for (const [depth, node] of error.path.entries()) {
+      nodeIds.push(`${index}-${depth}`);
+      const newNode: PathNode = {
+        id: `${index}-${depth}`,
+        type: 'path',
+        children: [],
+        node,
+      };
+
+      if (current) {
+        current.children.push(newNode);
+        current = newNode;
+      } else {
+        root = newNode;
+        current = newNode;
       }
+    }
+
+    nodeIds.push(`${index}`);
+    const message: MessageNode = {
+      id: `${index}`,
+      type: 'message',
+      children: [],
+      message: error.message,
     };
 
-    return (
-      <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={close} scroll="paper" maxWidth="lg" fullWidth onKeyDown={handleKeyDown}>
-        <DialogTitle id="dialog-title">Erreurs de validation</DialogTitle>
+    current.children.push(message);
 
-        <DialogContent dividers>
-          <DialogText value={'Le projet a les erreurs de validation suivantes :'} />
-
-          <List className={classes.list}>
-            {errors.map((error, index) => (
-              <React.Fragment key={index}>
-                <ListItem>
-                  <ElementPathBreadcrumbs item={error.path} />
-                </ListItem>
-
-                <ListItem>
-                  <Typography>{error.message}</Typography>
-                </ListItem>
-
-              </React.Fragment>
-            ))}
-          </List>
-
-          <DialogText value={`${errors.length} erreurs`} />
-
-        </DialogContent>
-
-        <DialogActions>
-          <Button color="primary" onClick={close}>OK</Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }, [errors, onClose]);
-
-  return (errors: UiValidationError[]) => new Promise<void>(resolve => {
-    setErrors(errors);
-    setOnClose(() => resolve); // else useState think resolve is a state updater
-
-    showModal();
+    return root;
   });
+
+  return { roots, nodeIds, errorsCount };
 }
