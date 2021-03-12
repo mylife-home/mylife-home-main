150072546ec869d45d59a9b5326c45531f74f653
vincent-tr <vincent.tr@gmail.com>
2021-03-12 15:03:37 +0100

    split actions

---
+++ packages/mylife-home-studio/public/app/components/core-designer/selection-panel/component/actions.tsx
@@ -0,0 +1,98 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+
+import DeleteButton from '../../../lib/delete-button';
+import { useTabPanelId } from '../../../lib/tab-panel';
+import { useFireAsync } from '../../../lib/use-error-handling';
+import { useCanvasTheme } from '../../drawing/theme';
+import { Rectangle } from '../../drawing/types';
+import { computeComponentRect } from '../../drawing/shapes';
+import { useSelection } from '../../selection';
+import CenterButton from '../center-button';
+import { useRenameDialog } from '../../../dialogs/rename';
+
+import { AppState } from '../../../../store/types';
+import * as types from '../../../../store/core-designer/types';
+import { getComponentIds, getComponent, getPlugin } from '../../../../store/core-designer/selectors';
+import { clearComponent, renameComponent } from '../../../../store/core-designer/actions';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'row',
+  },
+}), { name: 'properties-component-actions' });
+
+const Actions: FunctionComponent = () => {
+  const classes = useStyles();
+  const { componentIds, component, plugin, clear, rename } = useActionsConnect();
+  const componentCenterPosition = useCenterComponent(component, plugin);
+  const fireAsync = useFireAsync();
+  const showRenameDialog = useRenameDialog(componentIds, component.id, 'Entrer un nom de composant');
+  
+  const onRename = () =>
+    fireAsync(async () => {
+      const { status, newName } = await showRenameDialog();
+      if (status === 'ok') {
+        rename(newName);
+      }
+    });
+
+  return (
+    <div className={classes.container}>
+      <CenterButton position={componentCenterPosition} />
+
+      <Tooltip title="Renommer">
+        <IconButton onClick={onRename}>
+          <EditIcon />
+        </IconButton>
+      </Tooltip>
+
+      <DeleteButton icon tooltip="Supprimer" onConfirmed={clear} />
+    </div>
+  );
+};
+
+export default Actions;
+
+function useActionsConnect() {
+  const tabId = useTabPanelId();
+  const { selection } = useSelection();
+  const dispatch = useDispatch();
+
+  const componentId = selection.id;
+  const component = useSelector((state: AppState) => getComponent(state, tabId, componentId));
+  const plugin = useSelector((state: AppState) => getPlugin(state, tabId, component.plugin));
+  const componentIds = useSelector((state: AppState) => getComponentIds(state, tabId));
+
+  const { clear, rename } = useMemo(() => ({
+    clear: () => {
+      dispatch(clearComponent({ id: tabId, componentId }));
+    },
+    rename: (newId: string) => {
+      dispatch(renameComponent({ id: tabId, componentId, newId }));
+    },
+  }), [tabId, dispatch, componentId]);
+
+  return { componentIds, component, plugin, clear, rename };
+}
+
+function useCenterComponent(component: types.Component, plugin: types.Plugin) {
+  const theme = useCanvasTheme();
+  return useMemo(() => {
+    const rect = computeComponentRect(theme, component, plugin);
+    return computeCenter(rect);
+  }, [theme, component]);
+}
+
+function computeCenter(rect: Rectangle) {
+  return {
+    x: rect.x + rect.width / 2,
+    y: rect.y + rect.height / 2
+  };
+}
--- packages/mylife-home-studio/public/app/components/core-designer/selection-panel/component/index.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/selection-panel/component/index.tsx
@@ -1,39 +1,27 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
-import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
-import DeleteButton from '../../../lib/delete-button';
 import { useTabPanelId } from '../../../lib/tab-panel';
 import { useTabSelector } from '../../../lib/use-tab-selector';
-import { useFireAsync } from '../../../lib/use-error-handling';
 import { StateIcon, ActionIcon } from '../../../lib/icons';
-import { useCanvasTheme } from '../../drawing/theme';
-import { Rectangle } from '../../drawing/types';
-import { computeComponentRect } from '../../drawing/shapes';
 import { useSelection } from '../../selection';
-import CenterButton from '../center-button';
 import { Group, Item } from '../../../lib/properties-layout';
-import { useRenameDialog } from '../../../dialogs/rename';
 
 import { AppState } from '../../../../store/types';
 import * as types from '../../../../store/core-designer/types';
-import { getComponentIds, getComponent, getPlugin, getBinding } from '../../../../store/core-designer/selectors';
-import { clearComponent, renameComponent } from '../../../../store/core-designer/actions';
+import { getComponent, getPlugin, getBinding } from '../../../../store/core-designer/selectors';
+
+import Actions from './actions';
 
 const useStyles = makeStyles((theme) => ({
-  actions: {
-    margin: theme.spacing(1),
-    display: 'flex',
-    flexDirection: 'row',
-  },
   bindingLink: {
     display: 'flex',
   },
@@ -84,36 +72,6 @@ const Component: FunctionComponent<{ className?: string; }> = ({ className }) =>
 };
 
 export default Component;
-
-const Actions: FunctionComponent = () => {
-  const classes = useStyles();
-  const { componentIds, component, plugin, clear, rename } = useActionsConnect();
-  const componentCenterPosition = useCenterComponent(component, plugin);
-  const fireAsync = useFireAsync();
-  const showRenameDialog = useRenameDialog(componentIds, component.id, 'Entrer un nom de composant');
-  
-  const onRename = () =>
-    fireAsync(async () => {
-      const { status, newName } = await showRenameDialog();
-      if (status === 'ok') {
-        rename(newName);
-      }
-    });
-
-  return (
-    <div className={classes.actions}>
-      <CenterButton position={componentCenterPosition} />
-
-      <Tooltip title="Renommer">
-        <IconButton onClick={onRename}>
-          <EditIcon />
-        </IconButton>
-      </Tooltip>
-
-      <DeleteButton icon tooltip="Supprimer" onConfirmed={clear} />
-    </div>
-  );
-};
 
 const Configuration: FunctionComponent = () => {
   const { component, plugin } = useComponentData();
@@ -235,47 +193,10 @@ function getBindingDisplay(binding: types.Binding, memberType: types.MemberType)
   }
 }
 
-function useActionsConnect() {
-  const tabId = useTabPanelId();
-  const { selection } = useSelection();
-  const dispatch = useDispatch();
-
-  const componentId = selection.id;
-  const component = useSelector((state: AppState) => getComponent(state, tabId, componentId));
-  const plugin = useSelector((state: AppState) => getPlugin(state, tabId, component.plugin));
-  const componentIds = useSelector((state: AppState) => getComponentIds(state, tabId));
-
-  const { clear, rename } = useMemo(() => ({
-    clear: () => {
-      dispatch(clearComponent({ id: tabId, componentId }));
-    },
-    rename: (newId: string) => {
-      dispatch(renameComponent({ id: tabId, componentId, newId }));
-    },
-  }), [tabId, dispatch, componentId]);
-
-  return { componentIds, component, plugin, clear, rename };
-}
-
 function useComponentData() {
   const tabId = useTabPanelId();
   const { selection } = useSelection();
   const component = useSelector((state: AppState) => getComponent(state, tabId, selection.id));
   const plugin = useSelector((state: AppState) => getPlugin(state, tabId, component.plugin));
   return { component, plugin };
-}
-
-function useCenterComponent(component: types.Component, plugin: types.Plugin) {
-  const theme = useCanvasTheme();
-  return useMemo(() => {
-    const rect = computeComponentRect(theme, component, plugin);
-    return computeCenter(rect);
-  }, [theme, component]);
-}
-
-function computeCenter(rect: Rectangle) {
-  return {
-    x: rect.x + rect.width / 2,
-    y: rect.y + rect.height / 2
-  };
 }
