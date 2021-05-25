a4f64b3608bd0253e8b55e9119a628c0ae6e6116
vincent-tr <vincent.tr@gmail.com>
2021-05-25 16:59:03 +0200

    clean snackbars

--- packages/mylife-home-studio/public/app/components/dialogs/provider.tsx
+++ packages/mylife-home-studio/public/app/components/dialogs/provider.tsx
@@ -1,18 +1,45 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, createRef } from 'react';
 import { ModalProvider } from 'react-modal-hook';
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider, SnackbarKey } from 'notistack';
 import { TransitionGroup } from 'react-transition-group'; // used by material-ui
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import ErrorDialog from './error';
 import ConnectingDialog from './connecting';
 
-const DialogProvider: FunctionComponent = ({ children }) => (
-  <SnackbarProvider dense>
-    <ModalProvider rootComponent={TransitionGroup}>
-      <ErrorDialog />
-      <ConnectingDialog />
-      {children}
-    </ModalProvider>
-  </SnackbarProvider>
-);
+const DialogProvider: FunctionComponent = ({ children }) => {
+  return (
+    <SnackbarCustomizedProvider>
+      <ModalProvider rootComponent={TransitionGroup}>
+        <ErrorDialog />
+        <ConnectingDialog />
+        {children}
+      </ModalProvider>
+    </SnackbarCustomizedProvider>
+  );
+};
 
 export default DialogProvider;
+
+const SnackbarCustomizedProvider: FunctionComponent = ({ children }) => {
+  const notistackRef = createRef<SnackbarProvider>();
+
+  const onClickDismiss = (key: SnackbarKey) => () => { 
+    notistackRef.current.closeSnackbar(key);
+  };
+
+  return (
+    <SnackbarProvider
+      dense
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      ref={notistackRef}
+      action={(key) => (
+        <IconButton onClick={onClickDismiss(key)} color='inherit'>
+          <CloseIcon />
+        </IconButton>
+      )}
+    >
+      {children}
+    </SnackbarProvider>
+  );
+};
