597ab561d54f0f18b190e7968ef849ba0a17ff1d
vincent-tr <vincent.tr@gmail.com>
2021-05-26 08:41:48 +0200

    core deploy ui

--- packages/mylife-home-studio/public/app/components/core-designer/actions.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/actions.tsx
@@ -1,17 +1,34 @@
 import React, { FunctionComponent } from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import PublishIcon from '@material-ui/icons/Publish';
-import { ToolbarIconButton } from '../lib/toolbar';
+import { ToolbarIconButton, IconWithBadge } from '../lib/toolbar';
+import { InstanceIcon } from '../lib/icons';
+import { FileIcon } from '../deploy/icons';
 
 const Actions: FunctionComponent<{ className?: string }> = ({ className }) => {
 
   return (
     <Toolbar className={className}>
 
-      <ToolbarIconButton title="Déployer" icon={<PublishIcon />} onClick={() => console.log('TODO')} />
+      <ToolbarIconButton title="Déployer vers un fichier de configuration de déploiement" icon={<DeployToFileIcon />} onClick={() => console.log('TODO')} />
+      <ToolbarIconButton title="Déployer sur une instance en ligne" icon={<DeployOnlineIcon />} onClick={() => console.log('TODO')} />
 
     </Toolbar>
   );
 }
 
 export default Actions;
+
+const DeployToFileIcon: FunctionComponent = () => (
+  <IconWithBadge
+    main={<PublishIcon />}
+    badge={<FileIcon />}
+  />
+);
+
+const DeployOnlineIcon: FunctionComponent = () => (
+  <IconWithBadge
+    main={<PublishIcon />}
+    badge={<InstanceIcon />}
+  />
+);
