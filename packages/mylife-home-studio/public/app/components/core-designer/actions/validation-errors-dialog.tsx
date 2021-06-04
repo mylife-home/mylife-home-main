d58254d416727ef0fa96be3f2ebfd0db3bd66b58
vincent-tr <vincent.tr@gmail.com>
2021-06-04 10:19:16 +0200

    validation errors dialog can now be confirm dialog

--- packages/mylife-home-studio/public/app/components/core-designer/actions/validation-errors-dialog.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/actions/validation-errors-dialog.tsx
@@ -9,10 +9,10 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Typography from '@material-ui/core/Typography';
 
 import { TransitionProps, DialogText } from '../../dialogs/common';
+import { ConfirmResult } from '../../dialogs/confirm';
 import { CoreValidationError } from '../../../store/core-designer/types';
 
 const useStyles = makeStyles((theme) => ({
@@ -28,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function useShowValidationErrorsDialog() {
-  const classes = useStyles();
   const [errors, setErrors] = useState<CoreValidationError[]>();
   const [onClose, setOnClose] = useState<() => void>();
 
@@ -48,25 +47,14 @@ export function useShowValidationErrorsDialog() {
         }
       };
 
+      const actions = (
+        <Button color="primary" onClick={close}>
+          OK
+        </Button>
+      );
+
       return (
-        <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={close} scroll="paper" maxWidth="lg" fullWidth onKeyDown={handleKeyDown}>
-          <DialogTitle id="dialog-title">Erreurs de validation</DialogTitle>
-
-          <DialogContent dividers>
-            <DialogText value={'Le projet a les erreurs de validation suivantes :'} />
-
-            <List className={classes.list}>
-              {errors.map((error, index) => (<ErrorItem key={index} error={error} />))}
-            </List>
-
-          </DialogContent>
-
-          <DialogActions>
-            <Button color="primary" onClick={close}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <ErrorDialog open={open} onExited={onExited} onClose={close} onKeyDown={handleKeyDown} errors={errors} actions={actions} />
       );
     },
     [errors, onClose]
@@ -83,6 +71,105 @@ export function useShowValidationErrorsDialog() {
     [setErrors, setOnClose]
   );
 }
+
+export function useConfirmValidationErrorsDialog() {
+  const [errors, setErrors] = useState<CoreValidationError[]>();
+  const [onResult, setOnResult] = useState<(value: ConfirmResult) => void>();
+
+  const [showModal, hideModal] = useModal(
+    ({ in: open, onExited }: TransitionProps) => {
+      const cancel = () => {
+        hideModal();
+        onResult({ status: 'cancel' });
+      };
+
+      const validate = () => {
+        hideModal();
+        onResult({ status: 'ok' });
+      };
+
+      const handleKeyDown = (e: React.KeyboardEvent) => {
+        switch (e.key) {
+          case 'Enter':
+            validate();
+            break;
+
+          case 'Escape':
+            cancel();
+            break;
+        }
+      };
+
+      const contentBottom=(
+        <DialogText value={'Continuer ?'} />
+      );
+
+      const actions = (
+        <>
+          <Button color="primary" onClick={validate}>
+            Oui
+          </Button>
+
+          <Button onClick={cancel}>
+            Non
+          </Button>
+        </>
+      );
+
+      return (
+        <ErrorDialog open={open} onExited={onExited} onClose={close} onKeyDown={handleKeyDown} errors={errors} contentBottom={contentBottom} actions={actions} />
+      );
+    },
+    [errors, onResult]
+  );
+
+  return useCallback(
+    (errors: CoreValidationError[]) =>
+      new Promise<ConfirmResult>((resolve) => {
+        setErrors(errors);
+        setOnResult(() => resolve); // else useState think resolve is a state updater
+
+        showModal();
+      }),
+    [setErrors, setOnResult]
+  );
+}
+
+interface ErrorDialogProps {
+  open: boolean;
+  onExited: () => void;
+  onClose: () => void;
+  onKeyDown: (e: React.KeyboardEvent) => void;
+
+  errors: CoreValidationError[];
+  contentBottom?: React.ReactNode;
+  actions: React.ReactNode;
+}
+
+const ErrorDialog: FunctionComponent<ErrorDialogProps> = ({ open, onExited, onClose, onKeyDown, errors, contentBottom, actions }) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog aria-labelledby="dialog-title" open={open} onExited={onExited} onClose={close} scroll="paper" maxWidth="lg" fullWidth onKeyDown={onKeyDown}>
+      <DialogTitle id="dialog-title">Erreurs de validation</DialogTitle>
+
+      <DialogContent dividers>
+        <DialogText value={'Le projet a les erreurs de validation suivantes :'} />
+
+        <List className={classes.list}>
+          {errors.map((error, index) => (<ErrorItem key={index} error={error} />))}
+        </List>
+
+        {contentBottom}
+
+      </DialogContent>
+
+      <DialogActions>
+        {actions}
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 const CHANGE_TYPES = {
   add: 'Plugin ajouté', // Note: ne devrait pas apparaître
