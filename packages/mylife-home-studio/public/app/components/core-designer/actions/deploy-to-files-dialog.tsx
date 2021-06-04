0710748cf0177aaea2189c8c192f039b6e3ccd21
vincent-tr <vincent.tr@gmail.com>
2021-06-04 16:28:15 +0200

    wip deploy dialog

--- packages/mylife-home-studio/public/app/components/core-designer/actions/deploy-to-files-dialog.tsx
+++ packages/mylife-home-studio/public/app/components/core-designer/actions/deploy-to-files-dialog.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback, useState, useMemo, useEffect } from 'react';
+import React, { FunctionComponent, useCallback, useState, useMemo } from 'react';
 import { useModal } from 'react-modal-hook';
 import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,19 +9,21 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Collapse from '@material-ui/core/Collapse';
-import Checkbox from '@material-ui/core/Checkbox';
-import Typography from '@material-ui/core/Typography';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import { ConfirmResult } from '../../dialogs/confirm';
 import { TransitionProps, DialogText } from '../../dialogs/common';
-import { coreImportData, DeployChanges } from '../../../../../shared/project-manager';
+import { DeployChanges } from '../../../../../shared/project-manager';
 import DeployChangesList from './deploy-changes-list';
 
 const useStyles = makeStyles((theme) => ({
+  selector: {
+    width: 200,
+  },
+  separator: {
+    height: theme.spacing(8),
+  },
   changesList: {
     height: '40vh',
   },
@@ -35,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
 type ChangesDialogResult = ConfirmResult & { bindingsInstanceName?: string };
 
 interface DialogData {
-  bindingsInstanceName: { actual: string, needed: boolean };
+  bindingsInstanceName: { actual: string; needed: boolean };
   changes: DeployChanges;
   files: string[];
 }
@@ -50,6 +52,8 @@ export function useShowDhowDeployToFilesDialog() {
     ({ in: open, onExited }: TransitionProps) => {
       const { changes, bindingsInstanceName, files } = data;
       const currentBindingsInstanceName = bindingsInstanceName.needed ? selectedbindingsInstanceName : bindingsInstanceName.actual;
+      const needBindingsInstanceName = bindingsInstanceName.needed;
+      const canValidate = !needBindingsInstanceName || !!selectedbindingsInstanceName;
 
       const cancel = () => {
         hideModal();
@@ -57,6 +61,10 @@ export function useShowDhowDeployToFilesDialog() {
       };
 
       const validate = () => {
+        if (!canValidate) {
+          return;
+        }
+
         hideModal();
         onResult({ status: 'ok', bindingsInstanceName: 'TODO' });
       };
@@ -78,16 +86,22 @@ export function useShowDhowDeployToFilesDialog() {
           <DialogTitle id="dialog-title">Opérations à effectuer pour le déploiement</DialogTitle>
 
           <DialogContent dividers>
+            {needBindingsInstanceName && (
+              <>
+                <DialogText value={`L'instance de déploiement des bindings n'a pas pu être identifiée. Veuillez la sélectionner.`} />
+                <BindingsInstanceNameSelector className={classes.selector} changes={changes} value={selectedbindingsInstanceName} onChange={selectBindingsInstanceName} />
+                <Separator />
+              </>
+            )}
+
             <DialogText value={'Les changements suivants vont être effectués :'} />
+            <DeployChangesList className={classes.changesList} changes={changes} bindingsInstanceName={currentBindingsInstanceName} />
 
-            <DeployChangesList className={classes.changesList} changes={changes} bindingsInstanceName={currentBindingsInstanceName}/>
-
-            <DialogText value={'bindingsInstanceName'} />
+            <Separator />
 
             <DialogText value={'Les fichiers suivants vont être créés :'} />
-
             <List className={classes.filesList}>
-              {files.map(file => (
+              {files.map((file) => (
                 <ListItem key={file}>
                   <ListItemText primary={file} />
                 </ListItem>
@@ -101,7 +115,7 @@ export function useShowDhowDeployToFilesDialog() {
           </DialogContent>
 
           <DialogActions>
-            <Button color="primary" onClick={validate}>
+            <Button color="primary" onClick={validate} disabled={!canValidate}>
               OK
             </Button>
             <Button onClick={cancel}>Annuler</Button>
@@ -113,7 +127,7 @@ export function useShowDhowDeployToFilesDialog() {
   );
 
   return useCallback(
-    (bindingsInstanceName: { actual: string, needed: boolean }, changes: DeployChanges, files: string[]) =>
+    (bindingsInstanceName: { actual: string; needed: boolean }, changes: DeployChanges, files: string[]) =>
       new Promise<ChangesDialogResult>((resolve) => {
         setData({ bindingsInstanceName, changes, files });
         selectBindingsInstanceName(null);
@@ -124,3 +138,35 @@ export function useShowDhowDeployToFilesDialog() {
     [setData, setOnResult, showModal]
   );
 }
+
+const Separator: FunctionComponent = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.separator} />
+  )
+}
+
+const BindingsInstanceNameSelector: FunctionComponent<{ className?: string; changes: DeployChanges; value: string; onChange: (newValue: string) => void; }> = ({ className, changes, value, onChange }) => {
+  const instancesNamesList = useMemo(() => buildInstancesNamesList(changes), [changes]);
+
+  return (
+    <Autocomplete
+      className={className}
+      value={value}
+      onChange={(event: React.ChangeEvent, newValue: string) => onChange(newValue)}
+      options={instancesNamesList}
+      disableClearable
+      renderInput={(params) => <TextField {...params} />}
+    />
+  );
+};
+
+function buildInstancesNamesList(changes: DeployChanges) {
+  const instances = new Set<string>();
+  for (const change of changes.components) {
+    instances.add(change.instanceName);
+  }
+
+  return Array.from(instances).sort();
+}
+
