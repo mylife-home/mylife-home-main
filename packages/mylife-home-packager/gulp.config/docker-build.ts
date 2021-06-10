9c5eb244a605d25d51d322be5df5f4a09bf2c13d
vincent-tr <vincent.tr@gmail.com>
2021-06-10 21:44:09 +0200

    cleanup

--- packages/mylife-home-packager/gulp.config/docker-build.ts
+++ packages/mylife-home-packager/gulp.config/docker-build.ts
@@ -1,7 +1,6 @@
 import path from 'path';
-import { spawn } from 'child_process';
 import { src, dest, series, parallel } from 'gulp';
-import { noop, name, withTempPath } from './utils';
+import { noop, name, withTempPath, run } from './utils';
 
 export interface DockerTaskOptions {
   readonly config: string;
@@ -18,20 +17,15 @@ export function createDockerTask(options: DockerTaskOptions) {
       name('copy config', () => src([makeSourcePath(options.config), '!**/Dockerfile']).pipe(dest(tempPathRef.path))),
       name('copy binaries', () => src([makeSourcePath(options.binaries), '!**/*.report.html']).pipe(dest(tempPathRef.path)))
     ),
-    name('docker build', () => runDockerBuild(tempPathRef.path, path.join(basePath, options.config, 'Dockerfile'), options.imageTag)),
-    options.publish ? name('docker push', () => runDockerPublish(options.imageTag)) : noop,
+    name('docker build', () => {
+      const dockerFilePath =  path.join(basePath, options.config, 'Dockerfile');
+      return run('docker', 'build', '--no-cache', '-t', options.imageTag, '-f', dockerFilePath, tempPathRef.path);
+    }),
+    options.publish ? name('docker push', () => run('docker', 'push', options.imageTag)) : noop,
     name('summary', async () => { console.log(`create docker image: '${options.imageTag}'`) }),
   ));
 
   function makeSourcePath(relative: string) {
     return path.join(basePath, relative) + '/**/*';
   }
-}
-
-function runDockerBuild(contextPath: string, dockerfilePath: string, imageTag: string) {
-  return spawn('docker', ['build', '--no-cache', '-t', imageTag, '-f', dockerfilePath, contextPath], { stdio: [null, null, process.stderr] });
-}
-
-function runDockerPublish(imageTag: string) {
-  return spawn('docker', ['push', imageTag], { stdio: [null, null, process.stderr] });
 }
