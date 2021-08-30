85f7b5ceba50f0b9993cc9fc74ab33798180a2c1
vincent-tr <vincent.tr@gmail.com>
2021-08-30 22:29:33 +0200

    wip mounted-fs draft

--- packages/mylife-home-core/src/store/operations/mounted-fs.ts
+++ packages/mylife-home-core/src/store/operations/mounted-fs.ts
@@ -1,22 +1,42 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import child_process from 'child_process';
 import { StoreOperations, StoreConfiguration } from '../common';
 import { StoreItem } from '../model';
 
 interface MountedFsStoreConfiguration extends StoreConfiguration {
+  readonly path: string;
+  readonly mountPoint: string;
 }
 
 export class MountedFsStoreOperations implements StoreOperations {
+  private readonly storeFile: string;
+  private readonly mountPoint: string;
+
   constructor(configuration: StoreConfiguration) {
     const typedConfig = configuration as MountedFsStoreConfiguration;
-    throw new Error('TODO');
+    this.storeFile = path.resolve(typedConfig.path);
+    this.mountPoint = typedConfig.mountPoint;
   }
 
   async load() {
-    throw new Error('TODO');
-    const array: StoreItem[] = [];
-    return array;
+    const content = await fs.readFile(this.storeFile, 'utf-8');
+    return JSON.parse(content) as StoreItem[];
   }
 
   async save(items: StoreItem[]) {
-    throw new Error('TODO');
+    const content = JSON.stringify(items, null, 2);
+    await exec(`mount -o remount,rw ${this.mountPoint}`);
+    try {
+      await fs.writeFile(this.storeFile, content);
+    } finally {
+      await exec(`mount -o remount,ro ${this.mountPoint}`);
+    }
   }
+}
+
+async function exec(command: string) {
+  await new Promise<void>((resolve, reject) => {
+    child_process.exec(command, (error) => error ? reject(error) : resolve());
+  });
 }
