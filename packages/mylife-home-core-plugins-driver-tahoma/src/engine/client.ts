99cd633a0529a231d6e7e79530dbda15e43fef87
vincent-tr <vincent.tr@gmail.com>
2021-09-27 10:55:15 +0200

    wip client

--- packages/mylife-home-core-plugins-driver-tahoma/src/engine/client.ts
+++ packages/mylife-home-core-plugins-driver-tahoma/src/engine/client.ts
@@ -1,9 +1,12 @@
-// Wrapper around overkiz-client to improve typing
+// Wrapper around overkiz-client to improve typings
 
-import { Client } from 'overkiz-client';
-import { logger } from 'mylife-home-common';
+import EventEmitter from 'events';
+import { Action, Client as OverkizClient, Command, Device, Execution } from 'overkiz-client';
+import { logger, tools } from 'mylife-home-common';
 
 const log = logger.createLogger('mylife:home:core:plugins:driver-tahoma:engine:client');
+
+export { Device };
 
 // Wrap calls: it logger's last argument is the error if any; other arguments are concatenated
 function rewriteArguments(args: any[]) {
@@ -11,7 +14,7 @@ function rewriteArguments(args: any[]) {
   let error: Error;
 
   for (const arg of args) {
-    if(arg instanceof Error) {
+    if (arg instanceof Error) {
       error = arg;
     } else {
       strings.push(arg as string);
@@ -35,26 +38,23 @@ const apiLogger = {
   error(...args: any[]) {
     log.error.apply(log, rewriteArguments(args));
   },
-}
-
-const client = new Client();
-client.execute()
+};
 
 export interface Config {
   /**
    * Poll for execution events every 5 seconds by default (in seconds)
-   */ 
-  readonly execPollingPeriod: number; 
+   */
+  readonly execPollingPeriod: number;
 
   /**
-   * // Poll for events every 60 seconds by default (in seconds)
+   * Poll for events every 60 seconds by default (in seconds)
    */
-   readonly pollingPeriod: number;
+  readonly pollingPeriod: number;
 
-   /**
-    * Refresh device states every 30 minutes by default (in minutes)
-    */
-   readonly refreshPeriod: number;
+  /**
+   * Refresh device states every 30 minutes by default (in minutes)
+   */
+  readonly refreshPeriod: number;
 
   /**
    * default: tahoma
@@ -63,4 +63,78 @@ export interface Config {
 
   readonly user: string;
   readonly password: string;
+}
+
+export declare interface Client extends EventEmitter {
+  on(event: 'onlineChanged', listener: (online: boolean) => void): this;
+  off(event: 'onlineChanged', listener: (online: boolean) => void): this;
+  once(event: 'onlineChanged', listener: (online: boolean) => void): this;
+}
+
+export class Client extends EventEmitter {
+  private readonly impl: OverkizClient;
+  private _online = false;
+  private readonly executionByDevice = new Map<string, string>();
+
+  constructor(config: Config) {
+    super();
+
+    this.impl = new OverkizClient(apiLogger, config);
+
+    // Register to connected and disconnected events of impl.restClient.
+    // Note: the restClient variable is assigned only once in ctor
+    const restClient = (this.impl as any).restClient as EventEmitter;
+
+    restClient.on('connect', () => {
+      this.setOnline(true);
+      tools.fireAsync(async () => {
+        // refresh immediately
+        // Note: refreshTask is private
+        await (this.impl as any).refreshTask();
+      });
+    });
+
+    restClient.on('disconnect', () => {
+      this.setOnline(false);
+    });
+  }
+
+  get devices() {
+    // Devices is declared as array but is used as object by deviceURL
+    const devices: { [deviceUrl: string]: Device; } = (this.impl as any).devices;
+    return devices;
+  }
+
+  get online() {
+    return this._online;
+  }
+
+  private setOnline(value: boolean) {
+    if (this._online !== value) {
+      this._online = value;
+      this.emit('onlineChanged', value);
+    }
+  }
+
+  async execute(deviceURL: string, name: string, ...args: any[]) {
+    const existingExecId = this.executionByDevice.get(deviceURL);
+    if (existingExecId && this.impl.hasExecution(existingExecId)) {
+      await this.cancelExecution(existingExecId);
+    }
+
+    const execId = await this.startExecution(deviceURL, name, args);
+    this.executionByDevice.set(deviceURL, execId);
+  }
+
+  private async startExecution(deviceURL: string, name: string, args: any[]) {
+    const cmd = new Command(name, ...args);
+    const action = new Action(deviceURL, [cmd]);
+    const exec = new Execution('MyLife Home command', action);
+    const execId: string = await this.impl.execute('apply', exec);
+    return execId;
+  }
+
+  private async cancelExecution(execId: string) {
+    return await this.impl.cancelExecution(execId);
+  }
 }
