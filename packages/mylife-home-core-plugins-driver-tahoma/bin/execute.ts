56b12e05c4a09394b5222813893c29e4bd14e68b
vincent-tr <vincent.tr@gmail.com>
2021-10-01 14:57:11 +0200

    execute with events

---
+++ packages/mylife-home-core-plugins-driver-tahoma/bin/execute.ts
@@ -0,0 +1,47 @@
+import { tools, logger } from 'mylife-home-common';
+import { API } from '../src/engine/api';
+import { Event, ExecutionStateChangedEvent } from '../src/engine/api-types/event';
+
+tools.injectConfig({ logging: { console: true } });
+logger.readConfig();
+
+const [, , username, password, deviceURL, command, ...args] = process.argv;
+
+async function main() {
+  try {
+    const api = new API(username, password);
+    const listenerId = await api.registerEvents();
+    const execId = await api.execute({ actions: [{ deviceURL, commands: [{ name: command, parameters: args }] }] });
+
+    while (true) {
+      const events = await api.fetchEvents(listenerId);
+      for (const event of events) {
+        if (processEvent(event, execId)) {
+          return;
+        }
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function processEvent(untypedEvent: Event, execId: string) {
+  if (untypedEvent.name !== 'ExecutionStateChangedEvent') {
+    return false;
+  }
+
+  const event = untypedEvent as ExecutionStateChangedEvent;
+  if (event.execId !== execId) {
+    return;
+  }
+
+  console.log(event.newState);
+
+  return event.timeToNextState === -1;
+}
+
+main();
+
