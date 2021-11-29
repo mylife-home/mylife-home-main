7918c000b7e40b6e57f0772b64d7dd1bb10d6b7a
vincent-tr <vincent.tr@gmail.com>
2021-11-29 10:33:44 +0100

    update pirev types

--- packages/mylife-home-common/src/types/pirev.d.ts
+++ packages/mylife-home-common/src/types/pirev.d.ts
@@ -1,17 +1,20 @@
-declare function getInfoSync(): Result;
-declare function getInfo(): Promise<Result>;
+declare function getInfoSync(filename?: string): Result;
+declare function getInfo(filename?: string): Promise<Result>;
 
 export interface Result {
+  cpus: { [name: string]: number | string }[];
+  hardware: string;
   revision: Revision;
+  serial: string;
+  model: string;
 }
 
 export interface Revision {
-  type: '2B';
-  memory: '256MB' | '512MB' | '1GB' | '2GB' | '4GB' | '8GB';
-  processor: 'BCM2835' | 'BCM2836' | 'BCM2837' | 'BCM2711';
-  revision: 1.1;
-  manufacturer: 'Egoman' | 'Embest' | 'Qisda' | 'Sony Japan' | 'Sony UK' | 'Stadium';
-
+  type: string;
+  memory: string; // '256MB' | '512MB' | '1GB' | '2GB' | '4GB' | '8GB';
+  processor: string; // 'BCM2835' | 'BCM2836' | 'BCM2837' | 'BCM2711';
+  revision: number;
+  manufacturer: string; // 'Egoman' | 'Embest' | 'Qisda' | 'Sony Japan' | 'Sony UK' | 'Stadium';
   overvoltage?: boolean;
   otp?: { program: boolean; read: boolean };
   warranty?: boolean;
@@ -23,15 +26,62 @@ pirev
 
 ReferenceError: No revision code found
 
+{
+  cpus: [
+    {
+      processor: 0,
+      bogomips: 38.4,
+      features: 'fp asimd evtstrm crc32 cpuid',
+      cpu_implementer: 65,
+      cpu_architecture: 8,
+      cpu_variant: 0,
+      cpu_part: 3331,
+      cpu_revision: 4
+    },
+    {
+      processor: 1,
+      bogomips: 38.4,
+      features: 'fp asimd evtstrm crc32 cpuid',
+      cpu_implementer: 65,
+      cpu_architecture: 8,
+      cpu_variant: 0,
+      cpu_part: 3331,
+      cpu_revision: 4
+    },
+    {
+      processor: 2,
+      bogomips: 38.4,
+      features: 'fp asimd evtstrm crc32 cpuid',
+      cpu_implementer: 65,
+      cpu_architecture: 8,
+      cpu_variant: 0,
+      cpu_part: 3331,
+      cpu_revision: 4
+    },
+    {
+      processor: 3,
+      bogomips: 38.4,
+      features: 'fp asimd evtstrm crc32 cpuid',
+      cpu_implementer: 65,
+      cpu_architecture: 8,
+      cpu_variant: 0,
+      cpu_part: 3331,
+      cpu_revision: 4
+    }
+  ],
+  hardware: 'BCM2835',
   revision: {
-    type: '2B',
+    type: '3B',
     memory: '1GB',
-    processor: 'BCM2836',
-    revision: 1.1,
-    manufacturer: 'Embest',
+    processor: 'BCM2837',
+    revision: 1.2,
+    manufacturer: 'Sony UK',
     overvoltage: true,
     otp: { program: true, read: true },
     warranty: true,
-    code: 'a21041'
-  }
+    code: 'a02082'
+  },
+  serial: '00000000957219d5',
+  model: 'Raspberry Pi 3 Model B Rev 1.2'
+}
 */
