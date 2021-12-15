b903b6aa5c17d257cb9cc02d9da6273b41e8d7c0
vincent-tr <vincent.tr@gmail.com>
2021-12-15 11:16:15 +0100

    fix timeout increase

--- packages/mylife-home-studio/src/services/online/index.ts
+++ packages/mylife-home-studio/src/services/online/index.ts
@@ -137,7 +137,7 @@ export class Online implements Service {
 
   async coreStoreSave(instanceName: string) {
     this.checkInstanceCapability(instanceName, 'store-api');
-    await this.transport.rpc.call(instanceName, 'store.save', 5000); // store save may be slow on rpi
+    await this.transport.rpc.call(instanceName, 'store.save', null, 5000); // store save may be slow on rpi
   }
 
   async executeComponentAction({ componentId, action, value }: { componentId: string, action: string, value: any; }) {
