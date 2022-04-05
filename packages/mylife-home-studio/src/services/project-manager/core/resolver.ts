429537271aadef8710a50439033ec8759ad91d72
vincent-tr <vincent.tr@gmail.com>
2022-04-05 15:03:22 +0200

    instantiation of templates

--- packages/mylife-home-studio/src/services/project-manager/core/resolver.ts
+++ packages/mylife-home-studio/src/services/project-manager/core/resolver.ts
@@ -1,6 +1,6 @@
 import { logger } from 'mylife-home-common';
-import { CoreComponentConfiguration, CorePluginData } from '../../../../shared/project-manager';
-import { BindingModel, PluginModel, ProjectModel, TemplateModel, BindingView, ComponentView, InstanceView, PluginView, ResolvedProjectView } from './model';
+import { CoreComponentConfiguration, CorePluginData, CoreTemplateExports, CoreTemplateMemberExport } from '../../../../shared/project-manager';
+import { BindingModel, PluginModel, ProjectModel, TemplateModel, BindingView, ComponentView, InstanceView, PluginView, ResolvedProjectView, ComponentModel, ComponentDefinitionModel, ViewModel } from './model';
 
 const log = logger.createLogger('mylife:home:studio:services:project-manager:core:resolver');
 
@@ -22,44 +22,121 @@ export function resolveProject(projectModel: ProjectModel): ResolvedProjectView 
     instanceView.plugins.set(pluginView.id, pluginView);
     projectView.plugins.set(pluginView.id, pluginView);
   }
+  
+  const builders: { [id: string]: ComponentBuilder } = {};
 
   for (const componentId of projectModel.getComponentsIds()) {
     const componentModel = projectModel.getComponent(componentId);
+    const { config, external } = componentModel.data;
 
-    if (componentModel.definition instanceof PluginModel) {
-      const pluginView = projectView.getPlugin(componentModel.definition.id);
-      const { config, external } = componentModel.data;
-      const componentView = new ComponentViewImpl(componentId, pluginView, config, external);
+    builders[componentId] = ComponentBuilder.create(projectView, componentId, componentModel.definition, config, external);
+  }
 
-      projectView.components.set(componentView.id, componentView);
-      pluginView.usage.set(componentView.id, componentView);
-      (pluginView.instance as InstanceViewImpl).usage.set(componentView.id, componentView);
+  buildBindings(projectView, projectModel, builders);
 
-      continue;
+  return projectView;
+}
+
+abstract class ComponentBuilder {
+  static create(projectView: ResolvedProjectViewImpl, componentId: string, definition: ComponentDefinitionModel, config: CoreComponentConfiguration, external: boolean) {
+    if (definition instanceof PluginModel) {
+      return new DirectComponentBuilder(projectView, componentId, definition, config, external);
     }
     
-    if (componentModel.definition instanceof TemplateModel) {
-      // TODO
-      throw new Error('TODO');
-
-      continue;
+    if (definition instanceof TemplateModel) {
+      return new TemplateBuilder(projectView, componentId, definition, config, external);
     }
-
+  
     throw new Error('Unknown component definition');
   }
 
-  for (const bindingId of projectModel.getBindingsIds()) {
-    const bindingModel = projectModel.getBinding(bindingId);
-    const sourceComponentView = projectView.getComponent(bindingModel.sourceComponent.id);
-    const targetComponentView = projectView.getComponent(bindingModel.targetComponent.id);
-    const bindingView = new BindingViewImpl(sourceComponentView, bindingModel.sourceState, targetComponentView, bindingModel.targetAction);
+  abstract buildBindings(): void;
 
-    projectView.bindings.set(bindingView.id, bindingView);
-    sourceComponentView.bindingsFrom.add(bindingView);
-    targetComponentView.bindingsTo.add(bindingView);
+  abstract getMemberData(id: string): { component: ComponentViewImpl; memberName: string; }
+}
+
+class DirectComponentBuilder extends ComponentBuilder {
+  private componentView: ComponentViewImpl;
+
+  constructor(projectView: ResolvedProjectViewImpl, componentId: string, pluginModel: PluginModel, config: CoreComponentConfiguration, external: boolean) {
+    super();
+
+    const pluginView = projectView.getPlugin(pluginModel.id);
+    const componentView = new ComponentViewImpl(componentId, pluginView, config, external);
+
+    projectView.components.set(componentView.id, componentView);
+    pluginView.usage.set(componentView.id, componentView);
+    (pluginView.instance as InstanceViewImpl).usage.set(componentView.id, componentView);
+
+    this.componentView = componentView;
   }
 
-  return projectView;
+  buildBindings() {
+  }
+
+  getMemberData(id: string) {
+    return { 
+      component: this.componentView, 
+      memberName: id
+    };
+  }
+}
+
+class TemplateBuilder extends ComponentBuilder {
+  private readonly children: { [id: string]: ComponentBuilder } = {};
+
+  constructor(private readonly projectView: ResolvedProjectViewImpl, componentId: string, private readonly templateModel: TemplateModel, config: CoreComponentConfiguration, external: boolean) {
+    super();
+    
+    const configToApply = new Map<string, CoreComponentConfiguration>();
+
+    for (const [id, item] of Object.entries(templateModel.data.exports.config)) {
+      let componentConfig = configToApply.get(item.component);
+      if (!componentConfig) {
+        componentConfig = {};
+        configToApply.set(item.component, componentConfig);
+      }
+  
+      componentConfig[item.configName] = config[id];
+    }
+  
+    for (const templateComponentId of templateModel.getComponentsIds()) {
+      const templateComponent = templateModel.getComponent(templateComponentId);
+      const id = templateComponent.id.replace('{{id}}', componentId);
+      const componentConfig = { ...templateComponent.data.config, ...configToApply.get(templateComponent.id) };
+
+      this.children[templateComponentId] = ComponentBuilder.create(projectView, id, templateComponent.definition, componentConfig, false);
+    }
+  }
+
+  buildBindings() {
+    buildBindings(this.projectView, this.templateModel, this.children);
+  }
+
+  getMemberData(id: string) {
+    const exportData = this.templateModel.data.exports.members[id];
+    const component = this.children[exportData.component];
+    return component.getMemberData(exportData.member);
+  }
+}
+
+function buildBindings(projectView: ResolvedProjectViewImpl, viewModel: ViewModel, builders: { [id: string]: ComponentBuilder }) {
+
+  for (const bindingId of viewModel.getBindingsIds()) {
+    const bindingModel = viewModel.getBinding(bindingId);
+
+    const sourceHalf = builders[bindingModel.sourceComponent.id].getMemberData(bindingModel.sourceState);
+    const targetHalf = builders[bindingModel.targetComponent.id].getMemberData(bindingModel.targetAction);
+
+    const bindingView = new BindingViewImpl(sourceHalf.component, sourceHalf.memberName, targetHalf.component, bindingModel.targetAction);
+    projectView.bindings.set(bindingView.id, bindingView);
+    sourceHalf.component.bindingsFrom.add(bindingView);
+    targetHalf.component.bindingsTo.add(bindingView);
+  }
+
+  for (const builder of Object.values(builders)) {
+    builder.buildBindings();
+  }
 }
 
 export class ResolvedProjectViewImpl implements ResolvedProjectView {
