68ddcda06eace1f515aacd58c3100dfe6cf958d9
vincent-tr <vincent.tr@gmail.com>
2022-04-11 09:22:46 +0200

    apply updates

--- packages/mylife-home-studio/src/services/project-manager/core/import/update-apply.ts
+++ packages/mylife-home-studio/src/services/project-manager/core/import/update-apply.ts
@@ -1,163 +1,225 @@
 import { logger } from 'mylife-home-common';
-import { coreImportData, BulkUpdatesStats } from '../../../../../shared/project-manager';
-import { ProjectModel, PluginModel, TemplateModel, BindingModel, ComponentModel } from '../model';
-import { ImportData, PluginImport, ComponentImport, loadOnlineData, loadProjectData } from './load';
-import { prepareChanges } from './diff';
+import { BulkUpdatesStats } from '../../../../../shared/project-manager';
+import { PluginImport, ComponentImport } from './load';
 import { UpdateServerData, Update, PluginSetUpdate, PluginClearUpdate, ComponentSetUpdate, ComponentClearUpdate, ComponentConfigUpdate, BindingClearUpdate, TemplateClearExportsUpdate } from './update-types';
-import { buildUpdates } from './update-builder';
 
 const log = logger.createLogger('mylife:home:studio:services:project-manager:core:import');
 
 export interface UpdateApi {
   setPlugin: (plugin: PluginImport) => void;
   clearPlugin: (pluginId: string) => void;
-  setComponent: (component: ComponentImport) => void;
+  setComponent: (component: ComponentImport) => void; // always directly on project
   resetComponentConfig: (templateId: string, componentId: string, configId: string) => void;
   clearComponentConfig: (templateId: string, componentId: string, configId: string) => void;
-  clearComponent: (templateId: string, componentId: string) => void;
+  clearComponent: (tcomponentId: string) => void; // always directly on project
   clearBinding: (templateId: string, bindingId: string) => void;
   clearTemplateExport: (templateId: string, exportType: 'config' | 'member', exportId: string) => void;
 }
 
-export function applyUpdates(serverData: UpdateServerData, selection: Set<string>, api: UpdateApi) {
-  const stats: BulkUpdatesStats = {
-    plugins: 0,
-    components: 0,
-    templates: 0,
-    bindings: 0,
+class ApplyContext {
+  // We must keep tracks on updated component to avoid to apply clear/reset of config (triggered by plugin updates) on already updated components
+  private readonly updatedComponents = new Set<string>();
+
+  readonly stats = {
+    plugins: new Set<string>(),
+    components: new Set<string>(),
+    templates: new Set<string>(),
+    bindings: new Set<string>(),
   };
 
+  constructor(private readonly selection: Set<string>, readonly api: UpdateApi) {
+  }
+
+  isSelected(update: Update) {
+    for (const key of update.objectChangeKeys) {
+      if (this.selection.has(key)) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+
+  statsAddPlugin(pluginId: string) {
+    this.stats.plugins.add(pluginId);
+  }
+
+  statsAddComponent(templateId: string, componentId: string) {
+    this.stats.components.add(`${templateId || ''}:${componentId}`);
+  }
+
+  statsAddTemplate(templateId: string) {
+    this.stats.templates.add(templateId);
+  }
+
+  statsAddBinding(templateId: string, bindingId: string) {
+    this.stats.bindings.add(`${templateId || ''}:${bindingId}`);
+  }
+
+  markComponentUpdated(id: string) {
+    this.updatedComponents.add(id);
+  }
+
+  isComponentUpdated(id: string) {
+    return this.updatedComponents.has(id);
+  }
+
+  buildStats() {
+    const stats: BulkUpdatesStats = {
+      plugins: this.stats.plugins.size,
+      components: this.stats.components.size,
+      templates: this.stats.templates.size,
+      bindings: this.stats.bindings.size,
+    };
+
+    return stats;
+  }
+}
+
+/*
+updates cn be applied in this order:
+'binding-clear' => no deps
+'component-clear' => always root update
+'plugin-set' => before component-set
+'component-set' => before plugin-clear, in case of pluginId update
+'plugin-clear'
+'template-clear-export' => no checks on exports data
+'component-clear-config' => no checks on config data
+'component-reset-config' => use template recursion to get config type, must be applied after template/plugin updates
+*/
+
+const ORDER: readonly Update['type'][] = [
+  'binding-clear',
+  'component-clear',
+  'plugin-set',
+  'component-set',
+  'plugin-clear',
+  'template-clear-export',
+  'component-clear-config',
+  'component-reset-config',
+];
+
+export function applyUpdates(serverData: UpdateServerData, selection: Set<string>, api: UpdateApi) {
+  const context = new ApplyContext(selection, api);
+
   log.info('Starting update');
+  
+  const updates: { [type in Update['type']]: Update[]} = {
+    'plugin-set': [],
+    'plugin-clear': [],
+    'component-set': [],
+    'component-clear': [],
+    'component-reset-config': [],
+    'component-clear-config': [],
+    'binding-clear': [],
+    'template-clear-export': [],
+  };
 
   for (const update of serverData.updates) {
-    if (!shouldApply(update, selection)) {
-      continue;
-    }
-
-    for (const impact of update.impacts) {
-      applyPrevImpact(impact, api, stats);
-    }
-
-    applyUpdate(update, api, stats);
-
-    for (const impact of update.impacts) {
-      applyNextImpact(impact, api, stats);
+    if (context.isSelected(update)) {
+      updates[update.type].push(update);
     }
   }
 
+  for (const type of ORDER) {
+    for (const update of updates[type]) {
+      applyUpdate(context, update);
+    }
+  }
+
+  const stats = context.buildStats();
   log.info(`Updated (${stats.plugins} plugins, ${stats.components} components, ${stats.templates} templates, ${stats.bindings} bindings)`);
 
   return stats;
 }
 
-function applyPrevImpact(impact: Impact, api: UpdateApi, stats: BulkUpdatesStats) {
-  switch (impact.type) {
-    case 'binding-delete': {
-      const typedImpact = impact as BindingDeleteImpact;
-      log.debug(`Impact: delete binding '${typedImpact.bindingId}' (template='${typedImpact.templateId}')`);
-      api.clearBinding(typedImpact.templateId, typedImpact.bindingId);
-      ++stats.bindings;
-      break;
-    }
-
-    case 'component-delete': {
-      const typedImpact = impact as ComponentDeleteImpact;
-      log.debug(`Impact: delete component '${typedImpact.componentId}' (template='${typedImpact.templateId}')`);
-      api.clearComponent(typedImpact.templateId, typedImpact.componentId);
-      ++stats.components;
-      break;
-    }
-
-    case 'component-config-clear':
-    case 'component-config-reset':
-      // will be applied after
-      break;
-
-    default:
-      throw new Error(`Unsupported impact type: '${impact.type}'`);
-  }
-}
-
-function applyNextImpact(impact: Impact, api: UpdateApi, stats: BulkUpdatesStats) {
-  switch (impact.type) {
-    case 'binding-delete':
-    case 'component-delete':
-      // applied before
-      break;
-
-    case 'component-config-clear':
-    case 'component-config-reset': {
-      const type = impact.type === 'component-config-clear' ? 'clear' : 'reset';
-      const typedImpact = impact as ComponentConfigImpact;
-      log.debug(`Impact: config component '${type}' -> '${typedImpact.componentId}' - '${typedImpact.configId}' (template='${typedImpact.templateId}')`);
-      api.updateComponentConfig(typedImpact.templateId, typedImpact.componentId, typedImpact.configId, type);
-      ++stats.components;
-      break;
-    }
-
-    default:
-      throw new Error(`Unsupported impact type: '${impact.type}'`);
-  }
-}
-
-function applyUpdate(update: Update, api: UpdateApi, stats: BulkUpdatesStats) {
+function applyUpdate(context: ApplyContext, update: Update) {
   switch (update.type) {
-    case 'plugin-clear': {
-      log.debug(`Update: delete plugin '${update.id}'`);
-      api.clearPlugin(update.id);
-      ++stats.plugins;
-      break;
-    }
-
-    case 'component-clear': {
-      log.debug(`Update: delete component '${update.id}'`);
-      api.clearComponent(null, update.id); // always directly on project
-      ++stats.components;
-      break;
-    }
-
     case 'plugin-set': {
       const { plugin } = update as PluginSetUpdate;
       log.debug(`Update: set plugin '${update.id}'`);
-      api.setPlugin(plugin);
-      ++stats.plugins;
+      context.api.setPlugin(plugin);
+      context.statsAddPlugin(plugin.id);
+
+      break;
+    }
+
+    case 'plugin-clear': {
+      const { pluginId } = update as PluginClearUpdate;
+      log.debug(`Update: delete plugin '${pluginId}'`);
+      context.api.clearPlugin(pluginId);
+      context.statsAddPlugin(pluginId);
+ 
       break;
     }
 
     case 'component-set': {
       const { component } = update as ComponentSetUpdate;
-      log.debug(`Update: set component '${update.id}'`);
-      api.setComponent(component);
-      ++stats.components;
+      log.debug(`Update: set component '${component.id}'`);
+      context.api.setComponent(component);
+      context.statsAddComponent(null, component.id);
+      context.markComponentUpdated(component.id);
+
+      break;
+    }
+
+    case 'component-clear': {
+      const { componentId } = update as ComponentClearUpdate;
+      log.debug(`Update: delete component '${componentId}'`);
+      context.api.clearComponent(componentId); // always directly on project
+      context.statsAddComponent(null, componentId);
+      context.markComponentUpdated(componentId);
+
+      break;
+    }
+
+    case 'component-reset-config': {
+      const { templateId, componentId, configId } = update as ComponentConfigUpdate;
+      if (!templateId && context.isComponentUpdated(componentId)) {
+        // already part of a component set/clear, so we don't apply this update which is dependency
+        break;
+      }
+
+      log.debug(`Update: reset component config '${componentId}' - '${configId}' (template='${templateId}')`);
+      context.api.resetComponentConfig(templateId, componentId, configId);
+      context.statsAddComponent(templateId, componentId);
+
+      break;
+    }
+
+    case 'component-clear-config': {
+      const { templateId, componentId, configId } = update as ComponentConfigUpdate;
+      if (!templateId && context.isComponentUpdated(componentId)) {
+        // already part of a component set/clear, so we don't apply this update which is dependency
+        break;
+      }
+
+      log.debug(`Update: clear component config '${componentId}' - '${configId}' (template='${templateId}')`);
+      context.api.clearComponentConfig(templateId, componentId, configId);
+      context.statsAddComponent(templateId, componentId);
+
+      break;
+    }
+
+    case 'binding-clear': {
+      const { templateId, bindingId } = update as BindingClearUpdate;
+      log.debug(`Update: delete binding '${bindingId}' (template='${templateId}')`);
+      context.api.clearBinding(templateId, bindingId);
+      context.statsAddBinding(templateId, bindingId);
+
+      break;
+    }
+
+    case 'template-clear-export': {
+      const { templateId, exportType, exportId } = update as TemplateClearExportsUpdate;
+      log.debug(`Update: delete template export '${exportType}' - '${exportId}' (template='${templateId}')`);
+      context.api.clearTemplateExport(templateId, exportType, exportId);
+      context.statsAddTemplate(templateId);
+
       break;
     }
 
     default:
       throw new Error(`Unsupported update type: '${update.type}'`);
   }
-}
-
-// TODO: apply this logic again
-
-function shouldApply(update: Update, selection: Set<string>) {
-  const dependency = update.dependencies[0];
-
-  if (dependency) {
-    // dependency only for components
-    const isDelete = update.type === 'component-clear';
-    const dependencySelected = selection.has(dependency);
-
-    if (isDelete && dependencySelected) {
-      // will already be deleted as an impact of plugin delete
-      return false;
-    }
-
-    if (!isDelete && !dependencySelected) {
-      // cannot apply component without plugin
-      return false;
-    }
-  }
-
-  const key = `${update.type}:${update.id}`;
-  return selection.has(key);
 }
