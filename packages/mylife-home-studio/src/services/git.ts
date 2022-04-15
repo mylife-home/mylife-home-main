37e77ebcd53d96dbed99370b90fe59f837f7683a
vincent-tr <vincent.tr@gmail.com>
2022-04-15 16:01:14 +0200

    move to simple-git

--- packages/mylife-home-studio/src/services/git.ts
+++ packages/mylife-home-studio/src/services/git.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import cp from 'child_process';
+import simpleGit, { FileStatusResult, SimpleGit } from 'simple-git';
 import { logger, tools } from 'mylife-home-common';
 import { Service, BuildParams } from './types';
 import { Services } from '.';
@@ -18,17 +18,20 @@ export class Git implements Service {
   private readonly notifiers = new SessionNotifierManager('git/notifiers', 'git/status');
   private readonly featuresPaths: { featureName: string, path: string; }[] = [];
   private status = DEFAULT_STATUS;
-  private rawStatus: string = null; // help to find changes before parsing
+  private git: SimpleGit;
 
   constructor(params: BuildParams) {
     const config = tools.getConfigItem<Config>('git');
     this.status.appUrl = config.appUrl;
 
-    this.intervalUpdater = new Interval(60000, this.intervalUpdate);
+    this.intervalUpdater = new Interval(6000, this.intervalUpdate);
     this.statusUpdater = new Debounce(100, this.updateStatus);
   }
 
   async init() {
+    const rootPath = Services.instance.pathManager.root;
+    this.git = simpleGit({ baseDir: rootPath, maxConcurrentProcesses: 1, timeout: { block: 5000 } });
+
     this.intervalUpdater.init();
     this.statusUpdater.init();
     this.notifiers.init();
@@ -81,23 +84,17 @@ export class Git implements Service {
     this.notifiers.notifyAll(notification);
   }
 
-  private runGit(...args: string[]) {
-    const rootPath = Services.instance.pathManager.root;
-    return cp.execFileSync('git', args, { encoding: 'utf8', cwd: rootPath, timeout: 5000 });
-  }
-
-  private readonly intervalUpdate = () => {
+  private readonly intervalUpdate = async () => {
     // we need to do that too because in case of git commit we have no file change
     this.notifyFileUpdate();
 
     try {
-      let branch = this.runGit('branch', '--show-current');
-      // remove last \n char at the end
-      branch = branch.slice(0, branch.length - 1);
+      const summary = await this.git.branch();
+      const branch = summary.current;
       this.updateBranchModel(branch);
     } catch (err) {
       log.error(err, 'Error while updating branch');
-      this.updateBranchModel(DEFAULT_STATUS.branch);
+      this.updateModel({ branch: DEFAULT_STATUS.branch });
     }
   };
 
@@ -111,36 +108,36 @@ export class Git implements Service {
     this.updateModel({ branch });
   }
 
-  private readonly updateStatus = () => {
+  private readonly updateStatus = async () => {
     try {
-      const raw = this.runGit('status', '--porcelain', '-z');
-      this.updateStatusModel(raw);
+      const status = await this.git.status();
+      this.updateStatusModel(status.files);
     } catch (err) {
       log.error(err, 'Error while updating status');
-      this.updateStatusModel(null);
+      this.updateModel({ changedFeatures: {} });
     }
   };
 
-  private updateStatusModel(raw: string) {
-    if (raw === this.rawStatus) {
+  private updateStatusModel(files: FileStatusResult[]) {
+    const changedFeatures = this.buildChangedFeatures(files);
+
+    // TODO: is this stable?
+    if (JSON.stringify(changedFeatures) === JSON.stringify(this.status.changedFeatures)) {
       // No change since last update
       return;
     }
 
-    const gitFiles = parseGitStatus(raw);
-    const changedFeatures = this.buildChangedFeatures(gitFiles);
     log.info(`Setting changed features to ${JSON.stringify(changedFeatures)}`);
     this.updateModel({ changedFeatures });
-    this.rawStatus = raw;
   }
 
-  private buildChangedFeatures(gitFiles: FileStatus[]) {
+  private buildChangedFeatures(files: FileStatusResult[]) {
     const rootPath = Services.instance.pathManager.root;
     const changedFeatures: GitStatus['changedFeatures'] = {};
 
     // Build new changedFeatures
-    for (const gitFile of gitFiles) {
-      const filePath = path.join(rootPath, gitFile.to);
+    for (const file of files) {
+      const filePath = path.join(rootPath, file.path);
 
       for (const { featureName, path: featurePath } of this.featuresPaths) {
         if (!filePath.startsWith(featurePath)) {
@@ -201,41 +198,4 @@ class Interval {
   terminate() {
     clearInterval(this.handler);
   }
-}
-
-// From https://github.com/jamestalmage/parse-git-status/blob/master/index.js
-
-interface FileStatus {
-  x: string;
-  y: string;
-  to: string;
-  from: string;
-}
-
-function parseGitStatus(input: string) {
-  const parts = input.split('\0');
-  const files: FileStatus[] = [];
-
-  for (var partIndex = 0; partIndex < parts.length; ++partIndex) {
-    const part = parts[partIndex];
-
-    if (!part.length) {
-      continue;
-    }
-
-    const fileStatus: FileStatus = {
-      x: part[0],
-      y: part[1],
-      to: part.substring(3),
-      from: null
-    };
-
-    if (fileStatus.x === 'R') {
-      fileStatus.from = parts[++partIndex];
-    }
-
-    files.push(fileStatus);
-  }
-
-  return files;
 }
