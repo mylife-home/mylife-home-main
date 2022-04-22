c066888e6ae5629eb7affd6d568444d204ed3dca
vincent-tr <vincent.tr@gmail.com>
2022-04-22 16:19:50 +0200

    add selector

--- packages/mylife-home-studio/public/app/store/git/selectors.ts
+++ packages/mylife-home-studio/public/app/store/git/selectors.ts
@@ -9,3 +9,4 @@ export const getGitBranch = (state: AppState) => getStatus(state).branch;
 export const getGitChangedFeatures = createSelector(getStatus, (status) => Object.keys(status.changedFeatures).sort());
 export const getGitChangedFiles = (state: AppState) => getStatus(state).changedFeatures;
 export const getGitAppUrl = (state: AppState) => getStatus(state).appUrl;
+export const getGitCommitsCount = createSelector(getStatus, (status) => ({ ahead: status.ahead, behind: status.behind }));
