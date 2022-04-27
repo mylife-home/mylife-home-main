4b406be52878130b63af0f70aef04cb100221e15
vincent-tr <vincent.tr@gmail.com>
2022-04-27 15:13:44 +0200

    remove old status

--- packages/mylife-home-studio/public/app/store/git/selectors.ts
+++ packages/mylife-home-studio/public/app/store/git/selectors.ts
@@ -6,7 +6,6 @@ export const getNotifierId = (state: AppState) => getGit(state).notifierId;
 const getStatus = (state: AppState) => getGit(state).status;
 
 export const getGitBranch = (state: AppState) => getStatus(state).branch;
-export const getGitChangedFeatures = createSelector(getStatus, (status) => Object.keys(status.changedFeatures).sort());
-export const getGitChangedFiles = (state: AppState) => getStatus(state).changedFeatures;
+export const getGitChangedFeatures = (state: AppState) => getStatus(state).changedFeatures;
 export const getGitAppUrl = (state: AppState) => getStatus(state).appUrl;
 export const getGitCommitsCount = createSelector(getStatus, (status) => ({ ahead: status.ahead, behind: status.behind }));
