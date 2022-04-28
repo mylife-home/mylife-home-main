e74f7f08bb55c9051820ee49510358bfe82d7ffc
vincent-tr <vincent.tr@gmail.com>
2022-04-28 17:52:21 +0200

    diff first version

--- packages/mylife-home-studio/public/app/components/root/status/git-diff-view.tsx
+++ packages/mylife-home-studio/public/app/components/root/status/git-diff-view.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { AppState } from '../../../store/types';
@@ -8,9 +9,53 @@ import { diff } from '../../../store/git/types';
 
 // https://github.com/olahol/viewdiff/blob/master/client.js
 
+const useStyles = makeStyles((theme) => ({
+  table: {
+    fontFamily: `Consolas, 'Liberation Mono', Menlo, Courier, monospace;`,
+    borderCollapse: 'collapse',
+    width: '100%',
+  },
+
+  td: {
+    padding: '5px 10px',
+    fontSize: '12px',
+  },
+
+  noStretch: {
+    width: '1%',
+    whiteSpace: 'nowrap',
+  },
+
+  chunkTd: {
+    padding: '15px 10px',
+    fontSize: '12px',
+  },
+
+  chunk: {
+    backgroundColor: '#f4f7fb',
+    color: '#bbb',
+  },
+
+  normal: {
+
+  },
+
+  add: {
+    backgroundColor: '#dbffdb',
+    fontWeight: 600,
+  },
+
+  del: {
+    backgroundColor: '#ffdddd',
+    fontWeight: 600,
+  }
+}));
+
 const DiffView: FunctionComponent<{ chunks: string[]; }> = ({ chunks }) => {
+  const classes = useStyles();
+
   return (
-    <table>
+    <table className={classes.table}>
       {chunks.map(chunkId => (
         <ChunkView key={chunkId} chunkId={chunkId} />
       ))}
@@ -21,14 +66,15 @@ const DiffView: FunctionComponent<{ chunks: string[]; }> = ({ chunks }) => {
 export default DiffView;
 
 const ChunkView: FunctionComponent<{ chunkId: string }> = ({ chunkId }) => {
+  const classes = useStyles();
   const chunk = useSelector((store: AppState) => getGitDiffChunk(store, chunkId));
 
   return (
     <tbody>
-      <tr className="chunk">
-        <td className="nostretch">---</td>
-        <td className="nostretch">+++</td>
-        <td>{chunk.content}</td>
+      <tr className={classes.chunk}>
+        <td className={clsx(classes.noStretch, classes.chunkTd)}>---</td>
+        <td className={clsx(classes.noStretch, classes.chunkTd)}>+++</td>
+        <td className={classes.td}>{chunk.content}</td>
       </tr>
       {chunk.changes.map(change => (
         <Change change={change} />
@@ -38,18 +84,19 @@ const ChunkView: FunctionComponent<{ chunkId: string }> = ({ chunkId }) => {
 };
 
 const Change: FunctionComponent<{ change: diff.Change }> = ({ change }) => {
+  const classes = useStyles();
   const ln1 = change.type === 'normal' ? change.ln1 : change.ln;
   const ln2 = change.type === 'normal' ? change.ln2 : change.ln;
 
   return (
-    <tr className={change.type}>
-      <td className="nostretch">
+    <tr className={classes[change.type]}>
+      <td className={clsx(classes.noStretch, classes.td)}>
         {change.type !== 'add' && ln1}
       </td>
-      <td className="nostretch">
+      <td className={clsx(classes.noStretch, classes.td)}>
         {change.type !== 'del' && ln2}
       </td>
-      <td>
+      <td className={classes.td}>
         {change.content}
       </td>
     </tr>
