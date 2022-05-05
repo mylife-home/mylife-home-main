3c2cb6051add799e963d857f3c198b5d05f9ab54
vincent-tr <vincent.tr@gmail.com>
2022-05-05 11:10:57 +0200

    link to css

--- packages/mylife-home-ui/public/app/store/store.ts
+++ packages/mylife-home-ui/public/app/store/store.ts
@@ -2,11 +2,12 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import { socketMiddleware } from './middlewares/socket';
+import { cssMiddleware } from './middlewares/css';
 import { resourcesMiddleware } from './middlewares/resources';
 import { navigationMiddleware } from './middlewares/navigation';
 import reducer from './reducers/index';
 
-const middlewares = [navigationMiddleware, socketMiddleware, resourcesMiddleware, thunk];
+const middlewares = [navigationMiddleware, socketMiddleware, resourcesMiddleware, cssMiddleware, thunk];
 
 if (process.env.NODE_ENV !== 'production') {
   const { createLogger } = require('redux-logger');
