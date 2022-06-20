b5c411aaa96a74a9955172d03c14afce2c2fa356
vincent-tr <vincent.tr@gmail.com>
2022-06-20 12:10:38 +0200

    fix value

--- packages/mylife-home-core-plugins-logic-base/src/value-percent.ts
+++ packages/mylife-home-core-plugins-logic-base/src/value-percent.ts
@@ -4,6 +4,11 @@ import m = components.metadata;
 
 @m.plugin({ usage: m.PluginUsage.LOGIC })
 export class ValuePercent {
+  private readonly toggleThreshold: number;
+
+  constructor({ toggleThreshold }: { toggleThreshold: number; }) {
+    this.toggleThreshold = toggleThreshold;
+  }
 
   @m.state({ type: new m.Range(0, 100) })
   value: number = 0;
@@ -17,6 +22,27 @@ export class ValuePercent {
   setPulse(arg: number) {
     if (arg !== -1) {
       this.value = arg;
+    }
+  }
+
+  @m.action
+  on(arg: boolean) {
+    if (arg) {
+      this.value = 100;
+    }
+  }
+
+  @m.action
+  off(arg: boolean) {
+    if (arg) {
+      this.value = 0;
+    }
+  }
+
+  @m.action
+  toggle(arg: boolean) {
+    if (arg) {
+      this.value = this.value < this.toggleThreshold ? 100 : 0;
     }
   }
 };
