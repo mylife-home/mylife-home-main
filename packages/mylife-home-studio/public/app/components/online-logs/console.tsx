d37262723bc13ceed32d2e1dad18ae9edd84ac08
vincent-tr <vincent.tr@gmail.com>
2023-01-24 15:09:38 +0100

    add logs console layout

--- packages/mylife-home-studio/public/app/components/online-logs/console.tsx
+++ packages/mylife-home-studio/public/app/components/online-logs/console.tsx
@@ -1,24 +1,137 @@
 import React, { FunctionComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles, darken } from '@material-ui/core/styles';
+import { AutoSizer, List } from 'react-virtualized';
+import clsx from 'clsx';
 
 import { addLineBreaks } from '../lib/add-line-breaks';
-import VirtualizedTable, { ColumnDefinition } from '../lib/virtualized-table';
 import { LogItem, LogError } from '../../store/online-logs/types';
 import { findLevelByValue, useLevelStyles, getLevelClass } from './levels';
 
+const useStyles = makeStyles((theme) => ({
+  row: {
+    marginLeft: theme.spacing(2),
+    fontFamily: 'monospace',
+    fontSize: 12,
+    display: 'inline',
+    whiteSpace: 'pre-wrap',
+    overflowWrap: 'break-word',
+  },
+  timestamp: {
+    color: darken(theme.palette.background.paper, 0.5),
+  },
+  level: {
+
+  },
+  location: {
+    color: darken(theme.palette.background.paper, 0.5),
+  },
+  message: {
+
+  },
+  error: {
+    color: theme.palette.error.main,
+  }
+}));
+
 const Console: FunctionComponent<{ className?: string; data: LogItem[]; }> = ({ className, data }) => {
+  const classes = useStyles();
 
   return (
     <div className={className}>
-      console
+      <AutoSizer>
+        {({ height, width }) => (
+          <List height={height} width={width} rowCount={data.length} rowHeight={16} rowRenderer={({ index, style }) => {
+            const item = data[index];
+            return (
+              <span key={item.id} className={classes.row} style={style}>
+                <Timestamp value={item.time} />
+                <Space />
+                <Level value={item.level} />
+                <Space />
+                <Location instanceName={item.instanceName} name={item.name} />
+                <Space />
+                <Message value={item.msg} />
+                {item.err && (
+                  <>
+                    <Space />
+                    <Error value={item.err} />
+                  </>
+                )}
+                <LineEnd />
+              </span>
+            );
+          }} />
+        )}
+      </AutoSizer>
     </div>
   );
 };
 
 export default Console;
+
+const Timestamp: FunctionComponent<{ value: Date }> = ({ value }) => {
+  const classes = useStyles();
+  return (
+    <span className={classes.timestamp}>
+      {value.toLocaleString('fr-FR')}
+    </span>
+  );
+};
+
+const Level: FunctionComponent<{ value: number }> = ({ value }) => {
+  const classes = useStyles();
+  const levelClasses = useLevelStyles();
+  const level = findLevelByValue(value);
+  const levelClass = getLevelClass(levelClasses, level);
+  const levelDisplay = level ? level.id.toUpperCase() : `${value}`;
+
+  return (
+    <span className={clsx(classes.level, levelClass)}>
+      {levelDisplay}
+    </span>
+  );
+};
+
+const Location: FunctionComponent<{ instanceName: string; name: string; }> = ({ instanceName, name }) => {
+  const classes = useStyles();
+  return (
+    <span className={classes.location}>
+      {`${name}@${instanceName}`}
+    </span>
+  );
+};
+
+const Message: FunctionComponent<{ value: string }> = ({ value }) => {
+  const classes = useStyles();
+  return (
+    <span className={classes.message}>
+      {value}
+    </span>
+  );
+};
+
+const Error: FunctionComponent<{ value: LogError }> = ({ value }) => {
+  const classes = useStyles();
+  return (
+    <span className={classes.error}>
+      {value.message}
+      {addLineBreaks(value.stack)}
+    </span>
+  );
+};
+
+const Space: FunctionComponent = () => {
+  return (
+    <span>
+      {' '}
+    </span>
+  );
+};
+
+const LineEnd: FunctionComponent = () => {
+  return (
+    <span>
+      {'\n'}
+    </span>
+  );
+};
